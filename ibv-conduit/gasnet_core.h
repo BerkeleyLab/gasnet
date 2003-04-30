0ac7eb81f231fbc8ed308b9d03b36f165ab45c57
Paul H. Hargrove <PHHargrove@lbl.gov>
2003-04-30 16:34:50 +0000

    Fix polling routines to avoid deadlock.
    The routines for polling RDMA completion have been restructured and
    have become inline functions.
    
    Previously a blocking RDMA operation would go into a tight spinloop
    that checked only the send queue, and did not service any incomming
    AMs.  That is fine when we have an extra thread that wakes on receive.
    However, the spinloops now do a full AMPoll, ensuring that we could
    drop the receive thread to yield a pure polling implementation which
    is deadlock free.

--- ibv-conduit/gasnet_core.h
+++ ibv-conduit/gasnet_core.h
@@ -1,6 +1,6 @@
 /*  $Archive:: /Ti/GASNet/template-conduit/gasnet_core.h                  $
- *     $Date: 2003/04/25 21:57:18 $
- * $Revision: 1.2.2.13 $
+ *     $Date: 2003/04/30 16:34:50 $
+ * $Revision: 1.2.2.14 $
  * Description: GASNet header for vapi conduit core
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -499,8 +499,27 @@ extern int gasnetc_AMReplyLongM(
 extern int gasnetc_rdma_put(int node, void *src_ptr, void *dst_ptr, uintptr_t nbytes, gasneti_atomic_t *mem_oust, gasneti_atomic_t *req_oust);
 extern int gasnetc_rdma_get(int node, void *src_ptr, void *dst_ptr, size_t nbytes, gasneti_atomic_t *req_oust);
 extern int gasnetc_rdma_memset(int node, void *dst_ptr, int val, size_t nbytes, gasneti_atomic_t *req_oust);
-extern void gasnetc_rdma_wait(gasneti_atomic_t *req_oust);
-extern int  gasnetc_rdma_test(gasneti_atomic_t *req_oust);
+
+/* Check if a given counter is marked as done.
+ * Note that the AMPoll belongs outside this call.
+ */
+GASNET_INLINE_MODIFIER(gasnetc_counter_test)
+int gasnetc_counter_test(gasneti_atomic_t *counter) { 
+  return (gasneti_atomic_read(counter) == 0);
+} 
+
+/* Wait until given counter is marked as done.
+ * Note that at least one AMPoll will be done.
+ */
+GASNET_INLINE_MODIFIER(gasnetc_counter_wait)
+void gasnetc_counter_wait(gasneti_atomic_t *counter) { 
+  GASNETI_TRACE_PRINTF(C, ("gasnetc_counter_wait: on entry counter %p has value %d", counter, (int)gasneti_atomic_read(counter)));
+  do {
+    gasnet_AMPoll();
+  } while (gasneti_atomic_read(counter) != 0);
+  GASNETI_TRACE_PRINTF(C, ("gasnetc_counter_wait: counter %p is done", counter));
+} 
+
 /* ------------------------------------------------------------------------------------ */
 
 END_EXTERNC
