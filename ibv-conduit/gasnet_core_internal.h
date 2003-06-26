c8ce5249523a139cf1fccfa7d350f09d9fbdeac8
Paul H. Hargrove <PHHargrove@lbl.gov>
2003-06-26 22:22:04 +0000

    Fixed serious bug in AM Long Replies.
    I realized over my moring coffee that some recent changes I had made
    caused a violation of the AM semantics when sending long replies.
    In particular I was calling the internal equivalent of AMPoll() while
    waiting for local completion.  This would poll the recv queue and thus
    potentially run a new AM handler interrupting the running one!
    
    The fix is that when polling for completion of the RDMA put in
    gasnetc_AMReplyLongM(), we now poll only the send queue.
    
    This bug was probably responsible for the one time I saw testam hang.

--- ibv-conduit/gasnet_core_internal.h
+++ ibv-conduit/gasnet_core_internal.h
@@ -1,6 +1,6 @@
 /*  $Archive:: /Ti/GASNet/template-conduit/gasnet_core_internal.h         $
- *     $Date: 2003/06/24 18:46:02 $
- * $Revision: 1.1.2.35 $
+ *     $Date: 2003/06/26 22:22:03 $
+ * $Revision: 1.1.2.36 $
  * Description: GASNet vapi conduit header for internal definitions in Core API
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -283,7 +283,6 @@ extern void gasnetc_bootstrapBroadcast(void *src, size_t len, void *dest, int ro
 extern void gasnetc_sndrcv_init(void);
 extern void gasnetc_sndrcv_fini(void);
 extern void gasnetc_sndrcv_init_cep(gasnetc_cep_t *cep);
-extern void gasnetc_sndrcv_poll(void);
 extern int gasnetc_RequestGeneric(gasnetc_category_t category,
 				  int dest, gasnet_handler_t handler,
 				  void *src_addr, int nbytes, void *dst_addr,
