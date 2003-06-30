5614a5ba2179859fd5a4820fe77bb3b4979ea607
Paul H. Hargrove <PHHargrove@lbl.gov>
2003-06-30 18:04:47 +0000

    Bring vapi-conduit up-to-date w.r.t recent changes by Dan. This includes the gasnet_Errorname changes. This also include the versions numbers, even though vapi-conduit will not be part of the 1.2 release.

--- ibv-conduit/gasnet_core_fwd.h
+++ ibv-conduit/gasnet_core_fwd.h
@@ -1,6 +1,6 @@
 /*  $Archive:: /Ti/GASNet/template-conduit/gasnet_core_fwd.h              $
- *     $Date: 2003/06/30 17:31:43 $
- * $Revision: 1.2.2.8 $
+ *     $Date: 2003/06/30 18:04:46 $
+ * $Revision: 1.2.2.9 $
  * Description: GASNet header for vapi conduit core (forward definitions)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -13,7 +13,7 @@
 #ifndef _GASNET_CORE_FWD_H
 #define _GASNET_CORE_FWD_H
 
-#define GASNET_CORE_VERSION      0.1
+#define GASNET_CORE_VERSION      1.2
 #define GASNET_CORE_VERSION_STR  _STRINGIFY(GASNET_CORE_VERSION)
 #define GASNET_CORE_NAME         VAPI
 #define GASNET_CORE_NAME_STR     _STRINGIFY(GASNET_CORE_NAME)
