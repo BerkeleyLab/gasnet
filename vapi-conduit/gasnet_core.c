b52a2f963029d442e9b09b38a689009a28cb0f1b
Christian Bell <csbell@cs.berkeley.edu>
2003-08-04 11:06:55 +0000

    firehose checkin
    - Implemented firehose-page, although still some things to iron out as the
      specification is still floating.
    - firehose.h has been revised, although there are a few things to add (such as
      the ability to run a handler on the remote node once a region is pinned).
    - implemented the firehose public interface inside GM.  The GM conduit lost
      ~35% in weight (lines of code).
    - Merge in HEAD on FIREHOSE_001 branch

--- vapi-conduit/gasnet_core.c
+++ vapi-conduit/gasnet_core.c
@@ -1,6 +1,6 @@
 /*  $Archive:: /Ti/GASNet/template-conduit/gasnet_core.c                  $
- *     $Date: 2003/07/03 23:07:56 $
- * $Revision: 1.5 $
+ *     $Date: 2003/08/04 11:06:55 $
+ * $Revision: 1.5.2.1 $
  * Description: GASNet vapi conduit Implementation
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -21,7 +21,7 @@ GASNETI_IDENT(gasnetc_IdentString_ConduitName, "$GASNetConduitName: " GASNET_COR
 
 
 #define GASNETC_QP_PATH_MTU		MTU1024
-#define GASNETC_QP_STATIC_RATE		2
+#define GASNETC_QP_STATIC_RATE		0
 #define GASNETC_QP_MIN_RNR_TIMER	IB_RNR_NAK_TIMER_0_08
 #define GASNETC_QP_RNR_RETRY		7	/* retry forever, but almost never happens */
 #define GASNETC_QP_TIMEOUT		0x20
