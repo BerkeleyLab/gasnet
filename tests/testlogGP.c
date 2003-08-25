6c94d9cbf2018c133656d32d3c08014912058a89
Christian Bell <csbell@cs.berkeley.edu>
2003-08-25 19:49:02 +0000

    commit tests changes to FIREHOSE_001 branch

--- tests/testlogGP.c
+++ tests/testlogGP.c
@@ -1,6 +1,11 @@
 /*  $Archive:: /Ti/GASNet/tests/testsmall.c                                 $
- *     $Date: 2003/08/04 11:06:53 $
- * $Revision: 1.3.2.1 $
+<<<<<<< testlogGP.c
+ *     $Date: 2003/08/25 19:49:02 $
+ * $Revision: 1.3.2.2 $
+=======
+ *     $Date: 2003/08/25 19:49:02 $
+ * $Revision: 1.3.2.2 $
+>>>>>>> 1.9
  * Description: GASNet logGP tester.
  *   measures the ping-pong average round-trip time and
  *   average flood throughput of GASNet gets and puts
@@ -167,11 +172,11 @@ void put_tests(int iters, int nbytes)
 	BARRIER();
 	
 	if (iamsender) {
-    		/* measure baseline (no cpu loop) gap for nonblocking explicit puts */
+    		/* measure baseline (no cpu loop) gap for nonblocking explicit bulk puts */
 		init_stat(&st, nbytes);
 		begin = TIME();
 		for (i = 0; i < iters; i++) {
-			gasnet_handle_t h = gasnet_put_nb(peerproc, peermem, mymem, nbytes);
+			gasnet_handle_t h = gasnet_put_nb_bulk(peerproc, peermem, mymem, nbytes);
 			gasnet_wait_syncnb(h);
 		}
 		end = TIME();
@@ -185,13 +190,13 @@ void put_tests(int iters, int nbytes)
 		init_stat(&st, nbytes);
 		begin = TIME();
 		for (i = 0; i < iters; i++) {
-			gasnet_handle_t h = gasnet_put_nb(peerproc, peermem, mymem, nbytes);
+			gasnet_handle_t h = gasnet_put_nb_bulk(peerproc, peermem, mymem, nbytes);
 			delay(loops);
 			gasnet_wait_syncnb(h);
 		}
 		end = TIME();
 	 	update_stat(&st, (end - begin) - delay_time, iters);
-		print_stat(myproc, &st, "put: o_i - put_nb", PRINT_OVERHEAD);
+		print_stat(myproc, &st, "put: o_i - put_nb_bulk", PRINT_OVERHEAD);
 	}
 
 	BARRIER();
@@ -255,11 +260,11 @@ void get_tests(int iters, int nbytes)
 	BARRIER();
 	
 	if (iamsender) {
-    		/* measure baseline (no cpu loop) gap for nonblocking explicit gets */
+    		/* measure baseline (no cpu loop) gap for nonblocking explicit bulk gets */
 		init_stat(&st, nbytes);
 		begin = TIME();
 		for (i = 0; i < iters; i++) {
-			gasnet_handle_t h = gasnet_get_nb(mymem, peerproc, peermem, nbytes);
+			gasnet_handle_t h = gasnet_get_nb_bulk(mymem, peerproc, peermem, nbytes);
 			gasnet_wait_syncnb(h);
 		}
 		end = TIME();
@@ -273,13 +278,13 @@ void get_tests(int iters, int nbytes)
 		init_stat(&st, nbytes);
 		begin = TIME();
 		for (i = 0; i < iters; i++) {
-			gasnet_handle_t h = gasnet_get_nb(mymem, peerproc, peermem, nbytes);
+			gasnet_handle_t h = gasnet_get_nb_bulk(mymem, peerproc, peermem, nbytes);
 			delay(loops);
 			gasnet_wait_syncnb(h);
 		}
 		end = TIME();
 	 	update_stat(&st, (end - begin) - delay_time, iters);
-		print_stat(myproc, &st, "get: o_i - get_nb", PRINT_OVERHEAD);
+		print_stat(myproc, &st, "get: o_i - get_nb_bulk", PRINT_OVERHEAD);
 	}
 
 	BARRIER();
--- tests/testthreads.c
+++ tests/testthreads.c
@@ -1,4 +1,4 @@
-/* $Id: testthreads.c,v 1.3 2003/08/25 19:02:16 phargrov Exp $
+/* $Id: testthreads.c,v 1.3.2.1 2003/08/25 19:49:02 csbell Exp $
  *
  * Description: GASNet threaded tester.
  *   The test initializes GASNet and forks off up to 256 threads.  Each of
