036825cbf12b8fc49bd29fd634b21ae3737be96c
Paul H. Hargrove <PHHargrove@lbl.gov>
2003-10-22 19:57:37 +0000

    Use VAPI's "repinning" operation in firehose_move_callback(). This presumably saves at least a lock acquisition, and may allow some reuse of driver/firmware resources.

--- ibv-conduit/gasnet_firehose.c
+++ ibv-conduit/gasnet_firehose.c
@@ -1,12 +1,12 @@
 /* vapi-conduit/gasnet_firehose.c
- * $Date: 2003/10/08 16:11:29 $
- * $Revision: 1.1.2.1 $
+ * $Date: 2003/10/22 19:57:37 $
+ * $Revision: 1.1.2.2 $
  * Description: Client-specific firehose code
  * Copyright 2003, E. O. Lawrence Berekely National Laboratory
  * Terms of use are as specified in license.txt
  */
 
-/* (NEARLY) EMPTY FILE FOR NOW */
+/* Implement client-specific callbacks for use by firehose-region */
 
 #include <gasnet.h>
 #include <gasnet_internal.h>
@@ -23,41 +23,72 @@ firehose_move_callback(gasnet_node_t node,
 {
     VAPI_ret_t    vstat;
     VAPI_mr_t     mr_in;
+    int repin_num;
     int i;
 
-    for (i = 0; i < unpin_num; i++) {
-	const firehose_region_t *region = unpin_list + i;
-
-	assert(region->addr % GASNETI_PAGESIZE == 0);
-	assert(region->len % GASNETI_PAGESIZE == 0);
-
-	vstat = VAPI_deregister_mr(gasnetc_hca, region->client.handle);
-	assert(vstat == VAPI_OK);
-    }
-                                                                                                              
     mr_in.type    = VAPI_MR;
     mr_in.pd_hndl = gasnetc_pd;
-    mr_in.acl     = VAPI_EN_LOCAL_WRITE | VAPI_EN_REMOTE_WRITE | VAPI_EN_REMOTE_READ;
+    mr_in.acl     = VAPI_EN_LOCAL_WRITE |
+		    VAPI_EN_REMOTE_WRITE |
+		    VAPI_EN_REMOTE_READ;
 
-    for (i = 0; i < pin_num; i++) {
+    /* Perform replacements where possible */
+    repin_num = MIN(unpin_num, pin_num);
+    for (i = 0; i < repin_num; i++) {
 	firehose_region_t *region = pin_list + i;
 	firehose_client_t *client = &region->client;
+	VAPI_mr_hndl_t old_handle = unpin_list[i].client.handle;
 	VAPI_mr_t mr_out;
 
 	assert(region->addr % GASNETI_PAGESIZE == 0);
 	assert(region->len % GASNETI_PAGESIZE == 0);
-                                                                                                              
+
 	mr_in.start = (uintptr_t)region->addr;
 	mr_in.size  = region->len;
-                                                                                                              
-	vstat = VAPI_register_mr(gasnetc_hca, &mr_in, &client->handle, &mr_out);
+
+	vstat = VAPI_reregister_mr(gasnetc_hca, old_handle,
+				   VAPI_MR_CHANGE_TRANS,
+				   &mr_in, &client->handle, &mr_out);
 	assert(vstat == VAPI_OK);
-                                                                                                              
+
 	client->lkey     = mr_out.l_key;
 	client->rkey     = mr_out.r_key;
     }
+    unpin_list += repin_num;
+    unpin_num -= repin_num;
+    pin_list += repin_num;
+    pin_num -= repin_num;
 
-    return -1;	/* not yet implemented */
+    /* Take care of any "left over".
+     * Note that we can't have entries left in *both* lists */
+    if (unpin_num) {
+        for (i = 0; i < unpin_num; i++) {
+	    VAPI_mr_hndl_t old_handle = unpin_list[i].client.handle;
+
+	    vstat = VAPI_deregister_mr(gasnetc_hca, old_handle);
+	    assert(vstat == VAPI_OK);
+        }
+    }
+    else if (pin_num) {
+        for (i = 0; i < pin_num; i++) {
+	    firehose_region_t *region = pin_list + i;
+	    firehose_client_t *client = &region->client;
+	    VAPI_mr_t mr_out;
+    
+	    assert(region->addr % GASNETI_PAGESIZE == 0);
+	    assert(region->len % GASNETI_PAGESIZE == 0);
+    
+	    mr_in.start = (uintptr_t)region->addr;
+	    mr_in.size  = region->len;
+    
+	    vstat = VAPI_register_mr(gasnetc_hca, &mr_in, &client->handle, &mr_out);
+	    assert(vstat == VAPI_OK);
+    
+	    client->lkey     = mr_out.l_key;
+	    client->rkey     = mr_out.r_key;
+	}
+    }
+    return 0;
 }
 
 extern int
@@ -66,6 +97,6 @@ firehose_remote_callback(gasnet_node_t node,
                          firehose_remotecallback_args_t *args)
 {
     /* DO NOTHING.  IF WE GET CALLED WE COMPLAIN. */
-    assert(0);
+    gasneti_fatalerror("attempted to call firehose_remote_callback()");
     return -1;
 }
