051529b38b7e7660c584d1c015b41f4c8aaaede5
Paul H. Hargrove <PHHargrove@lbl.gov>
2003-10-24 21:27:47 +0000

    Merge Dan's DEBUG -> GASNET_DEBUG changes into firehose-region branch.

--- ibv-conduit/gasnet_extended_fwd.h
+++ ibv-conduit/gasnet_extended_fwd.h
@@ -1,6 +1,6 @@
 /*  $Archive:: /Ti/GASNet/extended/gasnet_extended_fwd.h                  $
- *     $Date: 2003/10/14 16:34:00 $
- * $Revision: 1.2.6.1 $
+ *     $Date: 2003/10/24 21:27:42 $
+ * $Revision: 1.2.6.2 $
  * Description: GASNet Extended API Header (forward decls)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -25,7 +25,7 @@ struct _gasnete_op_t;
 typedef struct _gasnete_op_t *gasnet_handle_t;
 #define GASNET_INVALID_HANDLE ((gasnet_handle_t)0)
 
-#ifdef GASNETI_CLIENT_THREADS
+#if GASNETI_CLIENT_THREADS
   #define GASNETI_THREADINFO_OPT
   #define GASNETI_LAZY_BEGINFUNCTION
 #endif
