c572bda5dbe7715ad6231a810bd063dce8a31cd5
Paul H. Hargrove <PHHargrove@lbl.gov>
2004-03-31 23:28:31 +0000

    Start a branch for work on GASNet collective reference implementation.

---
+++ extended-ref/gasnet_coll_putget.c
@@ -0,0 +1,12 @@
+/*  $Archive:: /Ti/GASNet/extended-ref/gasnet_extended_refcoll.c $
+ *     $Date: 2004/03/31 23:28:27 $
+ * $Revision: 1.1.2.1 $
+ * Description: Reference implemetation of GASNet Collectives
+ * Copyright 2004, Dan Bonachea <bonachea@cs.berkeley.edu>
+ * Terms of use are as specified in license.txt
+ */
+                                                                                                              
+#ifndef GASNETI_GASNET_EXTENDED_COLL_C
+  #error This file not meant to be compiled directly - included by gasnet_extended.c
+#endif
+
