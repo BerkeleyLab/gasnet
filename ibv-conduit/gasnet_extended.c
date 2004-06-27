744da0ef863bca15ed2baa12a9e12f207696af60
Dan Bonachea <DOBonachea@lbl.gov>
2004-06-27 18:31:08 +0000

    Merge recent changes from head to SCI branch

--- ibv-conduit/gasnet_extended.c
+++ ibv-conduit/gasnet_extended.c
@@ -1,6 +1,6 @@
 /*  $Archive:: /Ti/GASNet/extended-ref/gasnet_extended.c                  $
- *     $Date: 2004/04/20 00:24:39 $
- * $Revision: 1.8.2.3 $
+ *     $Date: 2004/06/27 18:31:04 $
+ * $Revision: 1.8.2.4 $
  * Description: GASNet Extended API Reference Implementation
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -314,42 +314,9 @@ SHORT_HANDLER(gasnete_done_reph,1,2,
 */
 /* called at startup to check configuration sanity */
 static void gasnete_check_config() {
-  gasneti_assert(sizeof(int8_t) == 1);
-  gasneti_assert(sizeof(uint8_t) == 1);
-  #if !defined(CRAYT3E)
-    gasneti_assert(sizeof(int16_t) == 2);
-    gasneti_assert(sizeof(uint16_t) == 2);
-  #endif
-  gasneti_assert(sizeof(int32_t) == 4);
-  gasneti_assert(sizeof(uint32_t) == 4);
-  gasneti_assert(sizeof(int64_t) == 8);
-  gasneti_assert(sizeof(uint64_t) == 8);
+  gasneti_check_config_postattach();
 
-  gasneti_assert(sizeof(uintptr_t) >= sizeof(void *));
-
-  /* check GASNET_PAGESIZE is a power of 2 and > 0 */
-  gasneti_assert(GASNET_PAGESIZE > 0 && 
-         (GASNET_PAGESIZE & (GASNET_PAGESIZE - 1)) == 0);
-
-  gasneti_assert(SIZEOF_GASNET_REGISTER_VALUE_T == sizeof(gasnet_register_value_t));
-  gasneti_assert(SIZEOF_GASNET_REGISTER_VALUE_T >= sizeof(int));
-  gasneti_assert(SIZEOF_GASNET_REGISTER_VALUE_T >= sizeof(void *));
-
-  #if    defined(GASNETI_PTR32) && !defined(GASNETI_PTR64)
-    gasneti_assert(sizeof(void*) == 4);
-  #elif !defined(GASNETI_PTR32) &&  defined(GASNETI_PTR64)
-    gasneti_assert(sizeof(void*) == 8);
-  #else
-    #error must #define exactly one of GASNETI_PTR32 or GASNETI_PTR64
-  #endif
-
-  gasneti_assert(gasnete_eopaddr_isnil(EOPADDR_NIL));
-
-  /*  verify sanity of the core interface */
-  gasneti_assert(gasnet_AMMaxArgs() >= 2*MAX(sizeof(int),sizeof(void*)));      
-  gasneti_assert(gasnet_AMMaxMedium() >= 512);
-  gasneti_assert(gasnet_AMMaxLongRequest() >= 512);
-  gasneti_assert(gasnet_AMMaxLongReply() >= 512);
+  gasneti_assert_always(gasnete_eopaddr_isnil(EOPADDR_NIL));
 }
 
 extern void gasnete_init() {
@@ -835,6 +802,17 @@ extern gasnet_register_value_t gasnete_wait_syncnb_valget(gasnet_valget_handle_t
 #define GASNETI_GASNET_EXTENDED_VIS_C 1
 #include "gasnet_extended_refvis.c"
 #undef GASNETI_GASNET_EXTENDED_VIS_C
+           
+/* ------------------------------------------------------------------------------------ */
+/*
+  Collectives:
+  ============
+*/
+
+/* use reference implementation of collectives */
+#define GASNETI_GASNET_EXTENDED_COLL_C 1
+#include "gasnet_extended_refcoll.c"
+#undef GASNETI_GASNET_EXTENDED_COLL_C
 
 /* ------------------------------------------------------------------------------------ */
 /*
@@ -847,6 +825,9 @@ static gasnet_handlerentry_t const gasnete_handlers[] = {
   #endif
   #ifdef GASNETE_REFVIS_HANDLERS
     GASNETE_REFVIS_HANDLERS(),
+  #endif
+  #ifdef GASNETE_REFCOLL_HANDLERS
+    GASNETE_REFCOLL_HANDLERS(),
   #endif
 
   /* ptr-width independent handlers */
