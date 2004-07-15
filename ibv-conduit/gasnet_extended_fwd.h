87f25c686015d6ebf331e73b2293327d3868ab6b
Christian Bell <csbell@cs.berkeley.edu>
2004-07-15 02:01:45 +0000

    Merge trunk back into firehose-smp branch
    Keep the branch in sync with the trunk for now, in case further development
    warrants work on the same branch.

--- ibv-conduit/gasnet_extended_fwd.h
+++ ibv-conduit/gasnet_extended_fwd.h
@@ -1,6 +1,6 @@
 /*  $Archive:: /Ti/GASNet/extended/gasnet_extended_fwd.h                  $
- *     $Date: 2004/07/08 16:59:24 $
- * $Revision: 1.6.4.1 $
+ *     $Date: 2004/07/15 02:01:39 $
+ * $Revision: 1.6.4.2 $
  * Description: GASNet Extended API Header (forward decls)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
