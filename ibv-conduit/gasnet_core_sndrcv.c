8940e0fc0b86b2a9e043203e4743a3cace312e5f
Christian Bell <csbell@cs.berkeley.edu>
2004-08-30 06:58:18 +0000

    Second part to shmem branch merge
    Propagate the $Source$ keyword change, which brings the shmem branch fully up
    to date with head.

--- ibv-conduit/gasnet_core_sndrcv.c
+++ ibv-conduit/gasnet_core_sndrcv.c
@@ -1,6 +1,6 @@
-/*  $Archive:: gasnet/gasnet-conduit/gasnet_core_sndrcv.c                  $
- *     $Date: 2004/08/30 05:05:22 $
- * $Revision: 1.47.2.3 $
+/*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core_sndrcv.c,v $
+ *     $Date: 2004/08/30 06:58:12 $
+ * $Revision: 1.47.2.4 $
  * Description: GASNet vapi conduit implementation, transport send/receive logic
  * Copyright 2003, LBNL
  * Terms of use are as specified in license.txt
