aef8c338f928ec3e1c16acc5750c2661cbf8168e
Christian Bell <csbell@cs.berkeley.edu>
2004-08-30 06:58:15 +0000

    Second part to shmem branch merge
    Propagate the $Source$ keyword change, which brings the shmem branch fully up
    to date with head.

--- ibv-conduit/gasnet_core_fwd.h
+++ ibv-conduit/gasnet_core_fwd.h
@@ -1,6 +1,6 @@
-/*  $Archive:: /Ti/GASNet/vapi-conduit/gasnet_core_fwd.h              $
- *     $Date: 2004/08/30 05:05:22 $
- * $Revision: 1.11.2.2 $
+/*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core_fwd.h,v $
+ *     $Date: 2004/08/30 06:58:12 $
+ * $Revision: 1.11.2.3 $
  * Description: GASNet header for vapi conduit core (forward definitions)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -22,7 +22,7 @@
 #endif
 
 
-#define GASNET_CORE_VERSION      1.3
+#define GASNET_CORE_VERSION      1.4
 #define GASNET_CORE_VERSION_STR  _STRINGIFY(GASNET_CORE_VERSION)
 #define GASNET_CORE_NAME         VAPI
 #define GASNET_CORE_NAME_STR     _STRINGIFY(GASNET_CORE_NAME)
