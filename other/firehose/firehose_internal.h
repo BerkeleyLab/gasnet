05f2d8431434dedf90019e09467422acf8adba16
Paul H. Hargrove <PHHargrove@lbl.gov>
2006-02-05 04:12:11 +0000

    Merge a fix from HEAD to 2.2 branch: > Disable firehose bucket debugging in an non-debug build. > This has apparently been enabled in all releases since GASNet 1.4 > (Berkeley UPC 2.0). > While the assert()ions would compile away, the state field in the bucket > struct would not, nor would the writes to that field. CVSMAIL: CC:

--- other/firehose/firehose_internal.h
+++ other/firehose/firehose_internal.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/other/firehose/firehose_internal.h,v $
- *     $Date: 2005/08/08 02:20:34 $
- * $Revision: 1.29 $
+ *     $Date: 2006/02/05 04:12:11 $
+ * $Revision: 1.29.2.1 $
  * Description: Internal Header file
  * Copyright 2004, Christian Bell <csbell@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -176,14 +176,15 @@ fh_refc_t;
 /*
  * Bucket and private types
  */
-#define DEBUG_BUCKETS
+#if GASNET_DEBUG
+  #define DEBUG_BUCKETS
+#endif
 #ifdef DEBUG_BUCKETS
   typedef enum { fh_local_fifo, fh_remote_fifo, fh_pending, fh_pending_commit, fh_used, fh_unused }
   fh_bstate_t;
   #define FH_BSTATE_ASSERT(entry, state) gasneti_assert((entry)->fh_state == state)
   #define FH_BSTATE_SET(entry, state)	 (entry)->fh_state = state
-  #define FH_BSTATE(entry)		 (entry)->fh_state
-  #else
+#else
   #define FH_BSTATE_ASSERT(entry, state)
   #define FH_BSTATE_SET(entry, state)
 #endif
