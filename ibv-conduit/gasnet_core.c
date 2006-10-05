74a9f627f5e9ec82fc714c8df96eb3d0972e83c9
Rajesh Nishtala <rajeshn@cs.berkeley.edu>
2006-10-05 00:01:33 +0000

    added missing directories in TREE COLL dir
    i forgot to do cvs up -d :-(
    
    CVSMAIL: CC:

--- ibv-conduit/gasnet_core.c
+++ ibv-conduit/gasnet_core.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core.c,v $
- *     $Date: 2006/10/02 19:09:18 $
- * $Revision: 1.135.4.2 $
+ *     $Date: 2006/10/05 00:01:31 $
+ * $Revision: 1.135.4.3 $
  * Description: GASNet vapi conduit Implementation
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
