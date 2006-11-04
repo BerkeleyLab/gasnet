b666df55716e032cb53c57bf4ae33662e0273b81
Rajesh Nishtala <rajeshn@cs.berkeley.edu>
2006-11-04 02:26:42 +0000

    updated to head CVSMAIL: CC:

--- ibv-conduit/gasnet_extended_fwd.h
+++ ibv-conduit/gasnet_extended_fwd.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_extended_fwd.h,v $
- *     $Date: 2006/08/11 00:53:56 $
- * $Revision: 1.12.2.1 $
+ *     $Date: 2006/11/04 02:26:40 $
+ * $Revision: 1.12.2.2 $
  * Description: GASNet Extended API Header (forward decls)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -15,7 +15,7 @@
 
 #include <firehose_trace.h>
 
-#define GASNET_EXTENDED_VERSION      1.7
+#define GASNET_EXTENDED_VERSION      1.8
 #define GASNET_EXTENDED_VERSION_STR  _STRINGIFY(GASNET_EXTENDED_VERSION)
 #define GASNET_EXTENDED_NAME         VAPI
 #define GASNET_EXTENDED_NAME_STR     _STRINGIFY(GASNET_EXTENDED_NAME)
