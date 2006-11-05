1b8c13afe6ee77aff216996f7c157cc7749cfde8
Paul H. Hargrove <PHHargrove@lbl.gov>
2006-11-05 07:54:29 +0000

    More bits for Rajesh. CVSMAIL: CC:

--- vapi-conduit/gasnet_core_internal.h
+++ vapi-conduit/gasnet_core_internal.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/vapi-conduit/Attic/gasnet_core_internal.h,v $
- *     $Date: 2006/10/28 01:27:14 $
- * $Revision: 1.134.12.11 $
+ *     $Date: 2006/11/05 07:54:25 $
+ * $Revision: 1.134.12.11.2.1 $
  * Description: GASNet vapi conduit header for internal definitions in Core API
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -482,7 +482,6 @@ extern void gasnetc_sndrcv_fini(void);
 extern void gasnetc_sndrcv_init_peer(gasnet_node_t node);
 extern void gasnetc_sndrcv_attach_peer(gasnet_node_t node);
 extern void gasnetc_sndrcv_fini_peer(gasnet_node_t node);
-extern void gasnetc_amrdma_init(int peer_count, gasnet_node_t *peers);
 extern void gasnetc_sndrcv_poll(void);
 extern int gasnetc_RequestGeneric(gasnetc_category_t category,
 				  int dest, gasnet_handler_t handler,
