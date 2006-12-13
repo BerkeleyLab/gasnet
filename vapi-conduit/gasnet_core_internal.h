aa1e2b135f3d9ac85e196d300bc4c9ca80e954d7
Paul H. Hargrove <PHHargrove@lbl.gov>
2006-12-13 00:58:19 +0000

    Connect up the hot-peer selection to actually choose the peers for AMRDMA. CVSMAIL: CC:

--- vapi-conduit/gasnet_core_internal.h
+++ vapi-conduit/gasnet_core_internal.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/vapi-conduit/Attic/gasnet_core_internal.h,v $
- *     $Date: 2006/12/12 21:53:28 $
- * $Revision: 1.145.2.2 $
+ *     $Date: 2006/12/13 00:58:16 $
+ * $Revision: 1.145.2.3 $
  * Description: GASNet vapi conduit header for internal definitions in Core API
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -70,6 +70,7 @@ extern gasneti_atomic_t gasnetc_exit_running;
 /* ------------------------------------------------------------------------------------ */
 #define GASNETC_HANDLER_BASE  1 /* reserve 1-63 for the core API */
 #define _hidx_gasnetc_auxseg_reqh             (GASNETC_HANDLER_BASE+0)
+#define _hidx_gasnetc_amrdma_grant_reqh       (GASNETC_HANDLER_BASE+1)
 /* add new core API handlers here and to the bottom of gasnet_core.c */
 
 /* System-category handlers.
@@ -392,7 +393,7 @@ typedef char gasnetc_amrdma_buf_t[GASNETC_AMRDMA_SZ];
 #define GASNETC_AMRDMA_DEPTH_MAX	32	/* Power-of-2 <= 32 */
 #define GASNETC_DEFAULT_AMRDMA_DEPTH	GASNETC_AMRDMA_DEPTH_MAX
 #define GASNETC_DEFAULT_AMRDMA_LIMIT	GASNETC_AMRDMA_LIMIT_MAX
-#define GASNETC_DEFAULT_AMRDMA_CYCLE	256	/* Number of AM rcvs before triggering hot-peer heuristic */
+#define GASNETC_DEFAULT_AMRDMA_CYCLE	8192	/* 2^i, Number of AM rcvs before hot-peer heuristic */
 
 /* Forward decl */
 struct gasnetc_cep_t_;
@@ -447,6 +448,7 @@ typedef struct {
   gasnetc_memreg_t	amrdma_reg;
   gasneti_lifo_head_t	amrdma_freelist;
   struct {
+    gasneti_weakatomic_val_t max_peers;
     gasneti_weakatomic_t count;
     gasnetc_cep_t	**cep;
   }	  amrdma_rcv;
