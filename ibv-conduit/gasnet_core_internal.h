4c7e37971736015d2767f94fffce0b2205e7147d
Rajesh Nishtala <rajeshn@cs.berkeley.edu>
2007-08-23 20:52:29 +0000

    updated to head CVSMAIL: CC:

--- ibv-conduit/gasnet_core_internal.h
+++ ibv-conduit/gasnet_core_internal.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core_internal.h,v $
- *     $Date: 2007/03/24 23:30:20 $
- * $Revision: 1.89.4.4 $
+ *     $Date: 2007/08/23 20:52:25 $
+ * $Revision: 1.89.4.5 $
  * Description: GASNet vapi conduit header for internal definitions in Core API
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -40,8 +40,6 @@
 
 /*  whether or not to use spin-locking for HSL's */
 #define GASNETC_HSL_SPINLOCK 1
-
-#define GASNETC_CACHE_PAD(SZ) (GASNETC_ALIGNUP(SZ,GASNETI_CACHE_LINE_BYTES)-(SZ))
 
 /* GASNETC_FH_OPTIONAL: whether or not firehose can be switched OFF at runtime */
 /* Enabled by default for DEBUG builds.  For NDEBUG builds, can force at compile time. */
