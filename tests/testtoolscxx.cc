e19726ab9f5733435284498d9619f3e331abe7a7
cvs2svn <upc-devel@lbl.gov>
2007-10-05 19:41:57 +0000

    This commit was manufactured by cvs2svn to create tag 'CVS.BONACHEA_1'.

--- tests/testtoolscxx.cc
+++
@@ -1,9 +0,0 @@
-/*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/tests/testtoolscxx.cc,v $
- *     $Date: 2006/08/11 00:53:49 $
- * $Revision: 1.1.4.1 $
- * Description: General GASNet correctness tests in C++
- * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
- * Terms of use are as specified in license.txt
- */
-
-#include "testtools.c"
