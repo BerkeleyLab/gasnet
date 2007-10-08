1241688a3ea65f7c92dfddaa16ff008d6777a5cc
Paul H. Hargrove <PHHargrove@lbl.gov>
2007-10-08 23:40:55 +0000

    Don't export GASNET_TEAM_ALL.  We keep to the convention that uppercase is a macro.

--- gasnet_coll.h
+++ gasnet_coll.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/gasnet_coll.h,v $
- *     $Date: 2006/12/18 19:31:54 $
- * $Revision: 1.22.6.15 $
+ *     $Date: 2007/10/08 23:40:53 $
+ * $Revision: 1.22.6.16 $
  * Description: GASNet Extended API Collective declarations
  * Copyright 2004, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -164,7 +164,8 @@ struct gasnete_coll_team_t_;
 typedef struct gasnete_coll_team_t_ *gasnete_coll_team_t;
 typedef gasnete_coll_team_t gasnet_team_handle_t;
 /*change this so even the TEAM_ALL has a default team allocated rather than NULL*/
-gasnet_team_handle_t GASNET_TEAM_ALL;
+gasnet_team_handle_t gasnete_coll_team_all;
+#define GASNET_TEAM_ALL gasnete_coll_team_all
 #endif
 
 /*---------------------------------------------------------------------------------*
