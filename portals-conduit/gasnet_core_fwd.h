c4d13e184c5795efaca1ac29020d4ed1443a8aa8
Paul H. Hargrove <PHHargrove@lbl.gov>
2009-05-17 19:40:43 +0000

    Previous commit was missing this file

--- portals-conduit/gasnet_core_fwd.h
+++ portals-conduit/gasnet_core_fwd.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/portals-conduit/Attic/gasnet_core_fwd.h,v $
- *     $Date: 2009/05/15 19:40:12 $
- * $Revision: 1.15.4.1 $
+ *     $Date: 2009/05/17 19:40:43 $
+ * $Revision: 1.15.4.2 $
  * Description: GASNet header for PORTALS conduit core (forward definitions)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -81,6 +81,7 @@
 	CNT(C, LONG_RAR, count)                 \
 	CNT(C, LONG_TMPMD, count)               \
 	GASNETC_FIREHOSE_STATS(CNT,VAL,TIME)	\
+	CNT(C, FRESH_REQRB, count)              \
         TIME(C, FRESH_STALL, stalled time)      \
         TIME(C, REFRESH_STALL, stalled time)    \
         VAL(C, EVENT_REAP, numreaped)
