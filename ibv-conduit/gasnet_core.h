02a8d52b75748bda9e97d3f91770a1c19fa668f7
Filip Blagojevic <fblagojevic@lbl.gov>
2009-05-20 22:17:00 +0000

    SYSV ibv/vapi implementation. Also PARSYNC used for atomic operations with SYSV. Tested on pcp-f-2 only.
    CVSMAIL: CC:

--- ibv-conduit/gasnet_core.h
+++ ibv-conduit/gasnet_core.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core.h,v $
- *     $Date: 2007/04/16 18:35:10 $
- * $Revision: 1.54 $
+ *     $Date: 2009/05/20 22:16:59 $
+ * $Revision: 1.54.24.1 $
  * Description: GASNet header for vapi conduit core
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -183,8 +183,18 @@ void gasnetc_counter_wait(gasnetc_counter_t *counter, int handler_context) {
 #define GASNETC_MAX_ARGS_USER	16
 #define GASNETC_MAX_ARGS_EXTRA	1	/* For flow-control info */
 #define GASNETC_MAX_ARGS	(GASNETC_MAX_ARGS_USER + GASNETC_MAX_ARGS_EXTRA)
+#if GASNET_SYSV
+  /* HACK: set max medium to size known to be smaller than
+   * GASNETI_SYSVNET_MAX_PAYLOAD (which isn't visible to this file yet) 
+   * - TODO: fix sysV max medium to be automatically defined to equal
+   *   GASNETI_SYSVNET_MAX_PAYLOAD, and make that a bigger value!
+   */
+  #define GASNETC_MAX_MEDIUM 65536 //3984
+  //#define GASNETC_MAX_MEDIUM 3984
+#else
 #define GASNETC_MAX_MEDIUM	\
 		(GASNETC_BUFSZ - GASNETI_ALIGNUP(GASNETC_MEDIUM_HDRSZ + 4*GASNETC_MAX_ARGS, 8))
+#endif
 #define GASNETC_MAX_LONG_REQ	(0x7fffffff)
 #define GASNETC_MAX_PACKEDLONG	(GASNETC_BUFSZ - GASNETC_LONG_HDRSZ - 4*GASNETC_MAX_ARGS)
 #if GASNETC_PIN_SEGMENT
