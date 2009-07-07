33637f9a7cb745981a30ede5957b78fde6c7bdc1
Rajesh Nishtala <rajeshn@cs.berkeley.edu>
2009-07-07 22:17:38 +0000

    accidentally commented in a headerfile that doesn't exist CVSMAIL: CC:

--- extended-ref/gasnet_extended.h
+++ extended-ref/gasnet_extended.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/extended-ref/gasnet_extended.h,v $
- *     $Date: 2009/07/07 20:42:38 $
- * $Revision: 1.41.20.5 $
+ *     $Date: 2009/07/07 22:17:38 $
+ * $Revision: 1.41.20.6 $
  * Description: GASNet Extended API Header
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -18,7 +18,7 @@
 #include <gasnet_extended_help.h>
 #include <gasnet_coll.h>
 //#include <gasnet_coll_autotune.h>
-#include <gasnet_coll_internal.h>
+//#include <gasnet_coll_internal.h>
 GASNETI_BEGIN_EXTERNC
 
 /*  TODO: add debug code to enforce restrictions on SEQ and PARSYNC config */
