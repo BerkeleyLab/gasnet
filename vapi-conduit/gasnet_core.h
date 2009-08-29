acbc8d6b689a7c2ec1252264e7157c6f8709d52c
Paul H. Hargrove <PHHargrove@lbl.gov>
2009-08-29 01:59:04 +0000

    Vapi/ibv conduit should observe AMSYSV limit on Medium size. There was once a comment here stating that we didn't need this because we "knew" that the conduit-specific MAX_MEDIUM was smaller than AMSYSV's.

--- vapi-conduit/gasnet_core.h
+++ vapi-conduit/gasnet_core.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/vapi-conduit/Attic/gasnet_core.h,v $
- *     $Date: 2009/08/27 00:22:47 $
- * $Revision: 1.54.24.5 $
+ *     $Date: 2009/08/29 01:59:03 $
+ * $Revision: 1.54.24.6 $
  * Description: GASNet header for vapi conduit core
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -184,8 +184,13 @@ void gasnetc_counter_wait(gasnetc_counter_t *counter, int handler_context) {
 #define GASNETC_MAX_ARGS_EXTRA	1	/* For flow-control info */
 #define GASNETC_MAX_ARGS	(GASNETC_MAX_ARGS_USER + GASNETC_MAX_ARGS_EXTRA)
 
-#define GASNETC_MAX_MEDIUM	\
+#define GASNETC_MAX_MEDIUM_	\
                (GASNETC_BUFSZ - GASNETI_ALIGNUP_NOASSERT(GASNETC_MEDIUM_HDRSZ + 4*GASNETC_MAX_ARGS, 8))
+#if GASNET_SYSV
+  #define GASNETC_MAX_MEDIUM	MIN(GASNETC_MAX_MEDIUM_, GASNETI_MAX_MEDIUM_SYSV)
+#else
+  #define GASNETC_MAX_MEDIUM	GASNETC_MAX_MEDIUM_
+#endif
 #define GASNETC_MAX_LONG_REQ	(0x7fffffff)
 #define GASNETC_MAX_PACKEDLONG	(GASNETC_BUFSZ - GASNETC_LONG_HDRSZ - 4*GASNETC_MAX_ARGS)
 #if GASNETC_PIN_SEGMENT
