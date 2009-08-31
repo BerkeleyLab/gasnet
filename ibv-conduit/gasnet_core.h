420ad08ccd3d56cd6a44a14c7e06d1736e771f91
Paul H. Hargrove <PHHargrove@lbl.gov>
2009-08-31 23:35:00 +0000

    BIG rename SYSV->PSHM

--- ibv-conduit/gasnet_core.h
+++ ibv-conduit/gasnet_core.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core.h,v $
- *     $Date: 2009/08/29 01:59:03 $
- * $Revision: 1.54.24.6 $
+ *     $Date: 2009/08/31 23:34:58 $
+ * $Revision: 1.54.24.7 $
  * Description: GASNet header for vapi conduit core
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -186,8 +186,8 @@ void gasnetc_counter_wait(gasnetc_counter_t *counter, int handler_context) {
 
 #define GASNETC_MAX_MEDIUM_	\
                (GASNETC_BUFSZ - GASNETI_ALIGNUP_NOASSERT(GASNETC_MEDIUM_HDRSZ + 4*GASNETC_MAX_ARGS, 8))
-#if GASNET_SYSV
-  #define GASNETC_MAX_MEDIUM	MIN(GASNETC_MAX_MEDIUM_, GASNETI_MAX_MEDIUM_SYSV)
+#if GASNET_PSHM
+  #define GASNETC_MAX_MEDIUM	MIN(GASNETC_MAX_MEDIUM_, GASNETI_MAX_MEDIUM_PSHM)
 #else
   #define GASNETC_MAX_MEDIUM	GASNETC_MAX_MEDIUM_
 #endif
