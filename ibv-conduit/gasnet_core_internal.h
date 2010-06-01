51b826d9965caaef394a9e3dc764266abc35a29f
Paul H. Hargrove <PHHargrove@lbl.gov>
2010-06-01 22:53:20 +0000

    Make a distinction between the number of QPs per peer, and the number used "normally".
    This is in preparation of the flow-control work which is expected to add one or
    more QPs per peer that are dedicated only to AM Request traffic (as well as the
    possibility that AFTER the QP array is allocated we might determine that SRQ
    will NOT be used and thus we'll have unused spaces in the table).
    
    This adds nothing to the length of the critical paths (where we've just replaced
    one variable by a different one) and only mildly complicates the initialization
    code which must now skip over any unused slots.  I'll need to rework some of
    that init code to setup two distinct "classes" of QP for the SRQ flow control, but
    have not progressed far enough in the design to start those modifications yet.
    There will be resource calculation changes as well, since the hca struct fields
    that count qps have not be modified to make any normal-vs-other distinction yet
    (they just count "non-NULL" at the moment).
    
    Note that this was tested for both IBV and VAPI by manually setting
       gasnetc_num_qps = gasnetc_normal_qps + 1
    early in the init code, and running all the standard GASNet tests.

--- ibv-conduit/gasnet_core_internal.h
+++ ibv-conduit/gasnet_core_internal.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core_internal.h,v $
- *     $Date: 2010/05/26 03:54:35 $
- * $Revision: 1.160.10.4 $
+ *     $Date: 2010/06/01 22:53:19 $
+ * $Revision: 1.160.10.5 $
  * Description: GASNet vapi conduit header for internal definitions in Core API
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -593,7 +593,8 @@ extern int		gasnetc_am_oust_pp;
 extern int		gasnetc_bbuf_limit;
 extern int		gasnetc_use_rcv_thread;
 extern int		gasnetc_am_credits_slack;
-extern int		gasnetc_num_qps;
+extern int		gasnetc_num_qps;    /* Number of QPs per node in gasnetc_ceps[] */
+extern int		gasnetc_normal_qps; /* How many of those (the leading ones) for normal use? */
 extern size_t		gasnetc_packedlong_limit;
 extern size_t		gasnetc_inline_limit;
 extern size_t		gasnetc_bounce_limit;
