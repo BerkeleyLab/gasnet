c71c8b5c6f109be7e330193ed605c5200ff71c9d
Paul H. Hargrove <PHHargrove@lbl.gov>
2010-08-03 03:43:41 +0000

    Merge HEAD to IBV_SRQ_branch

--- portals-conduit/gasnet_core_fwd.h
+++ portals-conduit/gasnet_core_fwd.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/portals-conduit/Attic/gasnet_core_fwd.h,v $
- *     $Date: 2010/06/28 04:57:08 $
- * $Revision: 1.23.2.1 $
+ *     $Date: 2010/08/03 03:43:37 $
+ * $Revision: 1.23.2.2 $
  * Description: GASNet header for PORTALS conduit core (forward definitions)
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -35,7 +35,7 @@
 
   /* GASNET_PSHM defined 1 if this conduit supports PSHM. leave undefined otherwise. */
 #if GASNETI_PSHM_ENABLED
-/* #define GASNET_PSHM 1 */
+ #define GASNET_PSHM 1
 #endif
 
   /*  defined to be 1 if gasnet_init guarantees that the remote-access memory segment will be aligned  */
