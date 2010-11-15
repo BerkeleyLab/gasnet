325ac7182039c027f267d9dc2bdceb231f304f60
Filip Blagojevic <fblagojevic@lbl.gov>
2010-11-15 23:16:10 +0000

    As Paul suggested, the second argument of ftok() should always be greater than 0. Therefore, for the second ftok() argument we will use "gasneti_pshm_mynode+1" instead of "gasneti_pshm_mynode".
    CVSMAIL: CC:

--- gasnet_pshm.c
+++ gasnet_pshm.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/gasnet_pshm.c,v $
- *     $Date: 2010/09/16 03:51:35 $
- * $Revision: 1.27 $
+ *     $Date: 2010/11/15 23:16:10 $
+ * $Revision: 1.27.4.1 $
  * Description: GASNet infrastructure for shared memory communications
  * Copyright 2009, E. O. Lawrence Berekely National Laboratory
  * Terms of use are as specified in license.txt
@@ -128,8 +128,11 @@ void *gasneti_pshm_init(gasneti_bootstrapExchangefn_t exchangefn, size_t aux_sz)
     unsigned int *exchg;
     unsigned int tmp_sysvkey;
     
-    /* Each node gets the key for it's own memory region */
-    tmp_sysvkey = gasneti_pshm_makekey(gasneti_pshm_mynode);
+    /* Each node gets the key for it's own memory region.
+     * We pass mynode+1 to makekey since this argument will 
+     * eventually be passed to ftok(), and therefore has to 
+     * be larger than 0 */
+    tmp_sysvkey = gasneti_pshm_makekey(gasneti_pshm_mynode + 1);
 
     /* The keys are exchanged */
     exchg = gasneti_malloc((gasneti_nodes)*sizeof(unsigned int));
@@ -142,7 +145,7 @@ void *gasneti_pshm_init(gasneti_bootstrapExchangefn_t exchangefn, size_t aux_sz)
     
     /* PSHM rank 0 gets the key for vnet region */
     if (gasneti_pshm_mynode==0) {
-        tmp_sysvkey = gasneti_pshm_makekey(gasneti_pshm_nodes);
+        tmp_sysvkey = gasneti_pshm_makekey(gasneti_pshm_nodes + 1);
     }
     /* vnet key is broadcasted */
     (*exchangefn)(&tmp_sysvkey, sizeof(unsigned int), exchg);
