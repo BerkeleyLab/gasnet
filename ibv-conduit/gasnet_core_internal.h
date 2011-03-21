2d01acd0d90f8c8bb43b056c9bc2201906733f26
Paul H. Hargrove <PHHargrove@lbl.gov>
2011-03-21 20:55:31 +0000

    Merge gasnetc_connect_{static,init_dynamic}() into gasnetc_connect_init(). Also splits gasnetc_xrc_init() out of gasnetc_connect_init(). This results in earlier setup of dynamic connection support with the goal of perhaps someday reimplementing over IB the bootstrap Barrier and/or Exchange for operations issued after the connection subsystem is initialized. This commit also implements the "half hearted" initialization of dynamic connections for those nodes that are already fully connected (as suggested in my previous commit). Also the logging of connection information is slightly improved with regards to what is happening with static vs dynamic connections. Finally, this commit fixes a couple typos in comments and error messages.

--- ibv-conduit/gasnet_core_internal.h
+++ ibv-conduit/gasnet_core_internal.h
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core_internal.h,v $
- *     $Date: 2011/03/17 23:48:01 $
- * $Revision: 1.205.2.12 $
+ *     $Date: 2011/03/21 20:55:29 $
+ * $Revision: 1.205.2.13 $
  * Description: GASNet vapi conduit header for internal definitions in Core API
  * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
  * Terms of use are as specified in license.txt
@@ -631,9 +631,10 @@ typedef struct {
 } gasnetc_port_info_t;
 
 /* Routines in gasnet_core_connect.c */
-extern int gasnetc_connect_static(void);
+#if GASNETC_IBV_XRC
+extern int gasnetc_xrc_init(void);
+#endif
 extern int gasnetc_connect_init(void);
-extern int gasnetc_connect_init_dynamic(void);
 extern int gasnetc_connect_fini(void);
 extern gasnetc_cep_t *gasnetc_connect_to(gasnet_node_t node);
 extern void gasnetc_conn_implied_ack(gasnet_node_t node);
