27475accd56d45c8eb7c0afb882e0b93e53f748a
Paul H. Hargrove <PHHargrove@lbl.gov>
2012-01-04 23:14:01 +0000

    Fix a typo in a comment

--- ibv-conduit/gasnet_core_connect.c
+++ ibv-conduit/gasnet_core_connect.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/ibv-conduit/gasnet_core_connect.c,v $
- *     $Date: 2012/01/04 19:01:16 $
- * $Revision: 1.65.2.1 $
+ *     $Date: 2012/01/04 23:14:01 $
+ * $Revision: 1.65.2.2 $
  * Description: Connection management code
  * Copyright 2011, E. O. Lawrence Berekely National Laboratory
  * Terms of use are as specified in license.txt
@@ -2414,7 +2414,7 @@ static gasnet_node_t dump_conn_prev;
 static void
 dump_conn_write(int fd, const char *buf, size_t len)
 {
-  /* TODO: loop w/ retry on whort writes? */
+  /* TODO: loop w/ retry on short writes? */
   ssize_t rc = write(fd, buf, len);
   if_pf (rc != len) {
     gasneti_fatalerror("Write to connection file failed or truncated: rc=%ld errno=%s(%i)",
