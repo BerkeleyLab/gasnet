4f9960e102a50d6e051c6cb4ca7af6022c462ca7
Paul H. Hargrove <PHHargrove@lbl.gov>
2012-04-06 21:19:21 +0000

    Clone extended API reference implementation into pami-conduit Other than the naming in ident strings, this is still identical to extened-ref

---
+++ pami-conduit/gasnet_extended_fwd.h
@@ -0,0 +1,39 @@
+/*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/pami-conduit/gasnet_extended_fwd.h,v $
+ *     $Date: 2012/04/06 21:19:17 $
+ * $Revision: 1.1.2.1 $
+ * Description: GASNet Extended API Header (forward decls)
+ * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
+ * Copyright 2012, Lawrence Berkeley National Laboratory
+ * Terms of use are as specified in license.txt
+ */
+
+#ifndef _IN_GASNET_H
+  #error This file is not meant to be included directly- clients should include gasnet.h
+#endif
+
+#ifndef _GASNET_EXTENDED_FWD_H
+#define _GASNET_EXTENDED_FWD_H
+
+#define GASNET_EXTENDED_VERSION      0.1
+#define GASNET_EXTENDED_VERSION_STR  _STRINGIFY(GASNET_EXTENDED_VERSION)
+#define GASNET_EXTENDED_NAME         PAMI
+#define GASNET_EXTENDED_NAME_STR     _STRINGIFY(GASNET_EXTENDED_NAME)
+
+
+#define _GASNET_HANDLE_T
+/*  an opaque type representing a non-blocking operation in-progress initiated using the extended API */
+struct _gasnete_op_t;
+typedef struct _gasnete_op_t *gasnet_handle_t;
+#define GASNET_INVALID_HANDLE ((gasnet_handle_t)0)
+#define GASNETI_EOP_IS_HANDLE 1
+
+  /* this can be used to add statistical collection values 
+     specific to the extended API implementation (see gasnet_help.h) */
+#define GASNETE_CONDUIT_STATS(CNT,VAL,TIME)  \
+        GASNETI_VIS_STATS(CNT,VAL,TIME)      \
+        GASNETI_COLL_STATS(CNT,VAL,TIME)     \
+        CNT(C, DYNAMIC_THREADLOOKUP, cnt)    
+
+
+#endif
+
