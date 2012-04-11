06cbfa66da8f00d228ac806e3bca92b824d2ed62
Paul H. Hargrove <PHHargrove@lbl.gov>
2012-04-11 18:48:43 +0000

    Implement SEGMENT_EVERYTHING support for pami-conduit. Everything worked on the second try - only initializing the segment table was missing.

--- pami-conduit/gasnet_core.c
+++ pami-conduit/gasnet_core.c
@@ -1,6 +1,6 @@
 /*   $Source: /Users/kamil/work/gasnet-cvs2/gasnet/pami-conduit/gasnet_core.c,v $
- *     $Date: 2012/04/11 00:14:37 $
- * $Revision: 1.1.2.47 $
+ *     $Date: 2012/04/11 18:48:43 $
+ * $Revision: 1.1.2.48 $
  * Description: GASNet PAMI conduit Implementation
  * Copyright 2012, Lawrence Berkeley National Laboratory
  * Terms of use are as specified in license.txt
@@ -436,10 +436,16 @@ extern int gasnetc_attach(gasnet_handlerentry_t *table, int numentries,
       gasneti_assert(segsize % GASNET_PAGESIZE == 0);
     }
   #else
-    /* GASNET_SEGMENT_EVERYTHING */
+  { /* GASNET_SEGMENT_EVERYTHING */
+    gasnet_node_t i;
+    for (i=0; i<gasneti_nodes; i++) {
+      gasneti_seginfo[i].addr = (void *)0;
+      gasneti_seginfo[i].size = (uintptr_t)-1;
+    }
     segbase = (void *)0;
     segsize = (uintptr_t)-1;
     /* (###) add any code here needed to setup GASNET_SEGMENT_EVERYTHING support */
+  }
   #endif
 
   /* ------------------------------------------------------------------------------------ */
