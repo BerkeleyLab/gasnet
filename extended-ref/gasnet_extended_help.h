6a3500476ad47e56f95e80b8a6d33e67dd67d91d
Paul H. Hargrove <PHHargrove@lbl.gov>
2016-06-14 12:57:53 -0700

    Fix some issues in CHECK{ZEROSZ,PSHM} macros

--- extended-ref/gasnet_extended_help.h
+++ extended-ref/gasnet_extended_help.h
@@ -457,15 +457,18 @@ typedef union {
 /* helper macros */
 #define _GASNETI_RETURN_V  return
 #define _GASNETI_RETURN_H  return GASNET_INVALID_HANDLE
+// TODO-EX: merge next two w/ previous two
+#define _GASNETI_RETURNEX_V  return
+#define _GASNETI_RETURNEX_H  return GASNETEX_INVALID_HANDLE
 #define GASNETI_CHECKZEROSZ_GET(variety, rt) do {            \
     if_pf (nbytes == 0) {                                    \
       GASNETI_TRACE_GET_LOCAL(variety,dest,rank,src,nbytes); \
-      _GASNETI_RETURN_##rt;                                  \
+      _GASNETI_RETURNEX_##rt;                                \
     } } while(0)
 #define GASNETI_CHECKZEROSZ_PUT(variety, rt) do {            \
     if_pf (nbytes == 0) {                                    \
       GASNETI_TRACE_PUT_LOCAL(variety,rank,dest,src,nbytes); \
-      _GASNETI_RETURN_##rt;                                  \
+      _GASNETI_RETURNEX_##rt;                                \
     } } while(0)
 #define GASNETI_CHECKZEROSZ_MEMSET(variety, rt) do {            \
     if_pf (nbytes == 0) {                                       \
@@ -482,13 +485,13 @@ typedef union {
     if (gasneti_pshm_in_supernode(rank)) {      \
       GASNETE_FAST_MEMCPY(dest, gasneti_pshm_addr2local(rank, src), nbytes); \
       gasnete_loopbackget_memsync();            \
-      _GASNETI_RETURN_##rt;                     \
+      _GASNETI_RETURNEX_##rt;                   \
     }} while(0)
   #define GASNETI_CHECKPSHM_PUT(rt) do { \
     if (gasneti_pshm_in_supernode(rank)) {      \
       GASNETE_FAST_MEMCPY(gasneti_pshm_addr2local(rank, dest), src, nbytes); \
       gasnete_loopbackput_memsync();            \
-      _GASNETI_RETURN_##rt;                     \
+      _GASNETI_RETURNEX_##rt;                   \
     }} while(0)
   #define GASNETI_CHECKPSHM_GETVAL() do {     \
     if (gasneti_pshm_in_supernode(node)) {      \
@@ -507,8 +510,8 @@ typedef union {
       _GASNETI_RETURN_##rt;                     \
     }} while(0)
 #else
-  #define GASNETI_CHECKPSHM_GET(align, rt) ((void)0)
-  #define GASNETI_CHECKPSHM_PUT(align, rt) ((void)0)
+  #define GASNETI_CHECKPSHM_GET(rt)        ((void)0)
+  #define GASNETI_CHECKPSHM_PUT(rt)        ((void)0)
   #define GASNETI_CHECKPSHM_GETVAL()       ((void)0)
   #define GASNETI_CHECKPSHM_PUTVAL(rt)     ((void)0)
   #define GASNETI_CHECKPSHM_MEMSET(rt)     ((void)0)
