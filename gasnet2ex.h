21fb154adf92f93f7c08442b8ede85f88b8bcc31
Paul H. Hargrove <PHHargrove@lbl.gov>
2016-06-19 23:15:39 -0700

    Fix a typo in gasnet_get_nb_val

--- gasnet2ex.h
+++ gasnet2ex.h
@@ -336,7 +336,7 @@ gasnet_valget_handle_t gasnet_get_nb_val(gasnet_node_t node, void *src, size_t n
 {
   gasnet_valget_handle_t result = (gasnet_valget_handle_t)malloc(sizeof(struct g2ex_valget_handle_s));
 #ifdef PLATFORM_ARCH_BIG_ENDIAN
-  void *dest = (void*)((uinptr_t)&(result->v) + sizeof(gasnet_register_value_t) - nbytes);
+  void *dest = (void*)((uintptr_t)&(result->v) + sizeof(gasnet_register_value_t) - nbytes);
 #else /* little-endian */
   void *dest = &result->v;
 #endif
