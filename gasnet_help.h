a082157d0734d2a4a0708badfab5d43b8d4b40f6
Paul H. Hargrove <PHHargrove@lbl.gov>
2016-06-20 06:25:19 +0000

    Fix C99 non-conformance

--- gasnet_help.h
+++ gasnet_help.h
@@ -884,7 +884,7 @@ GASNETI_PUREP(gasneti_pshm_addr2local)
    one" relative to the canonical example(s).
  */
 
-#define GASNETI_AMNUMARGS(...) GASNETI_AMNUMARGS_(__VA_ARGS__,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
+#define GASNETI_AMNUMARGS(...) GASNETI_AMNUMARGS_(__VA_ARGS__,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,0)
 #define GASNETI_AMNUMARGS_(_0,_1,_2,_3,_4,_5,_6,_7,_8,_9,_10,_11,_12,_13,_14,_15,_16,N,...) N
 #define GASNETI_AMVA(_stem,...) _CONCAT(gasnetex_AM##_stem,GASNETI_AMNUMARGS(__VA_ARGS__))
 
