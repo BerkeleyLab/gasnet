af96438c97682e5f36290bf3be4d30b23ce7bd3a
Paul H. Hargrove <PHHargrove@lbl.gov>
2016-06-19 18:14:15 -0700

    Add recently-removed types to gasnet2ex.h

--- gasnet2ex.h
+++ gasnet2ex.h
@@ -13,6 +13,17 @@ GASNETI_BEGIN_EXTERNC
 
 /* ------------------------------------------------------------------------------------ */
 /*
+  Base types
+  ==========
+*/
+
+typedef gasnetex_rank_t gasnet_node_t;
+typedef gasnetex_token_t gasnet_token_t;
+typedef gasnetex_handler_t gasnet_handler_t;
+typedef gasnetex_handlerarg_t gasnet_handlerarg_t;
+
+/* ------------------------------------------------------------------------------------ */
+/*
   Active Message Request/Reply Functions
   ======================================
 */
