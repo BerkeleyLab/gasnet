bc09fd5408c27f866f36de6984f39679dfc6817b
bonachea <danbonachea@gmail.com>
2016-07-04 20:13:08 -0700

    Add configure logic to enable C99 mode
    Some compilers support C99 by default, others need one or more flags
    (including older versions of gcc). This logic is likely to grow as we
    re-test on various systems. Compliance flags belong in MISC_CFLAGS and
    MISC_CPPFLAGS, to ensure we use them regardless of opt/debug level.
    MISC_CPPFLAGS are also now added to libgasnet and libgasnet-tools
    builds, and the default value of MPI_CFLAGS (they really should have
    been there all along).
    
    Enabling -std=c99 on GCC also disables most of POSIX (the part outside
    C99), so we now additionally need _GNU_SOURCE (at least on Linux and
    Cygwin) to bring it back. This is actually a good thing because it also
    maximizes the system header declarations we may configure detect and
    use.
    
    Fix misc -pedantic warnings.

--- other/firehose/firehose.c
+++ other/firehose/firehose.c
@@ -423,7 +423,7 @@ firehose_remote_pin(gasnetex_rank_t node, uintptr_t addr, size_t len,
 		}
 		else {
 			GASNETI_TRACE_PRINTF(C, 
-			    ("Firehose callback req=%p", req));
+			    ("Firehose callback req=%p", (void *)req));
 			callback(context, req, 1);
 		}
 	}
