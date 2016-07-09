adc2f4ddf3ac398728c0da4ede03b6c150433748
Paul H. Hargrove <PHHargrove@lbl.gov>
2016-07-09 00:57:12 -0700

    fixup: Factor handle management out of gasnet_extended.c
    Relocate some internal-use-only macros out of a public header and into
    an internal one.  Thanks for Dan to pointing out that I'd picked a bad
    "parking spot" for these (which belong in a gasnet_extended_internal.h
    when that is brought back).  However, if they never do get moved, this
    is a far more appropriate place for these to end up.

--- psm-conduit/gasnet_core_internal.h
+++ psm-conduit/gasnet_core_internal.h
@@ -209,4 +209,20 @@ void gasnete_post_pending_mq_ops(void);
 /* This routine assumes that psm2_poll() has recently been called. */
 void gasnete_finish_mq_reqs(void);
 
+
+
+#if GASNETE_EOP_COUNTED && !GASNETE_AMREF_USE_MARKDONE
+  #define PSM_PACK_EOP_DONE(_eop)         (&(_eop)->completed_cnt)
+  #define PSM_PACK_IOP_DONE(_iop,_getput) (&(_iop)->completed_##_getput##_cnt)
+  #define PSM_MARK_DONE(_ptr,_isget)      gasneti_weakatomic_increment((gasneti_weakatomic_t *)(_ptr), \
+                                                                   (_isget) ? GASNETI_ATOMIC_REL : 0)
+#else
+  #define PSM_PACK_EOP_DONE(_eop)         (_eop)
+  #define PSM_PACK_IOP_DONE(_iop,_getput) (_iop)
+  #define PSM_MARK_DONE(_ptr,_isget)      do {                                                      \
+                                         if (_isget) gasneti_sync_writes();                     \
+                                         gasnete_op_markdone((gasnete_op_t *)(_ptr), (_isget)); \
+                                      } while (0)
+#endif
+
 #endif
