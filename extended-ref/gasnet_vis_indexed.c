evidence/axiom-evi-gasnet

Bug 3362: VIS_REMOTECONTIG SEGVs with PSHM
Add explicit assertions to verify the handle validity invariants that
were voicelessly violated in this bug.

Add a GASNETI_SUPERNODE_LOCAL macro to query supernode locality,
independent of PSHM setting.

Use GASNETI_SUPERNODE_LOCAL to force selection of individual put/get
algorithm for VIS ops on supernode peers.

extended-ref/gasnet_vis_indexed.c
@@ -219,6 +219,7 @@ gasnet_handle_t gasnete_puti_gather(gasnete_synctype_t synctype,
     gasnete_addrlist_pack(srccount, srclist, srclen, packedbuf, 0, (size_t)-1);
     visop->type = GASNETI_VIS_CAT_PUTI_GATHER;
     visop->handle = gasnete_put_nb_bulk(dstnode, dstlist[0], packedbuf, nbytes GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNET_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 0);
   }
 }
@@ -252,6 +253,7 @@ gasnet_handle_t gasnete_geti_scatter(gasnete_synctype_t synctype,
     visop->count = dstcount;
     visop->len = dstlen;
     visop->handle = gasnete_get_nb_bulk(packedbuf, srcnode, srclist[0], nbytes GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNET_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 1);
   }
 }
@@ -651,9 +653,9 @@ extern gasnet_handle_t gasnete_puti(gasnete_synctype_t synctype,
                                    size_t srccount, void * const srclist[], size_t srclen GASNETE_THREAD_FARG) {
   gasneti_assert(gasnete_vis_isinit);
   /* catch silly degenerate cases */
-  if_pf (dstcount + srccount <= 2 ||  /* empty or fully contiguous */
-         dstnode == gasneti_mynode) { /* purely local */ 
-    if (dstcount == 0) return GASNET_INVALID_HANDLE;
+  if (dstcount + srccount <= 2 ||  /* empty or fully contiguous */
+      GASNETI_SUPERNODE_LOCAL(dstnode)) { /* purely local */ 
+    if_pf (dstcount == 0) return GASNET_INVALID_HANDLE;
     else return gasnete_puti_ref_indiv(synctype,dstnode,dstcount,dstlist,dstlen,srccount,srclist,srclen GASNETE_THREAD_PASS);
   }
 
@@ -691,9 +693,9 @@ extern gasnet_handle_t gasnete_geti(gasnete_synctype_t synctype,
                                    size_t srccount, void * const srclist[], size_t srclen GASNETE_THREAD_FARG) {
   gasneti_assert(gasnete_vis_isinit);
   /* catch silly degenerate cases */
-  if_pf (dstcount + srccount <= 2 ||  /* empty or fully contiguous */
-         srcnode == gasneti_mynode) { /* purely local */ 
-    if (dstcount == 0) return GASNET_INVALID_HANDLE;
+  if (dstcount + srccount <= 2 ||  /* empty or fully contiguous */
+      GASNETI_SUPERNODE_LOCAL(srcnode)) { /* purely local */ 
+    if_pf (dstcount == 0) return GASNET_INVALID_HANDLE;
     else return gasnete_geti_ref_indiv(synctype,dstcount,dstlist,dstlen,srcnode,srccount,srclist,srclen GASNETE_THREAD_PASS);
   }
 

extended-ref/gasnet_vis_strided.c
@@ -677,6 +677,7 @@ gasnet_handle_t gasnete_puts_gather(gasnete_strided_stats_t const *stats, gasnet
     gasnete_strided_pack_all(srcaddr, srcstrides, count, stridelevels, packedbuf);
     visop->type = GASNETI_VIS_CAT_PUTS_GATHER;
     visop->handle = gasnete_put_nb_bulk(dstnode, dstaddr, packedbuf, nbytes GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNET_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 0);
   }
 }
@@ -715,6 +716,7 @@ gasnet_handle_t gasnete_gets_scatter(gasnete_strided_stats_t const *stats, gasne
     visop->addr = dstaddr;
     visop->len = stridelevels;
     visop->handle = gasnete_get_nb_bulk(packedbuf, srcnode, srcaddr, nbytes GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNET_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 1);
   }
 }
@@ -1154,8 +1156,8 @@ extern gasnet_handle_t gasnete_puts(gasnete_synctype_t synctype,
   /* catch silly degenerate cases */
   if_pf (stats.totalsz == 0) /* empty */
     return GASNET_INVALID_HANDLE;
-  if_pf (dstnode == gasneti_mynode || /* purely local */ 
-         stats.dualcontiguity == stridelevels) {/* fully contiguous */
+  if (GASNETI_SUPERNODE_LOCAL(dstnode) || /* purely local */ 
+      stats.dualcontiguity == stridelevels) {/* fully contiguous */
     return gasnete_puts_ref_indiv(&stats,synctype,dstnode,dstaddr,dststrides,srcaddr,srcstrides,count,stridelevels GASNETE_THREAD_PASS);
   }
 
@@ -1200,8 +1202,8 @@ extern gasnet_handle_t gasnete_gets(gasnete_synctype_t synctype,
   /* catch silly degenerate cases */
   if_pf (stats.totalsz == 0) /* empty */
     return GASNET_INVALID_HANDLE;
-  if_pf (srcnode == gasneti_mynode || /* purely local */ 
-         stats.dualcontiguity == stridelevels) {/* fully contiguous */
+  if (GASNETI_SUPERNODE_LOCAL(srcnode) || /* purely local */ 
+      stats.dualcontiguity == stridelevels) {/* fully contiguous */
     return gasnete_gets_ref_indiv(&stats,synctype,dstaddr,dststrides,srcnode,srcaddr,srcstrides,count,stridelevels GASNETE_THREAD_PASS);
   }
 

extended-ref/gasnet_vis_vector.c
@@ -232,6 +232,7 @@ gasnet_handle_t gasnete_putv_gather(gasnete_synctype_t synctype,
     gasnete_memvec_pack(srccount, srclist, packedbuf, 0, (size_t)-1);
     visop->type = GASNETI_VIS_CAT_PUTV_GATHER;
     visop->handle = gasnete_put_nb_bulk(dstnode, dstlist[0].addr, packedbuf, nbytes GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNET_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 0);
   }
 }
@@ -264,6 +265,7 @@ gasnet_handle_t gasnete_getv_scatter(gasnete_synctype_t synctype,
     visop->type = GASNETI_VIS_CAT_GETV_SCATTER;
     visop->count = dstcount;
     visop->handle = gasnete_get_nb_bulk(packedbuf, srcnode, srclist[0].addr, nbytes GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNET_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 1);
   }
 }
@@ -646,8 +648,8 @@ extern gasnet_handle_t gasnete_putv(gasnete_synctype_t synctype,
   /* catch silly degenerate cases */
   if_pf (dstcount == 0 || srccount == 0) /* empty (may miss some cases) */
     return GASNET_INVALID_HANDLE; 
-  if_pf (dstcount + srccount <= 2 ||  /* fully contiguous */
-         dstnode == gasneti_mynode) { /* purely local */ 
+  if (dstcount + srccount <= 2 ||  /* fully contiguous */
+      GASNETI_SUPERNODE_LOCAL(dstnode)) { /* purely local */ 
     return gasnete_putv_ref_indiv(synctype,dstnode,dstcount,dstlist,srccount,srclist GASNETE_THREAD_PASS);
   }
 
@@ -672,8 +674,8 @@ extern gasnet_handle_t gasnete_getv(gasnete_synctype_t synctype,
   /* catch silly degenerate cases */
   if_pf (dstcount == 0 || srccount == 0) /* empty (may miss some cases) */
     return GASNET_INVALID_HANDLE; 
-  if_pf (dstcount + srccount <= 2 ||  /* fully contiguous */
-         srcnode == gasneti_mynode) { /* purely local */ 
+  if (dstcount + srccount <= 2 ||  /* fully contiguous */
+      GASNETI_SUPERNODE_LOCAL(srcnode)) { /* purely local */ 
     return gasnete_getv_ref_indiv(synctype,dstcount,dstlist,srcnode,srccount,srclist GASNETE_THREAD_PASS);
   }
 

BerkeleyLab/gasnet

(cherry picked from commit 29b615a51c21e5e2e3f9868708204c10cd24227e)
Note the assertion added to gasnete_try_syncnb():gasnet_extended.c on
develop was moved to gasnete_test():gasnet_handle.c and
gasnete_handle_check():gasnet_handle_internal.h.

extended-ref/gasnet_vis_indexed.c
@@ -219,6 +219,7 @@ gasnetex_handle_t gasnete_puti_gather(gasnete_synctype_t synctype,
     gasnete_addrlist_pack(srccount, srclist, srclen, packedbuf, 0, (size_t)-1);
     visop->type = GASNETI_VIS_CAT_PUTI_GATHER;
     visop->handle = gasnete_put_nb(NULL, dstnode, dstlist[0], packedbuf, nbytes, GASNETEX_EVENT_DEFER, 0 GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNETEX_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 0);
   }
 }
@@ -252,6 +253,7 @@ gasnetex_handle_t gasnete_geti_scatter(gasnete_synctype_t synctype,
     visop->count = dstcount;
     visop->len = dstlen;
     visop->handle = gasnete_get_nb(NULL, packedbuf, srcnode, srclist[0], nbytes, 0 GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNETEX_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 1);
   }
 }
@@ -646,9 +648,9 @@ extern gasnetex_handle_t gasnete_puti(gasnete_synctype_t synctype,
                                    size_t srccount, void * const srclist[], size_t srclen GASNETE_THREAD_FARG) {
   gasneti_assert(gasnete_vis_isinit);
   /* catch silly degenerate cases */
-  if_pf (dstcount + srccount <= 2 ||  /* empty or fully contiguous */
-         dstnode == gasneti_mynode) { /* purely local */ 
-    if (dstcount == 0) return GASNETEX_INVALID_HANDLE;
+  if (dstcount + srccount <= 2 ||  /* empty or fully contiguous */
+      GASNETI_SUPERNODE_LOCAL(dstnode)) { /* purely local */ 
+    if_pf (dstcount == 0) return GASNETEX_INVALID_HANDLE;
     else return gasnete_puti_ref_indiv(synctype,dstnode,dstcount,dstlist,dstlen,srccount,srclist,srclen GASNETE_THREAD_PASS);
   }
 
@@ -686,9 +688,9 @@ extern gasnetex_handle_t gasnete_geti(gasnete_synctype_t synctype,
                                    size_t srccount, void * const srclist[], size_t srclen GASNETE_THREAD_FARG) {
   gasneti_assert(gasnete_vis_isinit);
   /* catch silly degenerate cases */
-  if_pf (dstcount + srccount <= 2 ||  /* empty or fully contiguous */
-         srcnode == gasneti_mynode) { /* purely local */ 
-    if (dstcount == 0) return GASNETEX_INVALID_HANDLE;
+  if (dstcount + srccount <= 2 ||  /* empty or fully contiguous */
+      GASNETI_SUPERNODE_LOCAL(srcnode)) { /* purely local */ 
+    if_pf (dstcount == 0) return GASNETEX_INVALID_HANDLE;
     else return gasnete_geti_ref_indiv(synctype,dstcount,dstlist,dstlen,srcnode,srccount,srclist,srclen GASNETE_THREAD_PASS);
   }
 

extended-ref/gasnet_vis_strided.c
@@ -677,6 +677,7 @@ gasnetex_handle_t gasnete_puts_gather(gasnete_strided_stats_t const *stats, gasn
     gasnete_strided_pack_all(srcaddr, srcstrides, count, stridelevels, packedbuf);
     visop->type = GASNETI_VIS_CAT_PUTS_GATHER;
     visop->handle = gasnete_put_nb(NULL, dstnode, dstaddr, packedbuf, nbytes, GASNETEX_EVENT_DEFER, 0 GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNETEX_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 0);
   }
 }
@@ -715,6 +716,7 @@ gasnetex_handle_t gasnete_gets_scatter(gasnete_strided_stats_t const *stats, gas
     visop->addr = dstaddr;
     visop->len = stridelevels;
     visop->handle = gasnete_get_nb(NULL, packedbuf, srcnode, srcaddr, nbytes, 0 GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNETEX_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 1);
   }
 }
@@ -1148,8 +1150,8 @@ extern gasnetex_handle_t gasnete_puts(gasnete_synctype_t synctype,
   /* catch silly degenerate cases */
   if_pf (stats.totalsz == 0) /* empty */
     return GASNETEX_INVALID_HANDLE;
-  if_pf (dstnode == gasneti_mynode || /* purely local */ 
-         stats.dualcontiguity == stridelevels) {/* fully contiguous */
+  if (GASNETI_SUPERNODE_LOCAL(dstnode) || /* purely local */ 
+      stats.dualcontiguity == stridelevels) {/* fully contiguous */
     return gasnete_puts_ref_indiv(&stats,synctype,dstnode,dstaddr,dststrides,srcaddr,srcstrides,count,stridelevels GASNETE_THREAD_PASS);
   }
 
@@ -1194,8 +1196,8 @@ extern gasnetex_handle_t gasnete_gets(gasnete_synctype_t synctype,
   /* catch silly degenerate cases */
   if_pf (stats.totalsz == 0) /* empty */
     return GASNETEX_INVALID_HANDLE;
-  if_pf (srcnode == gasneti_mynode || /* purely local */ 
-         stats.dualcontiguity == stridelevels) {/* fully contiguous */
+  if (GASNETI_SUPERNODE_LOCAL(srcnode) || /* purely local */ 
+      stats.dualcontiguity == stridelevels) {/* fully contiguous */
     return gasnete_gets_ref_indiv(&stats,synctype,dstaddr,dststrides,srcnode,srcaddr,srcstrides,count,stridelevels GASNETE_THREAD_PASS);
   }
 

extended-ref/gasnet_vis_vector.c
@@ -232,6 +232,7 @@ gasnetex_handle_t gasnete_putv_gather(gasnete_synctype_t synctype,
     gasnete_memvec_pack(srccount, srclist, packedbuf, 0, (size_t)-1);
     visop->type = GASNETI_VIS_CAT_PUTV_GATHER;
     visop->handle = gasnete_put_nb(NULL, dstnode, dstlist[0].addr, packedbuf, nbytes, GASNETEX_EVENT_DEFER, 0 GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNETEX_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 0);
   }
 }
@@ -264,6 +265,7 @@ gasnetex_handle_t gasnete_getv_scatter(gasnete_synctype_t synctype,
     visop->type = GASNETI_VIS_CAT_GETV_SCATTER;
     visop->count = dstcount;
     visop->handle = gasnete_get_nb(NULL, packedbuf, srcnode, srclist[0].addr, nbytes, 0 GASNETE_THREAD_PASS);
+    gasneti_assert(visop->handle != GASNETEX_INVALID_HANDLE);
     GASNETE_PUSH_VISOP_RETURN(td, visop, synctype, 1);
   }
 }
@@ -641,8 +643,8 @@ extern gasnetex_handle_t gasnete_putv(gasnete_synctype_t synctype,
   /* catch silly degenerate cases */
   if_pf (dstcount == 0 || srccount == 0) /* empty (may miss some cases) */
     return GASNETEX_INVALID_HANDLE; 
-  if_pf (dstcount + srccount <= 2 ||  /* fully contiguous */
-         dstnode == gasneti_mynode) { /* purely local */ 
+  if (dstcount + srccount <= 2 ||  /* fully contiguous */
+      GASNETI_SUPERNODE_LOCAL(dstnode)) { /* purely local */ 
     return gasnete_putv_ref_indiv(synctype,dstnode,dstcount,dstlist,srccount,srclist GASNETE_THREAD_PASS);
   }
 
@@ -667,8 +669,8 @@ extern gasnetex_handle_t gasnete_getv(gasnete_synctype_t synctype,
   /* catch silly degenerate cases */
   if_pf (dstcount == 0 || srccount == 0) /* empty (may miss some cases) */
     return GASNETEX_INVALID_HANDLE; 
-  if_pf (dstcount + srccount <= 2 ||  /* fully contiguous */
-         srcnode == gasneti_mynode) { /* purely local */ 
+  if (dstcount + srccount <= 2 ||  /* fully contiguous */
+      GASNETI_SUPERNODE_LOCAL(srcnode)) { /* purely local */ 
     return gasnete_getv_ref_indiv(synctype,dstcount,dstlist,srcnode,srccount,srclist GASNETE_THREAD_PASS);
   }
 