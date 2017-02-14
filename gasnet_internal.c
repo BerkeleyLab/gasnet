evidence/axiom-evi-gasnet

Rework the conduit interface to {ssh,mpi,pmi}-spawner
Spawner selection logic tweaks:

+ Accept upper-case values in GASNET_SPAWNER envvar
+ Allow mpi-spawner attempt when GASNET_SPAWNER is unspecified,
  thereby enabling an mpi-spawner app to launch directly from mpirun
  (fixing bug 3406)
+ Rename Makefile output variables GASNET_<conduit>_SPAWNER_{SSH,MPI,PMI}
  and GASNET_<conduit>_SPAWNER_DEFAULT to omit the conduit name, since only
  one such variable can be active in any given build.
+ These should be the only externally-visible feature changes in this commit

Common implementation notes:

+ Factor gasneti_bootstrapInit() out of each conduit into gasnet_internal
+ Change gasneti_bootstrapInit() API to return a method table object
+ Convert the implementation of spawner methods to use static scope
+ Remove */gasnet_bootstrap_internal.h which are now empty
+ Move spawner ident strings out of conduit code
+ Use GASNETI_RETURN_ERRR for spawn failures in gasnet_init
+ Standardize naming for bootstrapSNodeBroadcast

ibv-, ofi-, psm- and mxm-conduits:

+ Now using a gasneti_spawnerfn_t object via core_internal.h macros
+ This approach maintains the syntactic illusion of direct bootstrap function
  calls, while still allowing conduits to easily drop-in a native
  implementation partway through init (as ibv does).
+ Rename automake conditionals <conduit>_BOOTSTRAP_{MPI,SSH} to be
  conduit-independent, since we have never exposed per-conduit control
  over that, and the decision is now completely hidden from conduit source code.

ibv-conduit:

+ Make gasnetc_bootstrap{Barrier,Exchange}_ib static, as these should
  only be called via the non-suffixed variants by external code

gemini/aries-conduit and portals-conduit:

+ These conduits continue to force the PMI spawner when calling the
  unified Init, and invoke ops via resulting gasneti_spawnerfn_t object
+ Add GASNET_SPAWNER_DEFAULT and GASNET_SPAWNER_PMI Makefile output
  variables, for uniformity with other conduit

gasnet_internal.c
@@ -1332,6 +1332,73 @@ ssize_t gasneti_getline(char **buf_p, size_t *n_p, FILE *fp) {
 #endif /* gasneti_getline */
 
 /* ------------------------------------------------------------------------------------ */
+// Internal conduit interface to spawner
+
+#if HAVE_SSH_SPAWNER
+  extern gasneti_spawnerfn_t const *gasneti_bootstrapInit_ssh(int *argc, char ***argv, gasnet_node_t *nodes, gasnet_node_t *mynode);
+#endif
+#if HAVE_MPI_SPAWNER
+  extern gasneti_spawnerfn_t const *gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnet_node_t *nodes, gasnet_node_t *mynode);
+#endif
+#if HAVE_PMI_SPAWNER
+  extern gasneti_spawnerfn_t const *gasneti_bootstrapInit_pmi(int *argc, char ***argv, gasnet_node_t *nodes, gasnet_node_t *mynode);
+#endif
+
+extern gasneti_spawnerfn_t const *gasneti_spawnerInit(int *argc_p, char ***argv_p,
+                                  const char *force_spawner,
+                                  gasnet_node_t *nodes_p, gasnet_node_t *mynode_p) {
+  gasneti_spawnerfn_t const *res = NULL;
+  const char *not_set = "(not set)";
+  const char *spawner;
+  char *tmp = NULL;
+  if (force_spawner) spawner = force_spawner;
+  else spawner = gasneti_getenv_withdefault("GASNET_SPAWNER", not_set);
+
+  if (spawner != not_set) { // upper-case
+    tmp = gasneti_strdup(spawner);
+    for (char *p = tmp; *p; p++) *p = toupper(*p);
+    spawner = tmp;
+  }
+
+#if HAVE_MPI_SPAWNER
+  /* bug 3406: Try MPI-based spawn first, EVEN if the var is not set.
+   * This is a requirement for spawning using bare mpirun
+   */
+  if (!res && (spawner == not_set || !strcmp(spawner, "MPI")) &&
+      (res = gasneti_bootstrapInit_mpi(argc_p, argv_p, nodes_p, mynode_p))) {
+  }
+#endif
+
+#if HAVE_SSH_SPAWNER
+  /* GASNET_SPAWNER=ssh is set by gasnetrun for the ssh spawn master,
+   * and by the ssh command line for other processes (ie all normal uses).
+   * We might still reach here without the variable if MPI is disabled at configure time
+   * and the user is attempting a direct command-line launch (ie -GASNET-SPAWN-master)
+   */
+  if (!res && (spawner == not_set || !strcmp(spawner, "SSH")) &&
+      (res = gasneti_bootstrapInit_ssh(argc_p, argv_p, nodes_p, mynode_p))) {
+  }
+#endif
+
+#if HAVE_PMI_SPAWNER
+  /* Don't really expect GASNET_SPAWNER set if launched directly by srun, mpirun, yod, etc.
+   * So, when the env var is not set, we try pmi-based spawn last.
+   */
+  if (!res && (spawner == not_set || !strcmp(spawner, "PMI")) &&
+      (res = gasneti_bootstrapInit_pmi(argc_p, argv_p, nodes_p, mynode_p))) {
+  }
+#endif
+
+  if (!res) {
+    gasneti_fatalerror("Requested spawner \"%s\" is unknown or not supported in this build", spawner);
+  }
+
+  gasneti_free(tmp);
+
+  return res;
+}
+
+/* ------------------------------------------------------------------------------------ */
 /* Debug memory management
    debug memory format:
   | prev | next | allocdesc (2*sizeof(void*)) | datasz | BEGINPOST | <user data> | ENDPOST |

other/mpi-spawner/gasnet_bootstrap_mpi.c
@@ -7,7 +7,6 @@
 
 #include <gasnet_internal.h>
 #include <gasnet_core_internal.h>
-#include <mpi-spawner/gasnet_bootstrap_internal.h>
 #include <signal.h>
 #include <mpi.h>
 
@@ -26,6 +25,10 @@ static MPI_Comm gasnetc_mpi_comm;
 static int gasnetc_mpi_preinitialized = 0;
 static int gasnetc_mpi_size = -1;
 static int gasnetc_mpi_rank = -1;
+
+GASNETI_IDENT(gasnetc_IdentString_HaveMPISpawner, "$GASNetMPISpawner: 1 $");
+
+static gasneti_spawnerfn_t const spawnerfn;
 
 #ifndef HAVE_MPI_INIT_THREAD
 #define HAVE_MPI_INIT_THREAD (MPI_VERSION >= 2 || (defined(MPI_THREAD_SINGLE) && defined(MPI_THREAD_SERIALIZED)))
@@ -61,20 +64,20 @@ static const char *threadint2str(int id) {
   }
 }
 
-int gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnet_node_t *nodes, gasnet_node_t *mynode) {
+extern gasneti_spawnerfn_t const *gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnet_node_t *nodes, gasnet_node_t *mynode) {
   MPI_Group world;
   int err;
 
   /* Call MPI_Init exactly once */
   err = MPI_Initialized(&gasnetc_mpi_preinitialized);
-  if (MPI_SUCCESS != err) return GASNET_ERR_NOT_INIT;
+  if (MPI_SUCCESS != err) return NULL;
 #if !HAVE_MPI_INIT_THREAD
   if (!gasnetc_mpi_preinitialized) {
   #if MPI_VERSION < 2
-    if (!argc || !argv) return GASNET_ERR_BAD_ARG;
+    if (!argc || !argv) return NULL;
   #endif
     err = MPI_Init(argc, argv);
-    if (MPI_SUCCESS != err) return GASNET_ERR_NOT_INIT;
+    if (MPI_SUCCESS != err) return NULL;
   }
 #else /* HAVE_MPI_INIT_THREAD */
   // Setup/verify the threading mode
@@ -110,7 +113,7 @@ int gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnet_node_t *nodes, gas
         fflush(stderr);
       #endif
       err = MPI_Init_thread(argc, argv, required, &provided);
-      if (err != MPI_SUCCESS) return GASNET_ERR_NOT_INIT;
+      if (err != MPI_SUCCESS) return NULL;
     }
     #if GASNET_DEBUG_VERBOSE
       fprintf(stderr,"mpi-spawner: MPI threading mode: %s required, %s provided.\n",
@@ -147,13 +150,13 @@ int gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnet_node_t *nodes, gas
   *mynode = gasnetc_mpi_rank;
 
   gasneti_setupGlobalEnvironment(*nodes, *mynode,
-				 &gasneti_bootstrapExchange_mpi,
-				 &gasneti_bootstrapBroadcast_mpi);
+				 spawnerfn.Exchange,
+				 spawnerfn.Broadcast);
 
-  return GASNET_OK;
+  return &spawnerfn;
 }
 
-void gasneti_bootstrapFini_mpi(void) {
+static void bootstrapFini(void) {
   int err;
 
 #if (MPI_VERSION > 1)
@@ -177,7 +180,7 @@ void gasneti_bootstrapFini_mpi(void) {
   }
 }
 
-void gasneti_bootstrapAbort_mpi(int exitcode) {
+static void bootstrapAbort(int exitcode) {
   (void) MPI_Abort(gasnetc_mpi_comm, exitcode);
 
   gasneti_reghandler(SIGABRT, SIG_DFL);
@@ -185,14 +188,14 @@ void gasneti_bootstrapAbort_mpi(int exitcode) {
   /* NOT REACHED */
 }
 
-void gasneti_bootstrapBarrier_mpi(void) {
+static void bootstrapBarrier(void) {
   int err;
 
   err = MPI_Barrier(gasnetc_mpi_comm);
   gasneti_assert_always(err == MPI_SUCCESS);
 }
 
-void gasneti_bootstrapExchange_mpi(void *src, size_t len, void *dest) {
+static void bootstrapExchange(void *src, size_t len, void *dest) {
   const int inplace = ((uint8_t *)src == (uint8_t *)dest + len * gasnetc_mpi_rank);
   int err;
 
@@ -212,7 +215,7 @@ void gasneti_bootstrapExchange_mpi(void *src, size_t len, void *dest) {
 #endif
 }
 
-void gasneti_bootstrapAlltoall_mpi(void *src, size_t len, void *dest) {
+static void bootstrapAlltoall(void *src, size_t len, void *dest) {
   const int inplace = (src == dest);
   int err;
 
@@ -233,7 +236,7 @@ void gasneti_bootstrapAlltoall_mpi(void *src, size_t len, void *dest) {
 #endif
 }
 
-void gasneti_bootstrapBroadcast_mpi(void *src, size_t len, void *dest, int rootnode) {
+static void bootstrapBroadcast(void *src, size_t len, void *dest, int rootnode) {
   int err;
   
   if (gasnetc_mpi_rank == rootnode) {
@@ -243,7 +246,7 @@ void gasneti_bootstrapBroadcast_mpi(void *src, size_t len, void *dest, int rootn
   gasneti_assert_always(err == MPI_SUCCESS);
 }
 
-void gasneti_bootstrapSNodeBroadcast_mpi(void *src, size_t len, void *dest, int rootnode) {
+static void bootstrapSNodeBroadcast(void *src, size_t len, void *dest, int rootnode) {
   int err;
 
   if (gasnetc_mpi_rank == rootnode) {
@@ -267,6 +270,23 @@ void gasneti_bootstrapSNodeBroadcast_mpi(void *src, size_t len, void *dest, int 
   }
 }
 
-void gasneti_bootstrapCleanup_mpi(void) {
+static void bootstrapCleanup(void) {
   /* Nothing to do here */
 }
+
+static gasneti_spawnerfn_t const spawnerfn = {
+  bootstrapBarrier,
+  bootstrapExchange,
+  bootstrapBroadcast,
+  bootstrapSNodeBroadcast,
+  bootstrapAlltoall,
+  bootstrapAbort,
+  bootstrapCleanup,
+  bootstrapFini,
+#if GASNET_BLCR && 0 // BLCR-TODO
+  bootstrapPreCheckpoint,
+  bootstrapPostCheckpoint,
+  bootstrapRollback,
+#endif
+};
+

BerkeleyLab/gasnet

(cherry picked from commit 58206cef675697e8e2e246cc357f533c9d8f882e)

gasnet_internal.c
@@ -1334,6 +1334,73 @@ ssize_t gasneti_getline(char **buf_p, size_t *n_p, FILE *fp) {
 #endif /* gasneti_getline */
 
 /* ------------------------------------------------------------------------------------ */
+// Internal conduit interface to spawner
+
+#if HAVE_SSH_SPAWNER
+  extern gasneti_spawnerfn_t const *gasneti_bootstrapInit_ssh(int *argc, char ***argv, gasnetex_rank_t *nodes, gasnetex_rank_t *mynode);
+#endif
+#if HAVE_MPI_SPAWNER
+  extern gasneti_spawnerfn_t const *gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnetex_rank_t *nodes, gasnetex_rank_t *mynode);
+#endif
+#if HAVE_PMI_SPAWNER
+  extern gasneti_spawnerfn_t const *gasneti_bootstrapInit_pmi(int *argc, char ***argv, gasnetex_rank_t *nodes, gasnetex_rank_t *mynode);
+#endif
+
+extern gasneti_spawnerfn_t const *gasneti_spawnerInit(int *argc_p, char ***argv_p,
+                                  const char *force_spawner,
+                                  gasnetex_rank_t *nodes_p, gasnetex_rank_t *mynode_p) {
+  gasneti_spawnerfn_t const *res = NULL;
+  const char *not_set = "(not set)";
+  const char *spawner;
+  char *tmp = NULL;
+  if (force_spawner) spawner = force_spawner;
+  else spawner = gasneti_getenv_withdefault("GASNET_SPAWNER", not_set);
+
+  if (spawner != not_set) { // upper-case
+    tmp = gasneti_strdup(spawner);
+    for (char *p = tmp; *p; p++) *p = toupper(*p);
+    spawner = tmp;
+  }
+
+#if HAVE_MPI_SPAWNER
+  /* bug 3406: Try MPI-based spawn first, EVEN if the var is not set.
+   * This is a requirement for spawning using bare mpirun
+   */
+  if (!res && (spawner == not_set || !strcmp(spawner, "MPI")) &&
+      (res = gasneti_bootstrapInit_mpi(argc_p, argv_p, nodes_p, mynode_p))) {
+  }
+#endif
+
+#if HAVE_SSH_SPAWNER
+  /* GASNET_SPAWNER=ssh is set by gasnetrun for the ssh spawn master,
+   * and by the ssh command line for other processes (ie all normal uses).
+   * We might still reach here without the variable if MPI is disabled at configure time
+   * and the user is attempting a direct command-line launch (ie -GASNET-SPAWN-master)
+   */
+  if (!res && (spawner == not_set || !strcmp(spawner, "SSH")) &&
+      (res = gasneti_bootstrapInit_ssh(argc_p, argv_p, nodes_p, mynode_p))) {
+  }
+#endif
+
+#if HAVE_PMI_SPAWNER
+  /* Don't really expect GASNET_SPAWNER set if launched directly by srun, mpirun, yod, etc.
+   * So, when the env var is not set, we try pmi-based spawn last.
+   */
+  if (!res && (spawner == not_set || !strcmp(spawner, "PMI")) &&
+      (res = gasneti_bootstrapInit_pmi(argc_p, argv_p, nodes_p, mynode_p))) {
+  }
+#endif
+
+  if (!res) {
+    gasneti_fatalerror("Requested spawner \"%s\" is unknown or not supported in this build", spawner);
+  }
+
+  gasneti_free(tmp);
+
+  return res;
+}
+
+/* ------------------------------------------------------------------------------------ */
 /* Debug memory management
    debug memory format:
   | prev | next | allocdesc (2*sizeof(void*)) | datasz | BEGINPOST | <user data> | ENDPOST |

other/mpi-spawner/gasnet_bootstrap_mpi.c
@@ -7,7 +7,6 @@
 
 #include <gasnet_internal.h>
 #include <gasnet_core_internal.h>
-#include <mpi-spawner/gasnet_bootstrap_internal.h>
 #include <signal.h>
 #include <mpi.h>
 
@@ -26,6 +25,10 @@ static MPI_Comm gasnetc_mpi_comm;
 static int gasnetc_mpi_preinitialized = 0;
 static int gasnetc_mpi_size = -1;
 static int gasnetc_mpi_rank = -1;
+
+GASNETI_IDENT(gasnetc_IdentString_HaveMPISpawner, "$GASNetMPISpawner: 1 $");
+
+static gasneti_spawnerfn_t const spawnerfn;
 
 #ifndef HAVE_MPI_INIT_THREAD
 #define HAVE_MPI_INIT_THREAD (MPI_VERSION >= 2 || (defined(MPI_THREAD_SINGLE) && defined(MPI_THREAD_SERIALIZED)))
@@ -61,20 +64,20 @@ static const char *threadint2str(int id) {
   }
 }
 
-int gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnetex_rank_t *nodes, gasnetex_rank_t *mynode) {
+extern gasneti_spawnerfn_t const *gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnetex_rank_t *nodes, gasnetex_rank_t *mynode) {
   MPI_Group world;
   int err;
 
   /* Call MPI_Init exactly once */
   err = MPI_Initialized(&gasnetc_mpi_preinitialized);
-  if (MPI_SUCCESS != err) return GASNET_ERR_NOT_INIT;
+  if (MPI_SUCCESS != err) return NULL;
 #if !HAVE_MPI_INIT_THREAD
   if (!gasnetc_mpi_preinitialized) {
   #if MPI_VERSION < 2
-    if (!argc || !argv) return GASNET_ERR_BAD_ARG;
+    if (!argc || !argv) return NULL;
   #endif
     err = MPI_Init(argc, argv);
-    if (MPI_SUCCESS != err) return GASNET_ERR_NOT_INIT;
+    if (MPI_SUCCESS != err) return NULL;
   }
 #else /* HAVE_MPI_INIT_THREAD */
   // Setup/verify the threading mode
@@ -110,7 +113,7 @@ int gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnetex_rank_t *nodes, g
         fflush(stderr);
       #endif
       err = MPI_Init_thread(argc, argv, required, &provided);
-      if (err != MPI_SUCCESS) return GASNET_ERR_NOT_INIT;
+      if (err != MPI_SUCCESS) return NULL;
     }
     #if GASNET_DEBUG_VERBOSE
       fprintf(stderr,"mpi-spawner: MPI threading mode: %s required, %s provided.\n",
@@ -147,13 +150,13 @@ int gasneti_bootstrapInit_mpi(int *argc, char ***argv, gasnetex_rank_t *nodes, g
   *mynode = gasnetc_mpi_rank;
 
   gasneti_setupGlobalEnvironment(*nodes, *mynode,
-				 &gasneti_bootstrapExchange_mpi,
-				 &gasneti_bootstrapBroadcast_mpi);
+				 spawnerfn.Exchange,
+				 spawnerfn.Broadcast);
 
-  return GASNET_OK;
+  return &spawnerfn;
 }
 
-void gasneti_bootstrapFini_mpi(void) {
+static void bootstrapFini(void) {
   int err;
 
 #if (MPI_VERSION > 1)
@@ -177,7 +180,7 @@ void gasneti_bootstrapFini_mpi(void) {
   }
 }
 
-void gasneti_bootstrapAbort_mpi(int exitcode) {
+static void bootstrapAbort(int exitcode) {
   (void) MPI_Abort(gasnetc_mpi_comm, exitcode);
 
   gasneti_reghandler(SIGABRT, SIG_DFL);
@@ -185,14 +188,14 @@ void gasneti_bootstrapAbort_mpi(int exitcode) {
   /* NOT REACHED */
 }
 
-void gasneti_bootstrapBarrier_mpi(void) {
+static void bootstrapBarrier(void) {
   int err;
 
   err = MPI_Barrier(gasnetc_mpi_comm);
   gasneti_assert_always(err == MPI_SUCCESS);
 }
 
-void gasneti_bootstrapExchange_mpi(void *src, size_t len, void *dest) {
+static void bootstrapExchange(void *src, size_t len, void *dest) {
   const int inplace = ((uint8_t *)src == (uint8_t *)dest + len * gasnetc_mpi_rank);
   int err;
 
@@ -212,7 +215,7 @@ void gasneti_bootstrapExchange_mpi(void *src, size_t len, void *dest) {
 #endif
 }
 
-void gasneti_bootstrapAlltoall_mpi(void *src, size_t len, void *dest) {
+static void bootstrapAlltoall(void *src, size_t len, void *dest) {
   const int inplace = (src == dest);
   int err;
 
@@ -233,7 +236,7 @@ void gasneti_bootstrapAlltoall_mpi(void *src, size_t len, void *dest) {
 #endif
 }
 
-void gasneti_bootstrapBroadcast_mpi(void *src, size_t len, void *dest, int rootnode) {
+static void bootstrapBroadcast(void *src, size_t len, void *dest, int rootnode) {
   int err;
   
   if (gasnetc_mpi_rank == rootnode) {
@@ -243,7 +246,7 @@ void gasneti_bootstrapBroadcast_mpi(void *src, size_t len, void *dest, int rootn
   gasneti_assert_always(err == MPI_SUCCESS);
 }
 
-void gasneti_bootstrapSNodeBroadcast_mpi(void *src, size_t len, void *dest, int rootnode) {
+static void bootstrapSNodeBroadcast(void *src, size_t len, void *dest, int rootnode) {
   int err;
 
   if (gasnetc_mpi_rank == rootnode) {
@@ -267,6 +270,23 @@ void gasneti_bootstrapSNodeBroadcast_mpi(void *src, size_t len, void *dest, int 
   }
 }
 
-void gasneti_bootstrapCleanup_mpi(void) {
+static void bootstrapCleanup(void) {
   /* Nothing to do here */
 }
+
+static gasneti_spawnerfn_t const spawnerfn = {
+  bootstrapBarrier,
+  bootstrapExchange,
+  bootstrapBroadcast,
+  bootstrapSNodeBroadcast,
+  bootstrapAlltoall,
+  bootstrapAbort,
+  bootstrapCleanup,
+  bootstrapFini,
+#if GASNET_BLCR && 0 // BLCR-TODO
+  bootstrapPreCheckpoint,
+  bootstrapPostCheckpoint,
+  bootstrapRollback,
+#endif
+};
+