evidence/axiom-evi-gasnet

Fix bug 3435 - ibv: exit issues on upc_io_test
This commit fixes the abnormal-exit handling issues described in bug
3435 by making two small changes:
 + Ensure that gasnetc_dissem_peers is not zeroed prior to its
   use in coordinating an exit, by moving "gasnetc_dissem_peers = 0"
   from the end of gasnetc_sys_coll_fini() to the start of
   gasnetc_sys_coll_init().
   This ensures the coordination actually performs off-node comms.
 + Fix the pshm-based paths for System-category AMs to properly
   increment the completed counter, if any.
   This prevents blocking forever waiting for indication that a
   pshm-based exit-coordination AM has been sent.

Additionally, this commit adds (in a debug build only) a check
that gasnetc_sys_coll_fini+init+fini is safe (a prerequisite for
correct checkpoint/restart behavior that may not otherwise be
checked in normal testing).

ibv-conduit/gasnet_core_sndrcv.c
@@ -4105,9 +4105,12 @@ extern int gasnetc_RequestGeneric(gasnetc_category_t category,
 
 #if GASNET_PSHM
   if_pt (gasneti_pshm_in_supernode(dest)) {
-    return gasneti_AMPSHM_RequestGeneric(category, dest, handler,
+    int retval;
+    retval = gasneti_AMPSHM_RequestGeneric(category, dest, handler,
                                          src_addr, nbytes, dst_addr,
                                          numargs, argptr);
+    if_pf (completed) gasnetc_atomic_increment(completed,0);
+    return retval;
   }
 #endif
 
@@ -4126,9 +4129,11 @@ extern int gasnetc_ReplyGeneric(gasnetc_category_t category,
 
 #if GASNET_PSHM
   if_pt (gasnetc_token_is_pshm(token)) {
-      return gasneti_AMPSHM_ReplyGeneric(category, token, handler,
+    retval = gasneti_AMPSHM_ReplyGeneric(category, token, handler,
                                          src_addr, nbytes, dst_addr,
                                          numargs, argptr);
+    if_pf (completed) gasnetc_atomic_increment(completed,0);
+    return retval;
   }
 #endif
 

BerkeleyLab/gasnet

ibv-conduit/gasnet_core_sndrcv.c
@@ -4367,9 +4367,12 @@ extern int gasnetc_RequestGeneric(gasnetc_category_t category,
 
 #if GASNET_PSHM
   if_pt (gasneti_pshm_in_supernode(node)) {
-    return gasneti_AMPSHM_RequestGeneric(category, node, handler,
+    int retval;
+    retval = gasneti_AMPSHM_RequestGeneric(category, node, handler,
                                          src_addr, nbytes, dst_addr,
                                          flags, numargs, argptr);
+    if_pf (counter) gasnetc_atomic_increment(&counter->completed, 0);
+    return retval;
   }
 #endif
 
@@ -4392,9 +4395,11 @@ extern int gasnetc_ReplyGeneric(gasnetc_category_t category,
 
 #if GASNET_PSHM
   if_pt (gasnetc_token_is_pshm(token)) {
-      return gasneti_AMPSHM_ReplyGeneric(category, token, handler,
+    retval = gasneti_AMPSHM_ReplyGeneric(category, token, handler,
                                          src_addr, nbytes, dst_addr,
                                          flags, numargs, argptr);
+    if_pf (counter) gasnetc_atomic_increment(&counter->completed, 0);
+    return retval;
   }
 #endif
 