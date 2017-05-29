c5319027a91f0b989c7f38609ef364703bb7a82d
Paul H. Hargrove <PHHargrove@lbl.gov>
2017-05-28 19:47:57 -0700

    Address bug 3532 by delaying post descriptor alloc
    This commit addresses bug 3532:
         "testthreads -a N" deadlock when (GNI_NUM_PD <= N)
    
    by ensuring that threads executing AM Requests (other than LongAsync)
    will hold at most one post descriptor at a time.  This is accomplished
    by simply making the post descriptor the last resource (rather then
    the first) to be allocated when preparing to launch an AM Request.
    This ordering means that the post descriptor is not held until after
    any/all AMPoll calls (which may need a post descriptor to Reply).
    
    In addition to preventing the reported deadlock, this change should
    prevent threads that are stalled on AM flow-control from holding
    post descriptors that might otherwise be used by other threads to
    help tolerate the injection-to-completion latency.
    
    This commit's summary says "address" rather than "fix" because it is
    still believed that the LongAsync code path will hold two post
    descriptors simultaneously.  However, attempts to reproduce a deadlock
    attributable to that behavior have so far been unsuccessful.
    
    (cherry picked from commit 482597baaa516ee6d29df05d11d8cb5dcb741f13)

--- gemini-conduit/gasnet_gemini.c
+++ gemini-conduit/gasnet_gemini.c
@@ -1675,8 +1675,6 @@ gasnetc_post_descriptor_t *gasnetc_alloc_request_post_descriptor(gasnetex_rank_t
   GASNETC_DIDX_POST(GASNETC_DEFAULT_DOMAIN);
   DOMAIN_SPECIFIC_VAR(peer_struct_t * const, peer_data);
   peer_struct_t * const peer = &peer_data[dest];
-  gasnetc_post_descriptor_t *gpd = gasnetc_alloc_post_descriptor(GASNETC_DIDX_PASS_ALONE);
-  gni_post_descriptor_t *pd = &gpd->pd;
   unsigned int my_slot;
   unsigned int remote_slot;
   const unsigned int slots = MAX(1, ((length + am_slotsz - 1) >> am_slot_bits));
@@ -1713,6 +1711,8 @@ gasnetc_post_descriptor_t *gasnetc_alloc_request_post_descriptor(gasnetex_rank_t
 
   r->u.request_bits = mask;
 
+  gasnetc_post_descriptor_t *gpd = gasnetc_alloc_post_descriptor(GASNETC_DIDX_PASS_ALONE);
+  gni_post_descriptor_t *pd = &gpd->pd;
   pd->remote_addr = (uint64_t) peer->remote_request_base + (remote_slot << am_slot_bits);
   pd->sync_flag_value = build_notify(notify_request, r - reply_pool, remote_slot);
   
