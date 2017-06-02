8e304a1aa007c56a8579a2536eee707a114f2285
Paul H. Hargrove <PHHargrove@lbl.gov>
2017-06-02 14:27:36 -0700

    Empty all source files for non-EX conduits
    This commit "removes" the contents of all source files (.c and .h) in
    the five conduits that are not yet being ported forward to GASNet-EX
    as part of our initial work.
    
    All files remain in both the repository and the distribution tarball
    to avoid the need to remove (and restore) the corresponding autoconf
    and automake logic.  However, the source files now each contain just
    an explanatory comment.  This is intended to reduce confusion for
    anyone examining the sources, as well as ensuring that any git commits
    cherry-picked from develop *will* have conflicts in those directories.
    
    Prior to this commit, these five conduits contained only *partial*
    updates with respect to GASNet-EX infrastructure changes and had also
    not been receiving updates and bug fixes made on develop.

--- shmem-conduit/gasnet_extended_fwd.h
+++ shmem-conduit/gasnet_extended_fwd.h
@@ -1,53 +1,5 @@
-/*   $Source: bitbucket.org:berkeleylab/gasnet.git/shmem-conduit/gasnet_extended_fwd.h $
- * Description: GASNet Extended API Header (forward decls)
- * Copyright 2002, Dan Bonachea <bonachea@cs.berkeley.edu>
- * Terms of use are as specified in license.txt
- */
-
-#ifndef _IN_GASNETEX_H
-  #error This file is not meant to be included directly- clients should include gasnetex.h
-#endif
-
-#ifndef _GASNET_EXTENDED_FWD_H
-#define _GASNET_EXTENDED_FWD_H
-
-#define GASNET_EXTENDED_VERSION      1.9
-#define GASNET_EXTENDED_VERSION_STR  _STRINGIFY(GASNET_EXTENDED_VERSION)
-#define GASNET_EXTENDED_NAME         SHMEM
-#define GASNET_EXTENDED_NAME_STR     _STRINGIFY(GASNET_EXTENDED_NAME)
-
-#define _GASNET_HANDLE_T
-typedef int * gasnet_handle_t;
-#define GASNET_INVALID_HANDLE ((gasnet_handle_t)0)
-#define GASNETI_DISABLE_EOP_INTERFACE
-
-  /* this can be used to add statistical collection values 
-     specific to the extended API implementation (see gasnet_help.h) */
-#define GASNETE_CONDUIT_STATS(CNT,VAL,TIME)  \
-        CNT(C, DYNAMIC_THREADLOOKUP, cnt)    \
-	GASNETI_VIS_STATS(CNT,VAL,TIME)      \
-	GASNETI_COLL_STATS(CNT,VAL,TIME)
-
-#define GASNETE_AUXSEG_DECLS \
-    extern gasneti_auxseg_request_t gasnete_barr_auxseg_alloc(gasnet_seginfo_t *auxseg_info);
-#define GASNETE_AUXSEG_FNS() gasnete_barr_auxseg_alloc, 
-
-#define GASNETE_HAVE_EXTENDED_HELP_EXTRA_H
-
-#ifdef GASNETE_GLOBAL_ADDRESS
-  /* tweaks required for bounds checking on clients who lie about node number*/
-  extern int _gasneti_in_segment_bc(gasnet_node_t node, const void *ptr, size_t nbytes);
-  #define gasneti_in_segment_bc _gasneti_in_segment_bc
-  #define gasneti_in_nodes_bc(node) (node == (gasnet_node_t)-1 || node < gasneti_nodes)
-  #define gasneti_in_segment_allowoutofseg_bc gasneti_in_segment_bc
-#endif
-
-#define _GASNETE_MYTHREAD
-struct _gasnete_threaddata_t;
-extern struct _gasnete_threaddata_t * const gasnete_threaddata_ptr;
-#define gasnete_mythread() (gasnete_threaddata_ptr)
-#define GASNETI_MAX_THREADS 1
-
-#define GASNETE_COLL_CONDUIT_BARRIERS GASNETE_COLL_BARRIER_SHMEM
-
-#endif
+// This conduit is not currently supported in GASNet-EX.
+//
+// We hope that support can be restored in the future.
+// However, for the time being all of its source files
+// are empty to reduce confusion and ease maintenance.
