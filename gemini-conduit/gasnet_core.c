93be9d055f6cbafa2cf54259b827dc931cab9238
Paul H. Hargrove <PHHargrove@lbl.gov>
2017-06-13 03:10:45 -0700

    fixup: gasneti_amtbl_check: add category and is...
    This commit fixes recent regressions in PAMI and Aries/Gemini conduits
    due to incorrect identifiers used in
       71cd266b gasneti_amtbl_check: add category and isReq args

--- gemini-conduit/gasnet_core.c
+++ gemini-conduit/gasnet_core.c
@@ -1187,7 +1187,7 @@ int gasnetc_local_short_common(int is_req, gex_AM_Index_t handler,
   gex_AM_Token_t token = (gex_AM_Token_t)&the_token; /* RUN macros need an lvalue */
   gex_AM_Arg_t args[GASNETC_MAX_ARGS];
   
-  gasneti_amtbl_check(handler_entry, numargs, gasnetc_short, is_req);
+  gasneti_amtbl_check(handler_entry, numargs, gasneti_Short, is_req);
   for (i = 0; i < numargs; i++) {
     args[i] = (gex_AM_Arg_t)va_arg(argptr, gex_AM_Arg_t);
   }
@@ -1209,7 +1209,7 @@ int gasnetc_local_medium_common(int is_req, gex_AM_Index_t handler,
   gex_AM_Arg_t args[GASNETC_MAX_ARGS];
   void *payload = alloca(nbytes);
   
-  gasneti_amtbl_check(handler_entry, numargs, gasnetc_medium, is_req);
+  gasneti_amtbl_check(handler_entry, numargs, gasneti_Medium, is_req);
   for (i = 0; i < numargs; i++) {
     args[i] = (gex_AM_Arg_t)va_arg(argptr, gex_AM_Arg_t);
   }
@@ -1232,7 +1232,7 @@ int gasnetc_local_long_common(int is_req, gex_AM_Index_t handler,
   gex_AM_Arg_t args[GASNETC_MAX_ARGS];
   int i;
   
-  gasneti_amtbl_check(handler_entry, numargs, gasnetc_long, is_req);
+  gasneti_amtbl_check(handler_entry, numargs, gasneti_Long, is_req);
   for (i = 0; i < numargs; i++) {
     args[i] = (gex_AM_Arg_t)va_arg(argptr, gex_AM_Arg_t);
   }
--- pami-conduit/gasnet_core.c
+++ pami-conduit/gasnet_core.c
@@ -852,7 +852,7 @@ void run_short(gasnetc_token_t *token) {
   local_token.rep_sent = 0;
   client_token = (gex_AM_Token_t)(&local_token);
 #endif
-  gasneti_amtbl_check(&gasnetc_handler[handler_id], numargs, gasnetc_short, is_req);
+  gasneti_amtbl_check(&gasnetc_handler[handler_id], numargs, gasneti_Short, is_req);
   GASNETI_RUN_HANDLER_SHORT(is_req,handler_id,handler_fn,client_token,args,numargs);
 }
 
@@ -871,7 +871,7 @@ void run_medium(gasnetc_token_t *token) {
 #if GASNET_DEBUG
   header->rep_sent = 0;
 #endif
-  gasneti_amtbl_check(&gasnetc_handler[handler_id], numargs, gasnetc_medium, is_req);
+  gasneti_amtbl_check(&gasnetc_handler[handler_id], numargs, gasneti_Medium, is_req);
   GASNETI_RUN_HANDLER_MEDIUM(is_req,handler_id,handler_fn,client_token,args,numargs,data,nbytes);
 }
 
@@ -890,7 +890,7 @@ void run_long(gasnetc_token_t *token) {
 #if GASNET_DEBUG
   header->rep_sent = 0;
 #endif
-  gasneti_amtbl_check(&gasnetc_handler[handler_id], numargs, gasnetc_long, is_req);
+  gasneti_amtbl_check(&gasnetc_handler[handler_id], numargs, gasneti_Long, is_req);
   GASNETI_RUN_HANDLER_LONG(is_req,handler_id,handler_fn,client_token,args,numargs,data,nbytes);
 }
 
