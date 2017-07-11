8e42782aeec01f02dce6c1739a55850dc5c8cf07
Paul H. Hargrove <PHHargrove@lbl.gov>
2017-07-10 23:38:08 -0400

    Apply end versions two PGI bug work-arounds
    This commit adds end-versions for work-arounds used with PGI compilers
    which exhibit TPR 23290 or 23291.  These fixed-in versions are from
    PGI's website and have been tested.

--- gasnet_asm.h
+++ gasnet_asm.h
@@ -44,13 +44,13 @@
   #define GASNETI_HAVE_GCC_ASM 1
 #elif PLATFORM_COMPILER_PGI && PLATFORM_ARCH_POWERPC
   #define GASNETI_HAVE_GCC_ASM 1
-  #if PLATFORM_COMPILER_VERSION_GE(16,0,0) // All known versions through at least 16.10
+  #if PLATFORM_COMPILER_VERSION_LT(17,3,0) // All known versions prior to 17.3
     // PGI "tpr 23290"
     // Does not grok the immediate modifier "%I" in an asm template
     #define GASNETI_PGI_ASM_TPR23290 1
   #endif
   #if PLATFORM_COMPILER_PGI_CXX && \
-      PLATFORM_COMPILER_VERSION_GE(16,0,0) // All known versions through at least 16.10
+      PLATFORM_COMPILER_VERSION_LT(17,1,0) // All known versions prior to 17.1
     // PGI "tpr 23291"
     // C++ compiler does not grok "cr0", though C compiler does
     #define GASNETI_PGI_ASM_TPR23291 1
