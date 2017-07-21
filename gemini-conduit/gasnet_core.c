50ead50ad8763f0ea84003930b6a178d25195d3a
Paul H. Hargrove <PHHargrove@lbl.gov>
2017-07-21 13:48:06 -0700

    aries/gemini: fix regression in gex_Token_Info()
    This commit corrects a regression caused by failure to account for the
    existence of "packed long" when implementing the GEX_TI_IS_LONG query.

--- gemini-conduit/gasnet_core.c
+++ gemini-conduit/gasnet_core.c
@@ -1148,7 +1148,8 @@ extern gex_TI_t gasnetc_Token_Info(
   info->gex_is_req = (gc_notify_request == gc_notify_get_type(real_token->notify));
   result |= GEX_TI_IS_REQ;
 
-  info->gex_is_long = (GC_CMD_AM_LONG == gasnetc_am_command(real_token->notify));
+  info->gex_is_long = (GC_CMD_AM_LONG == gasnetc_am_command(real_token->notify)) ||
+                      (GC_CMD_AM_LONG_PACKED == gasnetc_am_command(real_token->notify));
   result |= GEX_TI_IS_LONG;
 
   return GASNETI_TOKEN_INFO_RETURN(result, info, mask);
