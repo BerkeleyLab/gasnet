719d9a814047728da22a2621a6cbaf677efd077e
Paul H. Hargrove <PHHargrove@lbl.gov>
2017-08-06 14:49:12 -0700

    Cherry-pick gasneti_platform_isWSL()
    This commit cherry picks *only* the factorization of
    gasneti_platform_isWSL(), which was implemented in commit
    ae2519a41a69afaa75f3810e0521939708a2a669.
    
    No other portion of that commit is being brought over to EX.

--- gasnet_internal.h
+++ gasnet_internal.h
@@ -255,6 +255,12 @@ GASNETI_MALLOCP(_gasneti_strndup)
 
 extern void gasneti_freezeForDebugger(void);
 
+#if PLATFORM_OS_LINUX || PLATFORM_OS_WSL
+  // dynamic check for Linux flavor, to detect binary porting
+  // return non-zero iff this Linux system is actually Microsoft Windows Subsystem for Linux
+  extern int gasneti_platform_isWSL(void);
+#endif
+
 /* GASNET_DEBUG_VERBOSE is set by configure to request job startup and general 
    status messages on stderr 
 */
