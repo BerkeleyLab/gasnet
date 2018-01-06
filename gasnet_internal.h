9197515d1e7c8d25e77511320a8bcc2668c670c6
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-01-05 16:27:53 -0800

    gasneti_THUNK_{EP,CLIENT,SEGMENT} must not SEGV
    This commit changes the gasneti_THUNK definitions to return NULL if
    called "too early", rather than (for all but TM) dereferencing the
    NULL value of gasneti_THUNK_TM.

--- gasnet_internal.h
+++ gasnet_internal.h
@@ -354,13 +354,22 @@ void gasneti_free_tm(gasneti_TM_t tm);
 // updated to propogate Client, TM, EP and Segment arguments.
 // For the time being there is at most one of each of those objects.
 //
-// Results are undefined prior to return from gex_Client_Init() or gasnet_init().
+// Results are NULL prior to return from gex_Client_Init() or gasnet_init().
 //
 extern gasneti_TM_t gasneti_thing_that_goes_thunk_in_the_dark;
 #define gasneti_THUNK_TM      gasneti_export_tm(gasneti_thing_that_goes_thunk_in_the_dark)
-#define gasneti_THUNK_EP      gasneti_export_ep(gasneti_thing_that_goes_thunk_in_the_dark->_ep)
-#define gasneti_THUNK_CLIENT  gasneti_export_client(gasneti_thing_that_goes_thunk_in_the_dark->_ep->_client)
-#define gasneti_THUNK_SEGMENT gasneti_export_segment(gasneti_thing_that_goes_thunk_in_the_dark->_ep->_segment)
+#define gasneti_THUNK_EP \
+        gasneti_export_ep(     gasneti_thing_that_goes_thunk_in_the_dark \
+                               ? gasneti_thing_that_goes_thunk_in_the_dark->_ep \
+                               : NULL )
+#define gasneti_THUNK_CLIENT \
+        gasneti_export_client( gasneti_thing_that_goes_thunk_in_the_dark \
+                               ? gasneti_thing_that_goes_thunk_in_the_dark->_ep->_client \
+                               : NULL )
+#define gasneti_THUNK_SEGMENT \
+        gasneti_export_segment( gasneti_thing_that_goes_thunk_in_the_dark \
+                                ? gasneti_thing_that_goes_thunk_in_the_dark->_ep->_segment \
+                                : NULL )
 
 /* ------------------------------------------------------------------------------------ */
 // Internal helpers for data types (like ctypes)
