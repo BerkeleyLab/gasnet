e64f9d9859f8de8e4ed29d105aba597b23632df1
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-02-22 22:32:27 -0800

    mpi: reduce cost of single-supernode AMPoll
    This commit ensures that in a single-supernode run with mpi-conduit,
    an AMPoll will make a call to AM_Poll() only once out of every 256
    calls.

--- mpi-conduit/gasnet_core.c
+++ mpi-conduit/gasnet_core.c
@@ -544,7 +544,14 @@ extern int gasnetc_AMPoll(void) {
   gasneti_AMPSHMPoll(0);
 #endif
   AMLOCK();
+  static int cntr;
+  // In single-supernode case never need to poll the network for client AMs.
+  // However, we'll still AM_Poll() every 256th call for orderly exit handling.
+  if ((gasneti_mysupernode.grp_count > 1) || !(0xff & cntr++)) {
     GASNETI_AM_SAFE_NORETURN(retval, AM_Poll(gasnetc_bundle));
+  } else {
+    retval = 0;
+  }
   AMUNLOCK();
   if_pf (retval) GASNETI_RETURN_ERR(RESOURCE);
   else return GASNET_OK;
