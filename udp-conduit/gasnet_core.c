3abe2c1e196d169b4b7bf66c57563eea1b88ec36
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-02-22 21:47:17 -0800

    udp: reduce cost of single-supernode AMPoll
    This commit ensures that in a single-supernode run with udp-conduit,
    an AMPoll will not make an expensive call to AM_Poll().  Instead it
    will call only AMUDP_SPMDHandleControlTraffic().

--- udp-conduit/gasnet_core.c
+++ udp-conduit/gasnet_core.c
@@ -636,7 +636,14 @@ extern int gasnetc_AMPoll(void) {
   gasneti_AMPSHMPoll(0);
 #endif
   AMLOCK();
+  // In single-supernode case never need to poll the network for client AMs.
+  // However, we'll still check for control traffic for orderly exit handling.
+  if (gasneti_mysupernode.grp_count > 1) {
     GASNETI_AM_SAFE_NORETURN(retval,AM_Poll(gasnetc_bundle));
+  } else {
+    // TODO-EX: a lock-free peek would allow elimination of a lock cycle
+    GASNETI_AM_SAFE_NORETURN(retval,AMUDP_SPMDHandleControlTraffic(NULL));
+  }
   AMUNLOCK();
   if_pf (retval) GASNETI_RETURN_ERR(RESOURCE);
   else return GASNET_OK;
