c752d923fe745d4571de60663d81f095f854d1c5
bonachea <dobonachea@lbl.gov>
2018-03-07 22:24:11 -0800

    Fix gasneti_offsetof for Pathscale
    Apply the same XLC workaround for Pathscale, where dynamic field
    member causes a compiler crash.

--- gasnet_basic.h
+++ gasnet_basic.h
@@ -188,7 +188,7 @@
 // containing non-constant expressions. If the member_field_expr contains only
 // constant expressions then the result is a constant, otherwise it is non-constant.
 // Some compilers (eg XLC) complain about using stddef.h offsetof() for this purpose
-#if PLATFORM_COMPILER_XLC
+#if PLATFORM_COMPILER_XLC || PLATFORM_COMPILER_PATHSCALE
   #define gasneti_offsetof(type, member_field_expr) \
           ((size_t)(uintptr_t)&(((type *)NULL)->member_field_expr))
 #elif PLATFORM_COMPILER_CRAY
