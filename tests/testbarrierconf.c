6451ced099787ed159a8df95a63170696492ac65
bonachea <dobonachea@lbl.gov>
2018-06-04 23:45:38 -0700

    testbarrierconf: reduce iterations on non-scalable portion of test
    (cherry picked from commit e2242d9aca0942919d2b829f53de2217cf703810)

--- tests/testbarrierconf.c
+++ tests/testbarrierconf.c
@@ -410,7 +410,8 @@ static void * doTest(void *arg) {
       MSG0("WARNING: pair mismatch tests skipped (only 1 node)");
     }
 
-    if (nodes > 2) {
+    if (nodes > 2 && 
+        i < MAX(2,iters/1000)) { // limit iterations of the node^2 test below
       int j, k;
 
       for (j = 0; j < nodes; ++j) {
