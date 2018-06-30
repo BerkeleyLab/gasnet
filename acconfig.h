f6e12a47ed1121d2621a76c985b4f16fa81e0f47
bonachea <dobonachea@lbl.gov>
2018-06-30 00:35:35 -0700

    Remove support for Altix and MMTIMER
    We no longer have access to the obsolete SGI Altix hardware and
    therefore are unable to maintain support.
    
    Also remove the MMTIMER Intel Multimedia Timer device interface code
    that was only added to ensure tick timer synchronization during thread
    migration across Altix cabinets (bug 1622), and AFAIK MMTIMER has never
    been implemented on any other HPC-relevant system.
    
    (cherry picked from commit 65203432e17e65959101bc0184812e199b9c549e)

--- acconfig.h
+++ acconfig.h
@@ -410,9 +410,6 @@
 /* compiler is Apple's variant of GCC */
 #undef GASNETI_GCC_APPLE
 
-/* platform is an SGI Altix multiprocessor */
-#undef GASNETI_USE_MMTIMER
-
 /* platform is a Linux cluster running IBM PE software */
 #undef GASNETI_ARCH_IBMPE
 
