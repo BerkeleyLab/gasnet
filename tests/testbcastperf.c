f0c4fc58fa7dd8e959116e1a2d91a585155566bc
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-07-16 12:42:34 -0700

    tests: remove test[OP]perf.c
    This commit removes un-maintained tests/test[OP]perf.c (for 'OP' in
    'bcast', 'exchange', 'gall', 'reduce').

--- tests/testbcastperf.c
+++
@@ -1,4 +0,0 @@
-#define ALL_COLL_ENABLED 0
-#define BROADCAST_ENABLED 1
-
-#include "testcollperf.c"
--- tests/testgallperf.c
+++
@@ -1,4 +0,0 @@
-#define ALL_COLL_ENABLED 0
-#define GATHER_ALL_ENABLED 1
-
-#include "testcollperf.c"
