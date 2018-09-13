bac38e701a7f0942f599eebfd295030ec8301bd1
bonachea <dobonachea@lbl.gov>
2018-09-13 13:54:26 -0700

    fixup: Add some uses of gasneti_assert...
    Revert one use of gasneti_assert_always_ptr() on function pointers,
    which is not strictly C compliant.

--- gasnet_internal.c
+++ gasnet_internal.c
@@ -321,7 +321,7 @@ extern void gasneti_defaultAMHandler(gasnet_token_t token) {
   extern gasneti_handler_fn_t gasnetc_handler[];
   static int gasnetc_amregister(gasnet_handler_t index, gasneti_handler_fn_t fnptr) {
     /* register a single handler */
-    gasneti_assert_ptr(gasnetc_handler[index] ,==, gasneti_defaultAMHandler);
+    gasneti_assert(gasnetc_handler[index] == gasneti_defaultAMHandler);
     gasnetc_handler[index] = fnptr;
     return GASNET_OK;
   }
