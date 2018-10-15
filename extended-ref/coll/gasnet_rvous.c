b260f4cda9f3b2f93b053ce59b67c42e85bbd3c7
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-10-15 01:08:29 -0700

    coll-ex: RVous Broadcast via binomial tree
    This commit updates the logic for RVous-based Broadcast (used for
    out-of-segment src and dst when too large for Eager) to use a binomial
    tree, rather than the previous "flat tree".  This may not be the
    optimal geometry, but should be a measurable improvement over the
    previous implementation.
    
    Similar changes for Scatter and Gather are possible, but are
    non-trivial because use of some sort of scratch space would be
    required at the internal nodes of the tree.

--- extended-ref/coll/gasnet_rvous.c
+++ extended-ref/coll/gasnet_rvous.c
@@ -204,13 +204,18 @@ gasnete_coll_bcast_TreeRVGet(gasnet_team_handle_t team,
                                            sequence, coll_params->num_params, coll_params->param_list GASNETI_THREAD_PASS);
 }
 
-/* bcast RVous: root node uses AM Mediums to send to addrs provided by each node */
+/* bcast RVous: use AM Mediums to send to addrs provided by each node of a binimial tree */
 /* Requires GASNETE_COLL_GENERIC_OPT_P2P on all nodes */
 static int gasnete_coll_pf_bcast_RVous(gasnete_coll_op_t *op GASNETI_THREAD_FARG) {
+  gex_TM_t const tm = op->e_tm;
   gasnete_coll_generic_data_t *data = op->data;
   const gasnete_coll_broadcast_args_t *args = GASNETE_COLL_GENERIC_ARGS(data, broadcast);
   int result = 0;
   gex_Flags_t imm_flag = 0;
+
+  // TODO-EX: pre-compute quantities such as these at injection,
+  //          rather than repeatedly upon every poll.
+  gex_Rank_t rel_rank = gasnete_tm_binom_rel_root(tm, args->srcnode);
 
   switch (data->state) {
   case 0:	/* Optional IN barrier */
@@ -221,38 +226,52 @@ static int gasnete_coll_pf_bcast_RVous(gasnete_coll_op_t *op GASNETI_THREAD_FARG
     imm_flag = GEX_FLAG_IMMEDIATE; // execute next step w/ IMM flag only on first attempt
     data->state = 1; GASNETI_FALLTHROUGH
     
-  case 1:	/* Rendevous w/ root to pass addr */
-    if (op->team->myrank == args->srcnode) {
+  case 1:	/* Rendevous w/ parent to pass addr */
+    if (! rel_rank) {
       GASNETI_MEMCPY_SAFE_IDENTICAL(args->dst, args->src, args->nbytes);
     } else {
-      /* Send our addr to root */
-      if (gasnete_tm_p2p_send_rtr(op, data->p2p, args->srcnode, op->team->myrank,
+      /* Send our addr to parent */
+      gex_Rank_t parent = gasnete_tm_binom_parent(tm, rel_rank);
+      gex_Rank_t offset = gasnete_tm_binom_age(tm, rel_rank);
+      if (gasnete_tm_p2p_send_rtr(op, data->p2p, parent, offset,
                                   args->dst, args->nbytes, imm_flag GASNETI_THREAD_PASS)) {
         break; // back pressure
       }
     }
-    imm_flag = GEX_FLAG_IMMEDIATE; // execute next step w/ IMM flag only on first attempt
     data->state = 2; GASNETI_FALLTHROUGH
     
-  case 2:
-    if (op->team->myrank == args->srcnode) {
-      /* Root sends at most one AM per peer for each poll */
-      int done = 1;
-      for (gex_Rank_t i=0; i<op->team->total_ranks; ++i) {
-        if (i == op->team->myrank) continue;
-        done &= gasnete_tm_p2p_send_data(op, data->p2p, i, i, args->src,
-                                         args->nbytes, imm_flag GASNETI_THREAD_PASS);
-      }
-      if (!done) {break;}
+  case 2:	/* Wait for data to arrive, unless root */
+    if (! rel_rank) {
+       // Nothing to do
     } else if (gasnete_tm_p2p_send_done(data->p2p)) {
       gasneti_sync_reads();
     } else {
       /* Not all data has arrived yet */
       break;
     }
+    imm_flag = GEX_FLAG_IMMEDIATE; // execute next step w/ IMM flag only on first attempt
     data->state = 3; GASNETI_FALLTHROUGH
+
+  case 3: {	/* Send data */
+    gex_Rank_t child_cnt = gasnete_tm_binom_children(tm, rel_rank);
+    if (child_cnt) {
+      /* Internal nodes send at most one AM per peer for each poll */
+      const gex_Rank_t self = gex_TM_QueryRank(tm);
+      const gex_Rank_t size = gex_TM_QuerySize(tm);
+      int idx = child_cnt;
+      int done = 1;
+      while (idx--) { // Note reverse order for deepest subtree first
+        gex_Rank_t distance = 1 << idx;
+        gex_Rank_t peer = (distance >= size - self) ? self - (size - distance) : self + distance;
+        done &= gasnete_tm_p2p_send_data(op, data->p2p, peer, idx, args->dst,
+                                         args->nbytes, imm_flag GASNETI_THREAD_PASS);
+      }
+      if (!done) {break;}
+    }
+    data->state = 4; GASNETI_FALLTHROUGH
+  }
     
-  case 3:	/* Optional OUT barrier */
+  case 4:	/* Optional OUT barrier */
     if (!gasnete_coll_generic_outsync(op->team, data)) {
       break;
     }
