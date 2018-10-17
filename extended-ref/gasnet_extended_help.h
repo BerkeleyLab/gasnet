56200e944bb5ff00fa72bb8fee2f39b2dddeebc8
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-10-17 08:56:14 +0200

    fixup: bug 3793: fix a testval warning on SPARC32 gcc-8/opt
    This commit fixes a minor error in porting the bug 3793 fix from G-1.

--- extended-ref/gasnet_extended_help.h
+++ extended-ref/gasnet_extended_help.h
@@ -326,14 +326,14 @@ typedef union {
   #define GASNETI_CHECKPSHM_GETVAL(tm,rank,src,nbytes) do {                     \
     size_t const _nbytes = (nbytes);                                            \
     gasneti_assume(_nbytes > 0); /* bug 3793 */                                 \
-    gasneti_assume(_nbytes <= sizeof(gasnet_register_value_t));                 \
+    gasneti_assume(_nbytes <= sizeof(gex_RMA_Value_t));                         \
     if (gasneti_pshm_in_supernode(tm,rank)) {                                   \
       GASNETE_VALUE_RETURN(gasneti_pshm_addr2local(tm,rank,src), _nbytes);       \
     }} while(0)
   #define GASNETI_CHECKPSHM_PUTVAL(tm,rank,dest,value,nbytes) do {              \
     size_t const _nbytes = (nbytes);                                            \
     gasneti_assume(_nbytes > 0); /* bug 3793 */                                 \
-    gasneti_assume(_nbytes <= sizeof(gasnet_register_value_t));                 \
+    gasneti_assume(_nbytes <= sizeof(gex_RMA_Value_t));                         \
     if (gasneti_pshm_in_supernode(tm,rank)) {                                   \
       GASNETE_VALUE_ASSIGN(gasneti_pshm_addr2local(tm,rank,dest), value, _nbytes); \
       gasnete_loopbackput_memsync();                                            \
