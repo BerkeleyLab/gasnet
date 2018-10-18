BerkeleyLab/gasnet

commit 73d36b2dfe4fea5e0fb9889b5a8baed68cbac356

fixupx2: Echo fatal errors/signals messages to tracefile
Rewrite the logic to satisfy compilers that foolishly bitch about libc
preconditions in intentionally and obviously unreachable code.

Also correct the va_start/end logic to be strictly compliant (the state
of va_list after vfprintf is technically indeterminate).

(cherry picked from commit 7259fc14d9e7d1c1637ff1eca89efdfada150ca0)

gasnet_internal.c
@@ -407,15 +407,16 @@ void gasneti_defaultSignalHandler(int sig) {
       oldsigpipe = gasneti_reghandler(SIGPIPE, SIG_IGN);
 
       GASNETC_FATALSIGNAL_CALLBACK(sig); /* give conduit first crack at it */
-      #define CRASH_ERR "*** Caught a fatal signal: %s(%i) on node %i/%i\n", \
-                        signame, sig, (int)gasnet_mynode(), (int)gasnet_nodes()
-      fprintf(stderr, CRASH_ERR);
-      fflush(stderr);
-      if (GASNETI_MAYBE_TRACEFILE) {
-        fprintf(GASNETI_MAYBE_TRACEFILE,CRASH_ERR);
-        fflush(GASNETI_MAYBE_TRACEFILE);
+
+      FILE * streams[] = { stderr, GASNETI_MAYBE_TRACEFILE };
+      for (int s = 0; s < sizeof(streams)/sizeof(streams[0]); s++) {
+        FILE *stream = streams[s];
+        if (stream) {
+          fprintf(stream, "*** Caught a fatal signal: %s(%i) on node %i/%i\n", 
+                        signame, sig, (int)gasnet_mynode(), (int)gasnet_nodes());
+          fflush(stream);
+        }
       }
-      #undef CRASH_ERR
 
       gasnett_freezeForDebuggerErr(); /* allow freeze */
 

gasnet_tools.c
@@ -563,15 +563,16 @@ extern double gasneti_tick_metric(int idx) {
   return _gasneti_tick_metric[idx];
 }
 /* ------------------------------------------------------------------------------------ */
-#if GASNET_TRACE
-  FILE *gasneti_tracefile; // intentional tentative defn
-  #define GASNETI_MAYBE_TRACEFILE gasneti_tracefile
-#else
-  #define GASNETI_MAYBE_TRACEFILE ((FILE *)NULL)
+#ifndef GASNETI_MAYBE_TRACEFILE
+  #if GASNET_TRACE
+    FILE *gasneti_tracefile; // intentional tentative defn
+    #define GASNETI_MAYBE_TRACEFILE gasneti_tracefile
+  #else
+    #define GASNETI_MAYBE_TRACEFILE ((FILE *)NULL)
+  #endif
 #endif
 volatile int gasnet_frozen = 0;
 extern void gasneti_fatalerror(const char *msg, ...) {
-  va_list argptr;
   #ifndef GASNETI_FATALERROR_LEN
   #define GASNETI_FATALERROR_LEN 80
   #endif
@@ -580,26 +581,26 @@ extern void gasneti_fatalerror(const char *msg, ...) {
   const size_t maxmsg = sizeof(expandedmsg)-sizeof(prefix)-4;
   const size_t msglen = strlen(msg);
 
-  va_start(argptr, msg); /*  pass in last argument */
-    if (msglen <= maxmsg) { /* short enough to send to stderr in a single operation */
-      strcpy(expandedmsg, prefix);
-      strncat(expandedmsg, msg, maxmsg);
-      if (expandedmsg[strlen(expandedmsg)-1] != '\n') strcat(expandedmsg, "\n");
-      vfprintf(stderr, expandedmsg, argptr);
-      if (GASNETI_MAYBE_TRACEFILE) vfprintf(GASNETI_MAYBE_TRACEFILE, expandedmsg, argptr);
-    } else { /* long format msg */
-      fprintf(stderr, prefix);
-      vfprintf(stderr, msg, argptr);
-      if (msg[strlen(msg)-1] != '\n') fprintf(stderr, "\n");
-      if (GASNETI_MAYBE_TRACEFILE) {
-        fprintf(GASNETI_MAYBE_TRACEFILE, prefix);
-        vfprintf(GASNETI_MAYBE_TRACEFILE, msg, argptr);
-        if (msg[strlen(msg)-1] != '\n') fprintf(GASNETI_MAYBE_TRACEFILE, "\n");
-      }
+  FILE * streams[] = { stderr, GASNETI_MAYBE_TRACEFILE };
+  for (int s = 0; s < sizeof(streams)/sizeof(streams[0]); s++) {
+    FILE *stream = streams[s];
+    if (stream) {
+      va_list argptr;
+      va_start(argptr, msg); /*  pass in last argument */
+        if (msglen <= maxmsg) { /* short enough to send to stderr in a single operation */
+          strcpy(expandedmsg, prefix);
+          strncat(expandedmsg, msg, maxmsg);
+          if (expandedmsg[strlen(expandedmsg)-1] != '\n') strcat(expandedmsg, "\n");
+          vfprintf(stream, expandedmsg, argptr);
+        } else { /* long format msg */
+          fprintf(stream, prefix);
+          vfprintf(stream, msg, argptr);
+          if (msg[strlen(msg)-1] != '\n') fprintf(stream, "\n");
+        }
+      va_end(argptr);
+      fflush(stream);
     }
-    fflush(stderr);
-    if (GASNETI_MAYBE_TRACEFILE) fflush(GASNETI_MAYBE_TRACEFILE);
-  va_end(argptr);
+  }
 
   gasnett_freezeForDebuggerErr(); /* allow freeze */
 

commit d9e3f557e733bc6b880d7971182578d693ae9075

Several improvements to fatal paths
* Remove dynamic allocation from assertion failure paths

Assertions were previously calling gasneti_current_loc to format
location information into a malloced string, which might be dangerous
on a crash path if the malloc heap is borked.

* Break gasneti_fatalerror into separate functions to allow separate
re-use of the message formatting and aborting tasks.

* Use the factored logic to improve error reporting in gasneti_defaultSignalHandler.
Non-fatal signals caught by GASNet are now also reported into the tracefile.

* Fix an unreported gasneti_fatalerror bug introduced in 73d36b2 / 7259fc1
This bug could cause data corruption and secondary crashes inside
gasneti_fatalerror when the tracefile was enabled, leading to loss of
backtraces and incomplete tracefiles.

gasnet_internal.c
@@ -412,15 +412,7 @@ void gasneti_defaultSignalHandler(int sig) {
 
       GASNETC_FATALSIGNAL_CALLBACK(sig); /* give conduit first crack at it */
 
-      FILE * streams[] = { stderr, GASNETI_MAYBE_TRACEFILE };
-      for (int s = 0; s < sizeof(streams)/sizeof(streams[0]); s++) {
-        FILE *stream = streams[s];
-        if (stream) {
-          fprintf(stream, "*** Caught a fatal signal: %s(%i) on node %i/%i\n", 
-                        signame, sig, (int)gasnet_mynode(), (int)gasnet_nodes());
-          fflush(stream);
-        }
-      }
+      gasneti_console_message("Caught a fatal signal", "%s(%i)", signame, sig);
 
       gasnett_freezeForDebuggerErr(); /* allow freeze */
 
@@ -449,9 +441,7 @@ void gasneti_defaultSignalHandler(int sig) {
       }
 
       oldsigpipe = gasneti_reghandler(SIGPIPE, SIG_IGN);
-      fprintf(stderr,"*** Caught a signal: %s(%i) on node %i/%i\n",
-        signame, sig, (int)gasnet_mynode(), (int)gasnet_nodes()); 
-      fflush(stderr);
+      gasneti_console_message("Caught a signal", "%s(%i)", signame, sig);
       (void) gasneti_reghandler(SIGPIPE, oldsigpipe);
 
       do_raise(SIGQUIT);

gasnet_toolhelp.h
@@ -14,6 +14,7 @@
 #include <errno.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <unistd.h>
 #if GASNETI_THREADS
   #if PLATFORM_OS_LINUX || PLATFORM_OS_UCLINUX
@@ -59,13 +60,20 @@ extern void gasneti_filesystem_sync(void);
 #endif
 
 extern char *gasneti_build_loc_str(const char *_funcname, const char *_filename, int _linenum);
+// NOTE: this returns a malloced buffer!
 #define gasneti_current_loc gasneti_build_loc_str(GASNETI_CURRENT_FUNCTION,__FILE__,__LINE__)
+
+GASNETI_FORMAT_PRINTF(_gasneti_assert_fail,4,5,
+extern void _gasneti_assert_fail(const char *_funcname, const char *_filename, int _linenum,
+                                 const char *_fmt, ...) GASNETI_NORETURN);
+GASNETI_NORETURNP(_gasneti_assert_fail)
 
 /* gasneti_assert_always():
  * an assertion that never compiles away - for sanity checks in non-critical paths 
  */
 #define gasneti_assert_always(expr) \
-    (GASNETT_PREDICT_TRUE(expr) ? (void)0 : gasneti_fatalerror("Assertion failure at %s: %s", gasneti_current_loc, #expr))
+    (GASNETT_PREDICT_TRUE(expr) ? (void)0 : \
+     _gasneti_assert_fail(GASNETI_CURRENT_FUNCTION,__FILE__,__LINE__,"%s",#expr))
 
 /* gasneti_assert():
  * an assertion that compiles away in non-debug mode - for sanity checks in critical paths 
@@ -91,10 +99,11 @@ extern char *gasneti_build_loc_str(const char *_funcname, const char *_filename,
       int const _gaa_negval = (_gaa_op1 < 0) || (_gaa_op2 < 0);     \
       int const _gaa_decwid = (_gaa_bigval ? 20 : 11);              \
       int const _gaa_hexwid = (_gaa_bigval || _gaa_negval ? 16 : 8);\
-      gasneti_fatalerror("Assertion failure at %s: %s %s %s\n"      \
+      _gasneti_assert_fail(GASNETI_CURRENT_FUNCTION,__FILE__,__LINE__,\
+         "%s %s %s\n"                                               \
          "   op1 : %*" PRId64 " (0x%0*" PRIx64 ") == %s\n"          \
          "   op2 : %*" PRId64 " (0x%0*" PRIx64 ") == %s\n"          \
-       , gasneti_current_loc, #op1, #operator, #op2                 \
+       , #op1, #operator, #op2                                      \
        , _gaa_decwid, _gaa_op1, _gaa_hexwid, _gaa_op1, #op1         \
        , _gaa_decwid, _gaa_op2, _gaa_hexwid, _gaa_op2, #op2         \
       );                                                            \
@@ -108,10 +117,11 @@ extern char *gasneti_build_loc_str(const char *_funcname, const char *_filename,
                               ((uint32_t)_gaa_op2 != _gaa_op2);     \
       int const _gaa_decwid = (_gaa_bigval ? 20 : 11);              \
       int const _gaa_hexwid = (_gaa_bigval ? 16 : 8);               \
-      gasneti_fatalerror("Assertion failure at %s: %s %s %s\n"      \
+      _gasneti_assert_fail(GASNETI_CURRENT_FUNCTION,__FILE__,__LINE__,\
+         "%s %s %s\n"                                               \
          "   op1 : %*" PRIu64 " (0x%0*" PRIx64 ") == %s\n"          \
          "   op2 : %*" PRIu64 " (0x%0*" PRIx64 ") == %s\n"          \
-       , gasneti_current_loc, #op1, #operator, #op2                 \
+       , #op1, #operator, #op2                                      \
        , _gaa_decwid, _gaa_op1, _gaa_hexwid, _gaa_op1, #op1         \
        , _gaa_decwid, _gaa_op2, _gaa_hexwid, _gaa_op2, #op2         \
       );                                                            \
@@ -127,10 +137,11 @@ extern char *gasneti_build_loc_str(const char *_funcname, const char *_filename,
     const void * const _gaa_op2 = _gasneti_voidp_cvt(op2);          \
     if (!GASNETT_PREDICT_TRUE(_gaa_op1 operator _gaa_op2)) {        \
       int const _gaa_hexwid = 2*sizeof(void *);                     \
-      gasneti_fatalerror("Assertion failure at %s: %s %s %s\n"      \
+      _gasneti_assert_fail(GASNETI_CURRENT_FUNCTION,__FILE__,__LINE__,\
+         "%s %s %s\n"                                               \
          "   op1 : 0x%0*" PRIxPTR " == %s\n"                        \
          "   op2 : 0x%0*" PRIxPTR " == %s\n"                        \
-       , gasneti_current_loc, #op1, #operator, #op2                 \
+       , #op1, #operator, #op2                                      \
        , _gaa_hexwid, (uintptr_t)_gaa_op1, #op1                     \
        , _gaa_hexwid, (uintptr_t)_gaa_op2, #op2                     \
       );                                                            \
@@ -140,10 +151,11 @@ extern char *gasneti_build_loc_str(const char *_funcname, const char *_filename,
     double const _gaa_op1 = (op1);                                  \
     double const _gaa_op2 = (op2);                                  \
     if (!GASNETT_PREDICT_TRUE(_gaa_op1 operator _gaa_op2)) {        \
-      gasneti_fatalerror("Assertion failure at %s: %s %s %s\n"      \
+      _gasneti_assert_fail(GASNETI_CURRENT_FUNCTION,__FILE__,__LINE__,\
+         "%s %s %s\n"                                               \
          "   op1 : %#13.6g (0x%016" PRIx64 ") == %s\n"              \
          "   op2 : %#13.6g (0x%016" PRIx64 ") == %s\n"              \
-       , gasneti_current_loc, #op1, #operator, #op2                 \
+       , #op1, #operator, #op2                                      \
        , _gaa_op1, *(uint64_t*)&_gaa_op1, #op1                      \
        , _gaa_op2, *(uint64_t*)&_gaa_op2, #op2                      \
       );                                                            \
@@ -229,9 +241,9 @@ extern char *gasneti_build_loc_str(const char *_funcname, const char *_filename,
       char _tmp[128];                                       \
       strncpy(_tmp, strerror(_retval), sizeof(_tmp));       \
       _tmp[sizeof(_tmp)-1] = '\0';                          \
-      gasneti_fatalerror(#op": %s(%i), errno=%s(%i) at %s", \
-        _tmp, _retval, strerror(errno), errno,              \
-        gasneti_current_loc);                               \
+      _gasneti_assert_fail(GASNETI_CURRENT_FUNCTION,__FILE__,__LINE__,\
+        "%s yielded %s(%i), errno=%s(%i)",                  \
+        #op, _tmp, _retval, strerror(errno), errno);        \
     }                                                       \
   } while (0)
   #define gasneti_assert_nzeroret(op) do {                  \
@@ -241,14 +253,14 @@ extern char *gasneti_build_loc_str(const char *_funcname, const char *_filename,
       char _tmp[128];                                       \
       strncpy(_tmp, strerror(_retval), sizeof(_tmp));       \
       _tmp[sizeof(_tmp)-1] = '\0';                          \
-      gasneti_fatalerror(#op": %s(%i), errno=%s(%i) at %s", \
-        _tmp, _retval, strerror(errno), errno,              \
-        gasneti_current_loc);                               \
+      _gasneti_assert_fail(GASNETI_CURRENT_FUNCTION,__FILE__,__LINE__,\
+        "%s yielded %s(%i), errno=%s(%i)",                  \
+        #op, _tmp, _retval, strerror(errno), errno);        \
     }                                                       \
   } while (0)
 #else
-  #define gasneti_assert_zeroret(op)  op
-  #define gasneti_assert_nzeroret(op) op
+  #define gasneti_assert_zeroret(op)  do { op; } while(0)
+  #define gasneti_assert_nzeroret(op) do { op; } while(0)
 #endif
 
 /* return physical memory of machine
@@ -257,9 +269,17 @@ extern uint64_t gasneti_getPhysMemSz(int _failureIsFatal);
 
 extern const char *gasneti_procid_str; // process identifier for error messages
 
+GASNETI_FORMAT_PRINTF(gasneti_console_message,2,3, // output a formatted message with a prefix type
+extern void gasneti_console_message(const char *_prefix, const char *_msg, ...));
+GASNETI_FORMAT_PRINTF(gasneti_console_messageVA,2,0,
+extern void gasneti_console_messageVA(const char *_prefix, const char *_msg, va_list _argptr));
+
 GASNETI_FORMAT_PRINTF(gasneti_fatalerror,1,2,
 extern void gasneti_fatalerror(const char *_msg, ...) GASNETI_NORETURN);
 GASNETI_NORETURNP(gasneti_fatalerror)
+
+extern void gasneti_error_abort(void) GASNETI_NORETURN; // perform pre-abort actions then abort
+GASNETI_NORETURNP(gasneti_error_abort)
 
 extern void gasneti_killmyprocess(int _exitcode) GASNETI_NORETURN;
 GASNETI_NORETURNP(gasneti_killmyprocess)

gasnet_tools.c
@@ -578,38 +578,53 @@ extern double gasneti_tick_metric(int idx) {
 extern const char *gasneti_procid_str;
 const char *gasneti_procid_str = NULL;
 
-extern void gasneti_fatalerror(const char *msg, ...) {
-  #ifndef GASNETI_FATALERROR_LEN
-  #define GASNETI_FATALERROR_LEN 128
+extern void gasneti_console_messageVA(const char *prefix, const char *msg, va_list argptr) {
+  #ifndef GASNETI_CONSOLEMSG_PREFIX_LEN
+  #define GASNETI_CONSOLEMSG_PREFIX_LEN 128
   #endif
-  char expandedmsg[GASNETI_FATALERROR_LEN];
+  char expandedmsg[GASNETI_CONSOLEMSG_PREFIX_LEN+16];
   if (gasneti_procid_str) {
-    snprintf(expandedmsg, sizeof(expandedmsg)-4, "*** FATAL ERROR (%s): ", gasneti_procid_str);
+    snprintf(expandedmsg, sizeof(expandedmsg)-4, "*** %s (%s): ", prefix, gasneti_procid_str);
   } else {
-    snprintf(expandedmsg, sizeof(expandedmsg)-4, "*** FATAL ERROR: ");
+    snprintf(expandedmsg, sizeof(expandedmsg)-4, "*** %s: ", prefix);
   }
   const size_t maxmsg = sizeof(expandedmsg)-4 - strlen(expandedmsg);
   const size_t msglen = strlen(msg);
+
+  int isshort = 0;
+  if (msglen <= maxmsg) { /* short enough to send to stderr in a single operation */
+    strncat(expandedmsg, msg, maxmsg);
+    if (expandedmsg[strlen(expandedmsg)-1] != '\n') strcat(expandedmsg, "\n");
+    isshort = 1;
+  }
 
   FILE * streams[] = { stderr, GASNETI_MAYBE_TRACEFILE };
   for (int s = 0; s < sizeof(streams)/sizeof(streams[0]); s++) {
     FILE *stream = streams[s];
     if (stream) {
-      va_list argptr;
-      va_start(argptr, msg); /*  pass in last argument */
-        if (msglen <= maxmsg) { /* short enough to send to stderr in a single operation */
-          strncat(expandedmsg, msg, maxmsg);
-          if (expandedmsg[strlen(expandedmsg)-1] != '\n') strcat(expandedmsg, "\n");
-          vfprintf(stream, expandedmsg, argptr);
+      va_list args;
+      va_copy(args, argptr);
+        if (isshort) {
+          vfprintf(stream, expandedmsg, args);
         } else { /* long format msg */
           fputs(expandedmsg, stream);
-          vfprintf(stream, msg, argptr);
-          if (msg[strlen(msg)-1] != '\n') fprintf(stream, "\n");
+          vfprintf(stream, msg, args);
+          if (msg[msglen-1] != '\n') fprintf(stream, "\n");
         }
-      va_end(argptr);
+      va_end(args);
       fflush(stream);
     }
   }
+}
+
+extern void gasneti_console_message(const char *prefix, const char *msg, ...) {
+  va_list argptr;
+  va_start(argptr, msg); /*  pass in last argument */
+    gasneti_console_messageVA(prefix, msg, argptr);
+  va_end(argptr);
+}
+
+extern void gasneti_error_abort(void) {
 
   gasnett_freezeForDebuggerErr(); /* allow freeze */
 
@@ -623,6 +638,51 @@ extern void gasneti_fatalerror(const char *msg, ...) {
 
   abort();
 }
+
+extern void gasneti_fatalerror(const char *msg, ...) {
+  va_list argptr;
+  va_start(argptr, msg); /*  pass in last argument */
+    gasneti_console_messageVA("FATAL ERROR", msg, argptr);
+  va_end(argptr);
+  gasneti_error_abort();
+}
+
+extern void _gasneti_assert_fail(const char *funcname, const char *filename, int linenum,
+                                 const char *fmt, ...) {
+  #ifndef GASNETI_ASSERT_FMT_LEN
+  #define GASNETI_ASSERT_FMT_LEN 256
+  #endif
+  #ifndef GASNETI_ASSERT_NAME_LEN
+  #define GASNETI_ASSERT_NAME_LEN  80
+  #endif
+  // use the last NAME_LEN characters of funcname and filename
+  if (!funcname) funcname = "";
+  size_t funclen = strlen(funcname);
+  if (funclen > GASNETI_ASSERT_NAME_LEN) funcname += (funclen - GASNETI_ASSERT_NAME_LEN);
+  if (!filename || !*filename) filename = "*unknown file*";
+  size_t filelen = strlen(filename);
+  if (filelen > GASNETI_ASSERT_NAME_LEN) filename += (filelen - GASNETI_ASSERT_NAME_LEN);
+
+  // prepend formatted location info to the assertion format string
+  char expandedfmt[GASNETI_ASSERT_FMT_LEN];
+  if (*funcname)
+    snprintf(expandedfmt, sizeof(expandedfmt), 
+             "Assertion failure in %s%s at %s:%i: %s",
+             funcname, (funcname[strlen(funcname)-1] != ')'?"()":""),
+             filename, linenum, fmt);
+  else
+    snprintf(expandedfmt, sizeof(expandedfmt), 
+             "Assertion failure at %s:%i: %s",
+             filename, linenum, fmt);
+
+  // generate the fatal error and crash
+  va_list argptr;
+  va_start(argptr, fmt); /*  pass in last argument */
+    gasneti_console_messageVA("FATAL ERROR", expandedfmt, argptr);
+  va_end(argptr);
+  gasneti_error_abort();
+}
+
 /* ------------------------------------------------------------------------------------ */
 extern void gasneti_killmyprocess(int exitcode) {
   /* wrapper for _exit() that does the "right thing" to immediately kill this process */