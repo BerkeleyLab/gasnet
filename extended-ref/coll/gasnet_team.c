ab41f4754ac54ca7cfa54be4897a3ee9062bd409
Paul H. Hargrove <PHHargrove@lbl.gov>
2018-11-21 20:50:53 -0800

    coll-ex: correct barrier usage in Split
    This commit converts the internal barrier usage in gex_TM_Split() to
    use the same "consensus barrier" as all other collectives.  This is
    required to ensure Split can be intermixed with other collectives
    (including barriers) on the parent team.

--- extended-ref/coll/gasnet_team.c
+++ extended-ref/coll/gasnet_team.c
@@ -383,8 +383,8 @@ gasnet_team_handle_t gasnete_coll_team_split(gasnet_team_handle_t team,
   /* short-circuit if excluded */
   if (mycolor == -1) {
     gasneti_free(all_args);
-    gasnete_coll_barrier(team, 0, GASNET_BARRIERFLAG_UNNAMED GASNETI_THREAD_PASS);
-    gasnete_coll_barrier(team, 0, GASNET_BARRIERFLAG_UNNAMED GASNETI_THREAD_PASS);
+    gasnete_coll_consensus_barrier(team GASNETI_THREAD_PASS);
+    gasnete_coll_consensus_barrier(team GASNETI_THREAD_PASS);
     return NULL;
   }
 
@@ -423,7 +423,7 @@ gasnet_team_handle_t gasnete_coll_team_split(gasnet_team_handle_t team,
   
   /* create a team */
   new_team_id = 0;
-  gasnete_coll_barrier(team, 0, GASNET_BARRIERFLAG_UNNAMED GASNETI_THREAD_PASS);
+  gasnete_coll_consensus_barrier(team GASNETI_THREAD_PASS);
 
 #ifdef DEBUG_TEAM
   fprintf(stderr, "gasnete_coll_team_split: new_total_ranks %u, new_myrank %u.\n",
@@ -435,7 +435,7 @@ gasnet_team_handle_t gasnete_coll_team_split(gasnet_team_handle_t team,
   newteam = gasnete_coll_team_create(new_total_ranks, new_myrank, rel2act_map, segments GASNETI_THREAD_PASS);
   
   gasneti_free(rel2act_map);
-  gasnete_coll_barrier(team, 0, GASNET_BARRIERFLAG_UNNAMED GASNETI_THREAD_PASS);
+  gasnete_coll_consensus_barrier(team GASNETI_THREAD_PASS);
 #ifdef GASNETI_USE_FCA
   gasnet_team_fca_enable(newteam);
 #endif
