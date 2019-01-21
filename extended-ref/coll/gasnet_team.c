3e106c4d1c47ff4539e8e13f154b0be5a84d4103
Paul H. Hargrove <PHHargrove@lbl.gov>
2019-01-20 22:21:27 -0800

    coll: simplify consensus barrier code
    This commit adjusts the logic for the consensus barrier to eliminate a
    single-bit left shift from the critical path.  This should make the
    code simpler to understand as well.
    
    (cherry picked from commit 6d367a06c0ae3ce93388d077f30db544c6c909ca)

--- extended-ref/coll/gasnet_team.c
+++ extended-ref/coll/gasnet_team.c
@@ -136,8 +136,7 @@ static void initialize_team_fields(gasnete_coll_team_t team,
   team->autotune_info = gasnete_coll_autotune_init(team, myrank, num_members, 
                                                    team->my_images, team->total_images,
                                                    smallest_scratch_seg GASNETI_THREAD_PASS);
-  team->consensus_issued_id = 0xfffffff8;  // Intentionally near to wrap-around
-  team->consensus_id = team->consensus_issued_id << 1;
+  team->consensus_id = team->consensus_issued_id = 0xfffffff8;  // Intentionally near to wrap-around
   gasnete_coll_alloc_new_scratch_status(team);
   gasneti_weakatomic_set(&team->num_multi_addr_collectives_started, 0, GASNETT_ATOMIC_WMB_PRE);
   if(!team->fixed_image_count && team->myrank ==0) {
