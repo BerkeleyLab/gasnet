fdb252477d2ad9677f609ba84ad38b258350eb70
Paul H. Hargrove <PHHargrove@lbl.gov>
2019-04-23 22:11:47 -0700

    ofi: address root cause of bug 3867
    This commit adds binding of a RECV CQ for the RDMA endpoint in order
    to eliminate the
       *** FATAL ERROR: fi_enable for rdma failed: -263
    reported as bug 3867.
    
    (cherry picked from commit be253c668ea157c6cc3a1adf1a12fb8dc17474c5)

--- ofi-conduit/gasnet_ofi.c
+++ ofi-conduit/gasnet_ofi.c
@@ -551,7 +551,7 @@ int gasnetc_ofi_init(void)
   if (FI_SUCCESS != ret) gasneti_fatalerror("fi_cq_open for am reply cq failed: %d\n", ret);
 
   /* Bind CQs to endpoints */
-  ret = fi_ep_bind(gasnetc_ofi_rdma_epfd, &gasnetc_ofi_tx_cqfd->fid, FI_TRANSMIT);
+  ret = fi_ep_bind(gasnetc_ofi_rdma_epfd, &gasnetc_ofi_tx_cqfd->fid, FI_TRANSMIT | FI_RECV);
   if (FI_SUCCESS != ret) gasneti_fatalerror("fi_ep_bind for tx_cq to rdma_epfd failed: %d\n", ret);
 
   ret = fi_ep_bind(gasnetc_ofi_request_epfd, &gasnetc_ofi_tx_cqfd->fid, FI_TRANSMIT);
