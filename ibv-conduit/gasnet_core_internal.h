e892682aa90c33613eff14f438a6eac34dde33b2
Paul H. Hargrove <PHHargrove@lbl.gov>
2019-07-30 17:47:48 -0700

    ibv: create/allow intra-nbrhd connections
    This commit changes connection and injection logic in ibv-conduit
    to permit intra-neighborhood communication over IBV.  This is required
    for multi-EP support, because non-primordial EPs are not cross-mapped by
    PSHM.  Additionally, this will be a requirement for eventual off-loaded
    remote atomics, which must perform all accesses via the HCA.  It is the
    future case for atomics which motivates not retaining logic to exclude
    loopback.
    
    To compensate for the loss of "!IN_NBRHD" assertions at the deepest
    levels of the call stack (where endpoint index info is unavailable),
    this commit introduces new ones in the Put, Get and AM code paths.  In
    the case of AMs the assertions is still "!IN_NBRHD".  However, for RMA
    operations the assertion is "!PRIMORDIAL || !IN_NBDHB".

--- ibv-conduit/gasnet_core_internal.h
+++ ibv-conduit/gasnet_core_internal.h
@@ -945,7 +945,6 @@ extern size_t			gasnetc_fh_align_mask;
 extern firehose_info_t		gasnetc_firehose_info;
 extern gasnetc_port_info_t      *gasnetc_port_tbl;
 extern int                      gasnetc_num_ports;
-extern gex_Rank_t            gasnetc_remote_nodes;
 #if GASNETC_DYNAMIC_CONNECT
   extern gasnetc_sema_t         gasnetc_zero_sema;
 #endif
