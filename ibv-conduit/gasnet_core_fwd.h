c7a56e1016af4f4579e76779ebc5cab3913a8457
Paul H. Hargrove <PHHargrove@lbl.gov>
2020-01-14 19:33:38 -0800

    NPAM: allow opt-in to passing numargs to Commit
    This commit provides a mechanism for conduits to opt-in to the passing
    of `numargs` to NPAM Commit calls, where previously this was omitted
    from NDEBUG builds (though passed and used for debug checks in DEBUG
    builds).

--- ibv-conduit/gasnet_core_fwd.h
+++ ibv-conduit/gasnet_core_fwd.h
@@ -90,6 +90,15 @@
 /* #define GASNETC_HAVE_NP_REQ_LONG 1 */
 /* #define GASNETC_HAVE_NP_REP_LONG 1 */
 
+  /* uncomment for each GASNETC_HAVE_NP_* enabled above if the Commit function
+     has the numargs argument even in an NDEBUG build (it is always passed in
+     DEBUG builds).
+   */
+//#define GASNETC_AM_COMMIT_REQ_MEDIUM_NARGS 1
+//#define GASNETC_AM_COMMIT_REP_MEDIUM_NARGS 1
+//#define GASNETC_AM_COMMIT_REQ_LONG_NARGS 1
+//#define GASNETC_AM_COMMIT_REP_LONG_NARGS 1
+
 #define GASNETI_AM_SRCDESC_EXTRA \
         int                 _have_flow;         \
         void *              _buf_alloc;         \
