fb43e952a15ea6468fcdae706795cb492ffcbeda
Paul H. Hargrove <PHHargrove@lbl.gov>
2020-03-27 11:56:30 -0700

    tests: avoid C++20 deprecation warnings
    This commit make small syntax changes in tests/test.h to avoid
    warnings from clang++ 10.0 in C++2a mode about the deprecation of
    increment on volatile-qualified types.

--- tests/test.h
+++ tests/test.h
@@ -770,7 +770,7 @@ static gex_TM_t _test_tm0;
       static volatile int phase = 0;
       const int myphase = phase;
       gasnett_mutex_lock(&(barrier[myphase].mutex));
-      barrier_count++;
+      barrier_count = 1 + barrier_count; // C++20 deprecates ++ and += on volatile
       if (barrier_count < local_pthread_count) {
 	/* CAUTION: changing the "do-while" to a "while" triggers a bug in the SunStudio 2006-08
          * compiler for x86_64.  See https://gasnet-bugs.lbl.gov/bugzilla/show_bug.cgi?id=1858
