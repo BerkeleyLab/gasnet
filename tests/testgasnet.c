d99bae387ce65d82aa2b6c3b8bd5b475f3e04e0a
bonachea <dobonachea@lbl.gov>
2020-06-11 23:38:22 -0700

    testgasnet: Fix a harmless valgrind warning
    Signature:
    ```
    ==22195== Syscall param socketcall.sendto(msg) points to uninitialised byte(s)
    ==22195==    at 0x4E45D23: ??? (syscall-template.S:81)
    ==22195==    by 0x6F6EE4: sendPacket(amudp_ep*, amudp_msg_t*, unsigned long, sockaddr_in, packet_type) (amudp_reqrep.cpp:91)
    ==22195==    by 0x6FD8C4: AMUDP_RequestGeneric(amudp_category_t, amudp_ep*, unsigned int, unsigned char, void*, unsigned long, unsigned long, int, __va_list_tag*, unsigned char, unsigned char) (amudp_reqrep.cpp:1040)
    ==22195==    by 0x6FEA6E: AMUDP_RequestIVA (amudp_reqrep.cpp:1218)
    ==22195==    by 0x463681: gasnetc_AMRequestMedium (gasnet_core.c:843)
    ==22195==    by 0x464095: gasnetc_AMRequestMediumM (gasnet_core.c:875)
    ==22195==    by 0x43665B: doit (testgasnet.c:755)
    ==22195==    by 0x4316B8: main (testgasnet.c:315)
    ==22195==  Address 0x5fd44d0 is 176 bytes inside a block of size 65,136 alloc'd
    ==22195==    at 0x4C29F33: malloc (vg_replace_malloc.c:307)
    ==22195==    by 0x7023D1: _AMX_malloc (amx_internal.h:130)
    ==22195==    by 0x6F02F5: AMUDP_AcquireBuffer (amudp_ep.cpp:110)
    ==22195==    by 0x6F73D1: AMUDP_DrainNetwork(amudp_ep*) (amudp_reqrep.cpp:214)
    ==22195==    by 0x6FC83A: AMUDP_ServiceIncomingMessages(amudp_ep*) (amudp_reqrep.cpp:829)
    ==22195==    by 0x6FCC62: AM_Poll (amudp_reqrep.cpp:886)
    ==22195==    by 0x6FCEEC: AMUDP_RequestGeneric(amudp_category_t, amudp_ep*, unsigned int, unsigned char, void*, unsigned long, unsigned long, int, __va_list_tag*, unsigned char, unsigned char) (amudp_reqrep.cpp:933)
    ==22195==    by 0x6FEA6E: AMUDP_RequestIVA (amudp_reqrep.cpp:1218)
    ==22195==    by 0x463681: gasnetc_AMRequestMedium (gasnet_core.c:843)
    ==22195==    by 0x464095: gasnetc_AMRequestMediumM (gasnet_core.c:875)
    ==22195==    by 0x43665B: doit (testgasnet.c:755)
    ==22195==    by 0x4316B8: main (testgasnet.c:315)
    
    ```

--- tests/testgasnet.c
+++ tests/testgasnet.c
@@ -719,6 +719,7 @@ void doit(int partner, int *partnerseg) {
       if (d == numranks) r = GEX_RANK_INVALID; // min of maxes
       else r = (myrank + d) % numranks;
       amsz_t max;
+      memset(&max,0,sizeof(max)); // avoid a valgrind warning for uninit bytes
       for (int lci = 0; lci < AM_LCOPT_CNT; lci++) {
         for (int flagsi = 0; flagsi < AM_FLAGS_CNT; flagsi++) {
           #define GET_MAX(cat) do {                                                              \
