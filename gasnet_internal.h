0df1f74e00551a1d67a7cf3ba95a7a8495cea5d2
Paul H. Hargrove <PHHargrove@lbl.gov>
2020-09-16 16:12:01 -0700

    Implement and document gex_Segment_Publish()
    This commit implements and documents `gex_Segment_Publish()`.
    
    There is a reference implementation currently used by conduits using
    the reference extended API code, and a mechanism by which conduit with
    a native extended API implementation provide their overrides (calling
    the reference implementation for much/most of the work).
    
    It is planned that in the future the conduit-specific logic will be
    more tightly integrated with the conduit-independent logic, in
    particular to avoid redundant communication of `gex_EP_Location_t`
    data.  However, sharing of common code with the aux segment
    registration and gex_Segment_Attach in several conduits makes the
    changes required non-trivial.  Additionally, both ibv and ucx have
    variable-length registration key data which renders a naive
    "EXTRA_FIELDS" approach insufficient on it own.
    
    Also planned is a role-reversal to make the conduit-independent
    version the main entry-point, which will call conduit-specific
    hooks.  This would replace the current name-shifting override.

--- gasnet_internal.h
+++ gasnet_internal.h
@@ -474,6 +474,12 @@ int gasneti_segmentCreate(
                 gex_MemKind_t           kind,
                 gex_Flags_t             flags);
 
+int gasneti_Segment_Publish(
+            gex_TM_t       tm,
+            gex_EP_t       *eps,
+            size_t         num_eps,
+            gex_Flags_t    flags);
+
 extern void gasneti_legacy_segment_attach_hook(gasneti_EP_t ep);
 extern void gasneti_legacy_alloc_tm_hook(gasneti_TM_t _tm);
 
