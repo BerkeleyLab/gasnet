a941209320d007ab7dc95f0b54f139ff7890ef2b
Paul H. Hargrove <PHHargrove@lbl.gov>
2020-10-02 01:33:36 -0700

    Initial implementation of gex_MK_t
    This commit provides an initial implementation of `gex_MK_t` with
    limited functionality.  Notably lacking at this stage is any support
    for passing client-created kinds to `gex_Segment_Create()` and any
    conduit-specific logic that would differ for kinds other than host
    memory.  Despite the limited functionality, there is a significant
    amount of new work in this commit.
    
    The type `gex_MK_t` has been fleshed out, with all the object
    semantics such as import/export and MAGIC signature.
    
    An infrastructure update adds a new `other/kinds/` directory, similar
    to `extended-ref/ratomic` deployed for `gex_AD_t`.  Within this new
    directory is is a new client-facing header `gasnet_mk.h`, in the same
    spirit as the per-subsystem headers for vis, coll and ratomic.
    
    The infrastructure design isolates code specific to a given device
    class to specific files (e.g. exactly one source file includes
    `cuda.h`).  While it is anticipated that one file per device class
    will be the common case, nothing precludes multiple files per device
    class or multiple device sub-classes in a single file.  This isolation
    involves new logic in `other/Makefile-{conduit,libgasnet}.mak.in` to
    introduce `$(KINDS_SPECIAL_OBJS)`.  Like `$(CONDUIT_SPECIAL_OBJS)`,
    this variable is a list of object files built with "special" rules,
    such as to supply additional elements in the include path (which are
    intentionally NOT used in any other compilation steps).
    
    The following client-facing types and functions are defined:
    
        `gex_MK_Class_t`
        `gex_MK_Create_args_t`
        `gex_MK_Create()`
        `gex_MK_Destroy()`
    
    The two new functions are wrappers around implementations specific to
    a device class, and provide tracing and argument validation before
    dispatching to class-specific code for the "real work".  By design,
    *only* `gex_MK_Create()` contains a "big `switch`" for dispatch, which
    additionally provides for proper reporting of calls requesting classes
    not enabled by `configure`.  Everything post-create is dispatched
    using a per-class "impl" structure containing (among other things) a
    table of function pointers to provide non-default implementations.
    
    This structure will grow with additional function pointers as new
    hooks are added, such as for integration with `gex_Segment_Create()`
    and for interaction with conduit-specific code.  Those details are
    still TBD.
    
    Finally, this commit contains an initial "stub" implementation of:
    
        `GEX_MK_CLASS_CUDA_UVA`         (unconditional enum value)
        `GASNET_HAVE_MK_CLASS_CUDA_UVA` (macro, either 1 or undef)
    
    The class-specific union member for the `gex_MK_Create()` API's `args`
    has been defined as taking a CUDA device id, and the implementation
    verifies that the given device is UVA-capable.  The device and its
    primary context are saved in the per-class extension to `gex_MK_t`.
    
    The configure-time probe for `CUDA_{CFLAGS,LDFLAGS,LIBS}` is based on the
    existing pattern for packages such as conduit libs, PMI and (formerly)
    BLCR.  It assumes for now that both the device and runtime APIs are
    required, but can be loosened in the future if we need only one or the
    other.  The defaults are sufficient in testing for at least CUDA 7, 10
    and 11 on Dirac, Summit and Wombat.  They are almost certainly not
    sufficient for macOS, but that is not currently a target for this
    work.  The option of extracting defaults from `nvcc` still exists, but
    the existing implementations of that are in Bash (not portable to
    other shells) or Python 2.

--- acconfig.h
+++ acconfig.h
@@ -218,6 +218,9 @@
 /* --with-max-threads value (if given) */
 #undef GASNETI_MAX_THREADS_CONFIGURE
 
+/* memory kinds support */
+#undef GASNET_HAVE_MK_CLASS_CUDA_UVA
+
 /* has clock_gettime() */
 #undef HAVE_CLOCK_GETTIME
 
