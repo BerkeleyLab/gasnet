8a0c65da18c683067af84c158986e2792945f4ad
Paul H. Hargrove <PHHargrove@lbl.gov>
2020-10-02 18:04:21 -0700

    Add tracing of gex_Client_Init() calls

--- ucx-conduit/gasnet_core.c
+++ ucx-conduit/gasnet_core.c
@@ -859,6 +859,10 @@ extern int gasnetc_Client_Init(
   }
   gasneti_EP_t ep = gasneti_import_ep(*ep_p);
 
+  // Do NOT move this prior to the gasneti_trace_init() call
+  GASNETI_TRACE_PRINTF(O,("gex_Client_Init: name='%s' argc_p=%p argv_p=%p flags=%d",
+                          clientName, argc, argv, flags));
+
   // TODO-EX: create team
   gasneti_TM_t tm = gasneti_init_done
                     ? gasneti_import_tm(gasnetc_bootstrap_tm) // gasnetc_init() creates very first TM
