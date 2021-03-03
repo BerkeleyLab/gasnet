8af7189205cb7f20d0b4c4460973b6a69cde6aaf
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-03-02 21:34:37 -0800

    PSHM,loopback: move NBRHD params to public header
    This commit relocates conditional preprocessor definitions from
    `gasnet_am.h`, where they were visible only to internal code, to
    `gasnetex.h` where they are now defined between inclusion of
    `gasnet_core_fwd.h` (where overrides belong) and `gasnet_core.h`.
    
    This move is in anticipation of needing at least the maximum size of
    the loopback/pshm Medium buffer to fully define the `gex_AM_Max*()`
    queries in `gasnet_core.h`.
    
    In addition to the pure movement (no changes) of these definitions,
    the ucx-conduit override of `GASNETC_MAX_MEDIUM_NBRHD` has been
    relocated from `ucx-conduit/gasnet_core.h` to its proper place in
    `ucx-conduit/gasnet_core_fwd.h`.

--- ucx-conduit/gasnet_core.h
+++ ucx-conduit/gasnet_core.h
@@ -126,10 +126,7 @@ size_t gasnetc_AMHeaderSize(void);
 
 #define GASNETC_MAX_ARGS            16
 #define GASNETC_UCX_HDR_SIZE        gasnetc_AMHeaderSize()
-#define GASNETC_MAX_MED             4096
 #define GASNETC_MAX_LONG            INT_MAX
-
-#define GASNETC_MAX_MEDIUM_NBRHD    GASNETC_MAX_MED
 
 #define GASNETC_ARGS_SIZE(numargs) (sizeof(gex_AM_Arg_t) * (numargs))
 
