30920796ecbdc670ba3ad648d9423cf7f52bb282
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-04-08 22:21:03 -0700

    aries: remove logic deprecating ancient env vars
    This commit removes from aries-conduit some logic added in March 2013
    when incorrectly prefixed environment variables were moved from
    `GASNETC_` to `GASNET_`.  The removed logic would whine about the use
    of old names but does not (and never did) honor the values.
    
    The old names are such that my Google search on a random selection
    found only this deprecation logic -- not code implementing them nor
    documentation describing them.

--- aries-conduit/gasnet_core.c
+++ aries-conduit/gasnet_core.c
@@ -131,35 +131,6 @@ static int gasnetc_bootstrapInit(int *argc, char ***argv) {
   }
   #endif
 
-  /* As good a place as any for this: */
-  if (!gasneti_mynode && !gasneti_getenv_yesno_withdefault("GASNET_QUIET",0)) {
-    static const char *old_vars[][3] = {
-        {"GASNET_PHYSMEM_PINNABLE_RATIO", "removed", "GASNET_PHYSMEM_MAX"},
-        {"GASNETC_GNI_MIN_NUM_PD", "removed", "GASNET_GNI_NUM_PD"},
-        {"GASNETC_GNI_MIN_BOUNCE_SIZE", "removed", "GASNET_GNI_BOUNCE_SIZE"},
-        {"GASNETC_GNI_AM_MEM_CONSISTENCY", "removed", NULL},
-        {"GASNETC_GNI_NUM_PD", "renamed", "GASNET_GNI_NUM_PD"},
-        {"GASNETC_GNI_BOUNCE_SIZE", "renamed", "GASNET_GNI_BOUNCE_SIZE"},
-        {"GASNETC_GNI_MEM_CONSISTENCY", "renamed","GASNET_GNI_MEM_CONSISTENCY"},
-        {"GASNETC_GNI_FMA_RDMA_CUTOVER", "split into two variables",
-                "GASNET_GNI_GET_FMA_RDMA_CUTOVER and/or GASNET_GNI_PUT_FMA_RDMA_CUTOVER"},
-        {"GASNETC_GNI_BOUNCE_REGISTER_CUTOVER", "split into two variables",
-                "GASNET_GNI_GET_BOUNCE_REGISTER_CUTOVER and/or GASNET_GNI_PUT_BOUNCE_REGISTER_CUTOVER"},
-        {NULL,NULL,NULL}};
-    int i;
-    for (i=0; old_vars[i][0] != NULL; ++i) {
-      const char *what = old_vars[i][0];
-      const char *why  = old_vars[i][1];
-      const char *who  = old_vars[i][2];
-      if (NULL != getenv(what)) {
-        fprintf(stderr, "WARNING: Detected a setting for environment variable %s, "
-                        "which has been %s.\n", what, why);
-        if (who)
-          fprintf(stderr, "WARNING: Please set %s instead.\n", who);
-      }
-    }
-  }
-
   return GASNET_OK;
 }
 
