BerkeleyLab/gasnet

commit 67af62eb41becda8515e92d1d7805f7ebb4cbe37

ibv: suppress bogus warning from older icc
This commit surgically suppresses the warning seen with some versions
of the Intel C compiler when the condition in an `__assume()` calls
`gasneti_leaf_is_pointer()` (a nearly trivial, inline, pure-annotated
function).

This has been tested on the following Intel versions:

  + 2021.1.2 on Dirac
    This is the newest module on Dirac
  + 19.0.117 on Dirac
    This is the first version which does NOT need the fix.
  + 18.1.163 on Cori GPU
    This is where the problem was noted in CI testing
  + 17.0.098 on Dirac
    This is the oldest module on Dirac
  + 8.1 (__INTEL_COMPILER_BUILD_DATE of 20050520) on pcp-v-15
    This ancient ia-32 compiler is available on the pcp-v-15 VM.
    I've verified the compiler honors the pragmas used in this commit
    to suppress warnings.  Though ibv-conduit does build here, the
    warning in question was not occurring prior to this commit.
    However, there were plenty of *other* bogus warnings to test with.

ibv-conduit/gasnet_core_internal.h
@@ -72,6 +72,21 @@ extern gasneti_atomic_t gasnetc_exit_running;
 /* May eventually be a hash? */
 #define GASNETC_NODE2CEP(_ep,_node) ((_ep)->cep_table[_node])
 
+#if (PLATFORM_COMPILER_INTEL && PLATFORM_COMPILER_VERSION_LT(19,0,20180800))
+  // Some Intel C prior to 2019.0.117 (builddate 20180804) issue a buggy warning
+  // about side effects in an __assume(), and these versions predate Intel's
+  // support for __builtin_assume, which avoids the warning.
+  #define gasnetc_assume_leaf_is_pointer(lc_opt) do { \
+    GASNETI_PRAGMA(warning push);                     \
+    GASNETI_PRAGMA(warning disable 2261);             \
+    gasneti_assume(gasneti_leaf_is_pointer(lc_opt));  \
+    GASNETI_PRAGMA(warning pop);                      \
+  } while (1)
+#else
+  #define gasnetc_assume_leaf_is_pointer(lc_opt) \
+          gasneti_assume(gasneti_leaf_is_pointer(lc_opt))
+#endif
+
 
 /*
  * In theory all resources should be recovered automatically at process exit.

ibv-conduit/gasnet_core.c
@@ -4777,7 +4777,7 @@ int gasnetc_AMRequestMedium(gex_TM_t tm, gex_Rank_t rank, gex_AM_Index_t handler
                                    argptr GASNETI_THREAD_PASS);
 
     if (eop) {
-      gasneti_assume(gasneti_leaf_is_pointer(lc_opt)); // avoid maybe-uninitialized warning (bug 3756)
+      gasnetc_assume_leaf_is_pointer(lc_opt); // avoid maybe-uninitialized warning (bug 3756)
       if (start_cnt == eop->initiated_alc) {
         // Synchronous LC - reset LC state and pass-back INVALID_HANDLE as result
         GASNETE_EOP_LC_FINISH(eop);
@@ -4865,7 +4865,7 @@ int gasnetc_AMRequestLong(  gex_TM_t tm, gex_Rank_t rank, gex_AM_Index_t handler
       /* block for local completion of RDMA transfer */
       gasnetc_counter_wait(&counter, 0 GASNETI_THREAD_PASS);
     } else if (eop) {
-      gasneti_assume(gasneti_leaf_is_pointer(lc_opt)); // avoid maybe-uninitialized warning (bug 3756)
+      gasnetc_assume_leaf_is_pointer(lc_opt); // avoid maybe-uninitialized warning (bug 3756)
       if (start_cnt == eop->initiated_alc) {
         // Synchronous LC - reset LC state and pass-back INVALID_HANDLE as result
         GASNETE_EOP_LC_FINISH(eop);
@@ -4950,7 +4950,7 @@ int gasnetc_AMReplyMedium(  gex_Token_t token, gex_AM_Index_t handler,
     gasneti_assert(!rbuf->rbuf_needReply || (flags & GEX_FLAG_IMMEDIATE));
 
     if (eop) {
-      gasneti_assume(gasneti_leaf_is_pointer(lc_opt)); // avoid maybe-uninitialized warning (bug 3756)
+      gasnetc_assume_leaf_is_pointer(lc_opt); // avoid maybe-uninitialized warning (bug 3756)
       if (start_cnt == eop->initiated_alc) {
         // Synchronous LC - reset LC state and pass-back INVALID_HANDLE as result
         GASNETE_EOP_LC_FINISH(eop);
@@ -5038,7 +5038,7 @@ int gasnetc_AMReplyLong(    gex_Token_t token, gex_AM_Index_t handler,
       /* block for local completion of RDMA transfer */
       gasnetc_counter_wait(&counter, 1 /* handler context */ GASNETI_THREAD_PASS);
     } else if (eop) {
-      gasneti_assume(gasneti_leaf_is_pointer(lc_opt)); // avoid maybe-uninitialized warning (bug 3756)
+      gasnetc_assume_leaf_is_pointer(lc_opt); // avoid maybe-uninitialized warning (bug 3756)
       if (start_cnt == eop->initiated_alc) {
         // Synchronous LC - reset LC state and pass-back INVALID_HANDLE as result
         GASNETE_EOP_LC_FINISH(eop);
@@ -5235,7 +5235,7 @@ void gasnetc_commit_common(
                      GASNETI_THREAD_PASS);
 
   if (eop) {
-    gasneti_assume(gasneti_leaf_is_pointer(lc_opt)); // avoid maybe-uninitialized warning (bug 3756)
+    gasnetc_assume_leaf_is_pointer(lc_opt); // avoid maybe-uninitialized warning (bug 3756)
     if (start_cnt == eop->initiated_alc) {
       // Synchronous LC - reset LC state and pass-back INVALID_HANDLE as result
       GASNETE_EOP_LC_FINISH(eop);

ibv-conduit/gasnet_extended.c
@@ -140,10 +140,7 @@ gex_Event_t gasnete_put_nb(
   } else if (lc_opt == GEX_EVENT_DEFER) {
     GASNETC_RDMA_PUT(NULL, NULL);
   } else {
-  // Intel C prior to 2019.0.117 (builddate 20180804) issues a buggy warning here
-  #if !(PLATFORM_COMPILER_INTEL && PLATFORM_COMPILER_VERSION_LT(19,0,20180800))
-    gasneti_assume(gasneti_leaf_is_pointer(lc_opt));
-  #endif
+    gasnetc_assume_leaf_is_pointer(lc_opt);
     GASNETE_EOP_LC_START(op);
     gasnetc_atomic_val_t start_cnt = op->initiated_alc;
     GASNETC_RDMA_PUT(&op->initiated_alc, gasnetc_cb_eop_alc);

commit fb606f87c1abdd139b22ee0f14986d52594d27b8

fixup: ibv: suppress bogus warning from older icc
Commit 67af62eb41becda8515e92d1d7805f7ebb4cbe37 introduced a hang
for Intel compilers older than 2019.0.117, which this commit repairs.

ibv-conduit/gasnet_core_internal.h
@@ -81,7 +81,7 @@ extern gasneti_atomic_t gasnetc_exit_running;
     GASNETI_PRAGMA(warning disable 2261);             \
     gasneti_assume(gasneti_leaf_is_pointer(lc_opt));  \
     GASNETI_PRAGMA(warning pop);                      \
-  } while (1)
+  } while (0)
 #else
   #define gasnetc_assume_leaf_is_pointer(lc_opt) \
           gasneti_assume(gasneti_leaf_is_pointer(lc_opt))