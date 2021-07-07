1290744daec2b7f68c5c4fcfaf752c94f7d01088
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-07-07 05:02:42 -0700

    ibv: update all uses of GASNETI_WAITHOOK()
    This commit resolves Bug 4260 "RFE: update spinloops in ibv-conduit".
    
    Nearly all code previously using a "bare" WAITHOOK has been updated to
    use the `GASNETI_SPIN_*()` macros.  This eliminates multiple misuses
    of `GASNETI_WAITHOOK()` while simultaneously improving readability and
    roughly halving of the number of source lines dedicated to spinloops.
    
    One loop (in the conduit-specific barrier logic) has been converted to
    `gasneti_pollwhile()` instead of a `GASNETI_SPIN_*()` macro.

--- ibv-conduit/gasnet_core.c
+++ ibv-conduit/gasnet_core.c
@@ -4175,10 +4175,9 @@ void gasnetc_am_sema_poll(gasnetc_sema_t * const sema,
                           gasnetc_EP_t ep
                           GASNETI_THREAD_FARG))
 {
-  do {
-    GASNETI_WAITHOOK();
-    gasnetc_poll_rcv_all(ep, 1 GASNETI_THREAD_PASS);
-  } while (!gasnetc_sema_trydown(sema));
+  // Since callers have already failed a trydown, this loop is a "DOUNTIL"
+  GASNETI_SPIN_DOUNTIL(gasnetc_sema_trydown(sema),
+                       gasnetc_poll_rcv_all(ep, 1 GASNETI_THREAD_PASS));
 }
 
 // Helper for allocation of flow-control credit
