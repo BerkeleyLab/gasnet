BerkeleyLab/gasnet

commit df1449429cd69814522b37290ef141a67051f71e

Fix output interleaving in GASNET_VERBOSEENV
(cherry picked from commit 90da53f20a7539cf6dac4ea1c4fa26dd85e0c81c)

gasnet_tools.c
@@ -2119,10 +2119,10 @@ extern void gasneti_envstr_display(const char *key, const char *val, int is_dflt
     char tmpstr[255];
     char *displaystr = tmpstr;
     int width = MAX(10,55 - strlen(key) - strlen(displayval));
-    int len = snprintf(tmpstr, sizeof(tmpstr), "ENV parameter: %s = %s%*s", key, displayval, width, dflt);
+    int len = snprintf(tmpstr, sizeof(tmpstr), "ENV parameter: %s = %s%*s\n", key, displayval, width, dflt);
     if (len >= sizeof(tmpstr)) { /* Too long for the static buffer */
       displaystr = malloc(len + 1);
-      snprintf(displaystr, len+1, "ENV parameter: %s = %s%*s", key, displayval, width, dflt);
+      snprintf(displaystr, len+1, "ENV parameter: %s = %s%*s\n", key, displayval, width, dflt);
     }
     gasneti_mutex_lock(&envmutex);
       for (p = displaylist; p; p = p->next) { /* check for previous report */
@@ -2133,7 +2133,7 @@ extern void gasneti_envstr_display(const char *key, const char *val, int is_dflt
         p->key = strdup(key);
         if (verbose > 0 && !notyet) { /* display now */
           p->displaystr = NULL;
-          fprintf(stderr, "%s\n", displaystr);
+          fputs(displaystr, stderr);
           fflush(stderr);
         } else { /* cache for later */
           p->displaystr = strdup(displaystr);
@@ -2145,7 +2145,7 @@ extern void gasneti_envstr_display(const char *key, const char *val, int is_dflt
       }
       if (notyet && verbose > 0) { /* dump cached values */ 
         for (p = displaylist; p; p = p->next) {
-          fprintf(stderr, "%s\n", p->displaystr);
+          fputs(displaystr, stderr);
           fflush(stderr);
           free((void *)p->displaystr);
           p->displaystr = NULL;

commit f6cc2a8881f22580ff0f9298cbb44e2421b8a652

Fix bug 4281: Display glitch in VERBOSEENV console reporting
This defect was introduced in df1449429cd69814522b37290ef141a67051f71e
and first released in gex-2019.3.0

gasnet_tools.c
@@ -2301,7 +2301,7 @@ extern void gasneti_envstr_display(const char *key, const char *val, int is_dflt
       }
       if (notyet && verbose > 0) { /* dump cached values */ 
         for (p = displaylist; p; p = p->next) {
-          fputs(displaystr, stderr);
+          fputs(p->displaystr, stderr);
           fflush(stderr);
           free((void *)p->displaystr);
           p->displaystr = NULL;