c349167c4f04bf011203f3930c0149a1b9a7700e
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-08-20 13:35:18 -0700

    Bump ibv conduit internal version number
    This recognizes the importance of the fix to bugs 4148 and 4150

--- ibv-conduit/gasnet_core_fwd.h
+++ ibv-conduit/gasnet_core_fwd.h
@@ -15,7 +15,7 @@
   #error "VAPI-conduit is no longer supported"
 #endif
 
-#define GASNET_CORE_VERSION      2.8
+#define GASNET_CORE_VERSION      2.9
 #define GASNET_CORE_VERSION_STR  _STRINGIFY(GASNET_CORE_VERSION)
 #define GASNET_CORE_NAME         IBV
 #define GASNET_CORE_NAME_STR     _STRINGIFY(GASNET_CORE_NAME)
