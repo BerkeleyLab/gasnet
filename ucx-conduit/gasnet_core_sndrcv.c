708eb5c967cf3223c3aad28915a859834b16c795
Paul H. Hargrove <PHHargrove@lbl.gov>
2021-09-20 15:38:01 -0700

    ucx: resolve bug 4298
    This commit resolves Bug 4298 "ucx persona_example hang w/o PSHM" by
    ensuring that we fully progress the conduit (subject to handler
    context restriction) when spinning for an AM send buffer.
    
    This commit also adds polling of PSHM to `gasnetc_poll_snd()` in FAST
    and LARGE segment modes (where it was previously missing) and removes
    it from EVERYTHING mode (where PSHM is not supported).  This is
    somewhat orthogonal to the bug report (which is non-PSHM), but in the
    spirit of fully/correctly polling when stalled for resources.

--- ucx-conduit/gasnet_core_sndrcv.c
+++ ucx-conduit/gasnet_core_sndrcv.c
@@ -222,14 +222,21 @@ void gasnetc_send_fini(void)
 }
 
 GASNETI_INLINE(gasnetc_am_req_get)
-gasnetc_am_req_t *gasnetc_am_req_get(void)
+gasnetc_am_req_t *gasnetc_am_req_get(int is_request GASNETI_THREAD_FARG)
 {
   gasnetc_am_req_t *am_req;
 
-  do {
-    gasnetc_ucx_progress();
-  } while (NULL == (am_req = GASNETI_LIST_POP(&gasneti_ucx_module.sreq_free,
-                                              gasnetc_am_req_t)));
+  gasnetc_ucx_progress();
+  if (is_request) {
+    GASNETI_SPIN_UNTIL((am_req = GASNETI_LIST_POP(&gasneti_ucx_module.sreq_free,
+                                                  gasnetc_am_req_t)),
+                       gasnetc_poll_sndrcv(GASNETC_LOCK_INLINE GASNETI_THREAD_PASS));
+  } else {
+    GASNETI_SPIN_UNTIL((am_req = GASNETI_LIST_POP(&gasneti_ucx_module.sreq_free,
+                                                  gasnetc_am_req_t)),
+                       gasnetc_poll_snd(GASNETC_LOCK_INLINE GASNETI_THREAD_PASS));
+  }
+
   return am_req;
 }
 
@@ -580,7 +587,7 @@ int gasnetc_am_reqrep_inner(gasnetc_ucx_am_type_t am_type,
   gasnetc_ucx_request_t *req;
 
   GASNETC_LOCK_ACQUIRE(GASNETC_LOCK_REGULAR);
-  am_req = gasnetc_am_req_get();
+  am_req = gasnetc_am_req_get(is_request GASNETI_THREAD_PASS);
   gasneti_assert(am_req);
 
 #define __am_req_format(__is_packed) \
@@ -943,6 +950,16 @@ void gasnetc_poll_snd(gasnetc_lock_mode_t lmode GASNETI_THREAD_FARG)
   GASNETC_LOCK_ACQUIRE(lmode);
   gasnetc_ucx_progress();
   GASNETC_LOCK_RELEASE(lmode);
+#if GASNET_PSHM
+  if (lmode == GASNETC_LOCK_REGULAR) {
+    gasneti_AMPSHMPoll(1 GASNETI_THREAD_PASS);
+  } else if (lmode == GASNETC_LOCK_INLINE) {
+    /* `gasneti_AMPSHMPoll` should be called outside the lock */
+    GASNETC_LOCK_RELEASE(GASNETC_LOCK_REGULAR);
+    gasneti_AMPSHMPoll(1 GASNETI_THREAD_PASS);
+    GASNETC_LOCK_ACQUIRE(GASNETC_LOCK_REGULAR);
+  }
+#endif
 }
 
 #else
@@ -1029,16 +1046,6 @@ void gasnetc_poll_snd(gasnetc_lock_mode_t lmode GASNETI_THREAD_FARG)
     request->status = GASNETC_UCX_ACTIVE;
   }
   GASNETC_LOCK_RELEASE(lmode);
-#if GASNET_PSHM
-  if (lmode == GASNETC_LOCK_REGULAR) {
-    gasneti_AMPSHMPoll(0 GASNETI_THREAD_PASS);
-  } else if (lmode == GASNETC_LOCK_INLINE) {
-    /* `gasneti_AMPSHMPoll` should be called outside the lock */
-    GASNETC_LOCK_RELEASE(GASNETC_LOCK_REGULAR);
-    gasneti_AMPSHMPoll(0 GASNETI_THREAD_PASS);
-    GASNETC_LOCK_ACQUIRE(GASNETC_LOCK_REGULAR);
-  }
-#endif
 }
 
 GASNETI_INLINE(gasneti_req_probe_complete)
