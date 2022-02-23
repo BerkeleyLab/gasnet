4f4795a5c1446e1a8a23b7cc19e3c275ddf78036
Paul H. Hargrove <PHHargrove@lbl.gov>
2022-02-23 11:43:33 -0800

    Implement gasneti_finalize_all_nbi_ff()
    This commit implements `gasneti_finalize_all_nbi_ff()` as a means for
    a conduit (exit code in particular) to "steal" all the nbi_ff aops
    from both live threads, and from the leaked threaddata of any threads
    which exited with that aop still "live".
    
    A future commit will deploy this in ofi-conduit.

--- gasnet_internal.h
+++ gasnet_internal.h
@@ -1046,6 +1046,19 @@ void gasneti_end_nbi_ff(GASNETI_THREAD_FARG_ALONE)
   gasneti_assert(aop == GASNETI_MYTHREAD->nbi_ff_aop);
 }
 
+// Sets the nbi_ff_aop of all threads to NULL and returns (via reference
+// arguments) an array of events and its length.  This array, contains all of
+// the aops which were found to be non-NULL.  The array and count are suitable
+// for calls to gex_Event_{Try,Wait}{All,Some}().
+//
+// The caller is responsible for freeing the array, which may be non-NULL
+// even when the count is zero.
+//
+// NOTE: this does NOT adjust `iop_num` in other threads when stealing their
+// nbi_ff_aop.  This may force the threaddata to leak.  However, this should
+// not be a real issue at process exit (the main intended use of this call).
+extern void gasneti_finalize_all_nbi_ff(gex_Event_t **events_p, size_t *count_p GASNETI_THREAD_FARG);
+
 // DO NOT USE THIS!
 // This exists only to permit "safe" testing in gasnet_diagnostic.c.
 extern void gasneti_nbi_ff_drain_(GASNETI_THREAD_FARG_ALONE);
