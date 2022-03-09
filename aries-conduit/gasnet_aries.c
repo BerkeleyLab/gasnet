2be9f04ce75d16b7d712bbd1ed20d85641575101
Paul H. Hargrove <PHHargrove@lbl.gov>
2022-03-09 14:51:59 -0800

    aries: implement a Segment Destroy hook
    This commit implements `gasnetc_segment_destroy_hook()` for
    aries-conduit, factoring the exit-time shutdown equivalent to create
    `gasnetc_segment_deregister()`.

--- aries-conduit/gasnet_aries.c
+++ aries-conduit/gasnet_aries.c
@@ -834,6 +834,20 @@ void gasnetc_segment_register(gasnetc_Segment_t segment)
   gasneti_assert_always (status == GNI_RC_SUCCESS);
 }
 
+void gasnetc_segment_deregister(gasnetc_Segment_t segment)
+{
+#if GASNETC_USE_MULTI_DOMAIN
+  GASNETC_DIDX_POST(GASNETC_DEFAULT_DOMAIN);
+  DOMAIN_SPECIFIC_VAR(gni_nic_handle_t, nic_handle);
+#endif
+
+  gni_return_t status;
+  status = GNI_MemDeregister(nic_handle, &segment->mem_handle);
+  if_pf (status != GNI_RC_SUCCESS) {
+    gasnetc_GNIT_Log("MemDeregister(segment) failed with %s", gasnetc_gni_rc_string(status));
+  }
+}
+
 /*-------------------------------------------------*/
 // set the local memory handle for the client segment and exchanges with other procs
 // TODO: non-primordial EP support
@@ -1554,10 +1568,7 @@ void gasnetc_shutdown(void)
       GASNETI_SEGTBL_LOCK();
         gasneti_Segment_t seg;
         GASNETI_SEGTBL_FOR_EACH(seg) {
-          status = GNI_MemDeregister(nic_handle, &((gasnetc_Segment_t)seg)->mem_handle);
-          if_pf (status != GNI_RC_SUCCESS) {
-            gasnetc_GNIT_Log("MemDeregister(segment) failed with %s", gasnetc_gni_rc_string(status));
-          }
+          gasnetc_segment_deregister((gasnetc_Segment_t)seg);
         }
       GASNETI_SEGTBL_UNLOCK();
 
