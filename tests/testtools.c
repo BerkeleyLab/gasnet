0d522848495b12866e3cf1c08ad8080ed721745b
bonachea <dobonachea@lbl.gov>
2022-03-10 00:52:15 -0500

    testtools: fix a misleading error message

--- tests/testtools.c
+++ tests/testtools.c
@@ -249,7 +249,7 @@ int main(int argc, char **argv) {
         do {
           gasnett_tick_t next = gasnett_ticks_now();
           if (next < last) 
-            ERR("gasnett_ticks_to_us not monotonic! !(%" PRIu64 " <= %" PRIu64 ")",
+            ERR("gasnett_ticks_now not monotonic! !(%" PRIu64 " <= %" PRIu64 ")",
                  (uint64_t)last, (uint64_t)next);
           if (next <= GASNETT_TICK_MIN) 
             ERR("gasnett_ticks_to_us()=%" PRIu64 " <= GASNETT_TICK_MIN=%" PRIu64,
