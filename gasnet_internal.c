080c7c5996f91b87257e751aeda90048d869e564
Paul H. Hargrove <PHHargrove@lbl.gov>
2022-06-03 23:24:04 -0700

    fixup: Report invalid returns from gethostid()
    Do not report an invalid return from `gethostid()` unless such call
    actually occurs.

--- gasnet_internal.c
+++ gasnet_internal.c
@@ -1908,11 +1908,13 @@ extern uint32_t gasneti_gethostid(void) {
           || (myid == 0x0000017f)
           || (myid == 0x0001007f)
           || (myid == 0x0100007f)) {
+      #if HAVE_GETHOSTID && !PLATFORM_OS_CYGWIN
         gasneti_console_message("WARNING", "Invalid return 0x%08x from gethostid().  "
                                 "Please see documentation on GASNET_HOST_DETECT in README and "
                                 "consider setting its value to 'hostname' or reconfiguring using "
                                 "'--with-host-detect=hostname' to make that the default value.",
                                 (unsigned int)myid);
+      #endif
         uint64_t csum = gasneti_hosthash();
         myid = GASNETI_HIWORD(csum) ^ GASNETI_LOWORD(csum);
       }
