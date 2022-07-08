cd0e65ab7940c4bab01654c6d46d0be372139e1e
Paul H. Hargrove <PHHargrove@lbl.gov>
2022-07-07 22:27:46 -0700

    ofi: revise scalable memory registration
    This commit revises and completes ofi-conduit's implementation of
    memory registration.  The "revision" allows effective use of the
    ability to specify the memory registration key under `FI_MR_SCALABLE`,
    while the "completion" enables ofi-conduit to pass `testsegment`
    without a special case (which is thus removed in this commit).  This
    brings the implementation of segments up to the same level as other
    conduits which lack support for multiple endpoints, while making
    progress toward such support.
    
    The primary change is to ensure that under `FI_MR_SCALABLE` a remote
    memory registration key is always computable (and thus never needs to
    be stored).  This is accomplished by deferring memory registration
    until `gex_EP_BindSegment()`, when we can use the endpoint index to
    select a "computable" key (the computation is simply adding 1, to
    reserve 0 for the aux segment's key).  While this alone is not
    sufficient for support of multiple endpoints, it does ensure that no
    storage of remote registration keys will be required under
    `FI_MR_SCALABLE`.
    
    Additional changes include (1) dropping the unused registration of the
    aux segment in `EVERYTHING` segment mode, and (2) work toward logical
    separation of the multiple provider properties which comprise
    `FI_MR_SCALABLE`.
    
    Finally, the `FI_WRITE` and `FI_READ` macros have been revised to
    eliminate needless duplication between the `BASIC` and `SCALABLE`
    memory registration paths.  Additional cleanups have been deferred
    to the work on multi-endpoint support.

--- ofi-conduit/gasnet_ofi.h
+++ ofi-conduit/gasnet_ofi.h
@@ -176,7 +176,7 @@ typedef struct gasnetc_Segment_t_ {
 } *gasnetc_Segment_t;
 
 void gasnetc_auxseg_register(gasnet_seginfo_t si);
-int gasnetc_segment_register(gasnetc_Segment_t segment);
+int gasnetc_segment_register(gasnetc_Segment_t segment, uint64_t key);
 int gasnetc_segment_deregister(gasnetc_Segment_t segment);
 void gasnetc_segment_exchange(gex_TM_t tm, gex_EP_t *eps, size_t num_eps);
 
@@ -219,5 +219,11 @@ extern int gasnetc_exit_in_progress;
 #ifndef GASNETC_OFI_EVENTS_PER_POLL
 #define GASNETC_OFI_EVENTS_PER_POLL 16
 #endif
+
+// Conversion of EP index to memory registration key,
+// where -1 is used for the aux seg
+#define _GASNETC_CLIENT_KEY_BASE     1
+#define GASNETC_EPIDX_TO_KEY(epidx)  ((epidx)+_GASNETC_CLIENT_KEY_BASE)
+#define GASNETC_AUX_KEY              GASNETC_EPIDX_TO_KEY(-1)
 
 #endif /*_GASNET_OFI_H*/
