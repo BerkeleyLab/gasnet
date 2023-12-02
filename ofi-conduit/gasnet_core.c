3a1d9aa7b1661a6ec4a1852b702871c8be24ffae
Paul H. Hargrove <PHHargrove@lbl.gov>
2023-12-01 19:36:42 -0800

    ofi: restore precedence of Cray PMI over MPI
    This commit reverts one patch hunk from commit e766dd27 "Remove
    GASNET_SPAWN_CONTROL environment variable" which had inadvertently
    removed the intended special-case spawner precedence given to Cray
    PMI (over MPI) in ofi-conduit.
    
    In addition to restoring the forced-spawner logic, this commit updates
    it to use the correct/current variable `GASNET_OFI_SPAWNER` as well as
    logging a forced value as default for the environment variable (since
    it is known to be unset or empty).

--- ofi-conduit/gasnet_core.c
+++ ofi-conduit/gasnet_core.c
@@ -94,7 +94,21 @@ static int gasnetc_init( gex_Client_t            *client_p,
     gasneti_console_message("gasnetc_init","about to spawn..."); 
   #endif
 
-  gasneti_spawner = gasneti_spawnerInit(argc, argv, NULL, &gasneti_nodes, &gasneti_mynode);
+  const char *force_spawner = NULL;
+#if HAVE_PMI_CRAY_H
+  // On a system with Cray PMI, we want to avoid initializing the MPI library
+  // by default despite `srun` being the launch utility for both MPI and PMI.
+  // So, a little extra logic here overrides the normal precedence of MPI over
+  // PMI in gasneti_spawnerInit().
+  const char *spawn_control = gasneti_getenv("GASNET_OFI_SPAWNER"); // just testing for unset/empty
+  // Following line is a *temporary* fall-back to the legacy variable:
+  if (!spawn_control || !spawn_control[0]) spawn_control = gasneti_getenv("GASNET_SPAWN_CONTROL");
+  if (!spawn_control || !spawn_control[0]) {
+    force_spawner = "PMI";
+    gasneti_envstr_display("GASNET_OFI_SPAWNER", "PMI", 1);
+  }
+#endif
+  gasneti_spawner = gasneti_spawnerInit(argc, argv, force_spawner, &gasneti_nodes, &gasneti_mynode);
   if (!gasneti_spawner) GASNETI_RETURN_ERRR(NOT_INIT, "GASNet job spawn failed");
 
   // bug 4597: freeze following process spawning and environment propagation
